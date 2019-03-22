35707a86aabb9a2481ea60a53bec9056ac44d041
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2019-03-22 14:34:00 -0400

    Initial version of setup and version.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+PACKAGES = find_packages()
+
+# Get version and release info, which is all stored in scilpy/version.py
+ver_file = os.path.join('scilpy', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            install_requires=REQUIRES,
+            requires=REQUIRES)
+
+
+if __name__ == '__main__':
+    setup(**opts)
