4ce0834fb002dd043981fed13cab52ee5361c01b
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2019-04-26 23:05:28 -0400

    RF: removed unused code.

--- scilpy/tractanalysis/quick_tools.pyx
+++ scilpy/tractanalysis/quick_tools.pyx
@@ -5,58 +5,6 @@ import cython
 import numpy as np
 cimport numpy as cnp
 
-
-# @cython.boundscheck(False)
-# @cython.wraparound(False)
-# @cython.cdivision(True)
-# def get_intersection_profile_single(atlas_data, strl_indices):
-#     """
-#     TODO change
-#     Get the indices of the voxels traversed by each streamline; then returns
-#     an ArraySequence of indices. Yes, of *indices*. ArraySequence.data is
-#     always of type float32 and contains points except here: it's of type
-#     uint16 and contain indices. You can use this object exactly as you would
-#     use a normal ArraySequence.
-#
-#     :param streamlines: nibabel.streamlines.array_sequence.ArraySequence
-#         should be in voxel space, aligned to corner.
-#     """
-#     cdef:
-#         cnp.npy_intp nb_indices = strl_indices.shape[0]
-#         cnp.npy_intp at_point = 0
-#
-#     label_values = np.full((nb_indices, ), -1, dtype=np.int32)
-#     indices_values = np.full((nb_indices, ), -1, dtype=np.int32)
-#
-#     cdef:
-#         cnp.npy_intp index_idx = 0
-#         cnp.npy_int32[:, :, :] atlas_view = atlas_data
-#         cnp.npy_int32[:] data_view = label_values
-#         cnp.npy_int32[:] indices_view = indices_values
-#
-#
-#     data_view[at_point] = atlas_view[strl_indices[0][0],
-#                                     strl_indices[0][1],
-#                                     strl_indices[0][2]]
-#     indices_view[at_point] = 0
-#
-#     at_point += 1
-#
-#     for index_idx in range(1, nb_indices):
-#         next_val = atlas_view[strl_indices[index_idx][0],
-#                               strl_indices[index_idx][1],
-#                               strl_indices[index_idx][2]]
-#
-#         if next_val != data_view[at_point - 1]:
-#             data_view[at_point] = next_val
-#             indices_view[at_point] = index_idx
-#             at_point += 1
-#
-#     label_values.resize((at_point,), refcheck=False)
-#     indices_values.resize((at_point,), refcheck=False)
-#
-#     return (label_values, indices_values)
-
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def get_next_real_point(points_to_index, vox_index):
