f8841db09fdd606edb9ea5fda333c9358dd89da3
frheault <francois.m.rheault@usherbrooke>
2019-06-19 14:39:38 -0700

    Transfert from public repo, refactor to make both tracking scripts more similar

---
+++ scilpy/tracking/tools.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+DETERMINISTIC = 'det'
+PROBABILISTIC = 'prob'
+EUDX = 'eudx'
+
+
+def get_max_angle_from_curvature(curvature, step_size):
+    """
+    Parameters
+    ----------
+    curvature: float
+        Minimum radius of curvature in mm.
+    step_size: float
+        The tracking step size in mm.
+
+    Return
+    ------
+    theta: float
+        The maximum deviation angle in radian,
+        given the radius curvature and the step size.
+    """
+    theta = 2. * np.arcsin(step_size / (2. * curvature))
+    if np.isnan(theta) or theta > np.pi / 2 or theta <= 0:
+        theta = np.pi / 2.0
+    return theta
+
+
+def get_theta(requested_theta, curvature, step_size, tracking_type):
+    if requested_theta is not None:
+        theta = requested_theta
+    elif curvature > 0:
+        theta = np.rad2deg(get_max_angle_from_curvature(curvature, step_size))
+    elif tracking_type == PROBABILISTIC:
+        theta = 20
+    elif tracking_type == EUDX:
+        theta = 60
+    else:
+        theta = 45
+    return theta
