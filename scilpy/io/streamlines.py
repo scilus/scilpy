187fc46e293a2a6eee5fbdc05112535196c8ed8f
frheault <francois.m.rheault@usherbrooke>
2019-06-25 10:44:59 -0700

    Transfert from Bitbucket, python3 testing, remove tc dependency

---
+++ scilpy/io/streamlines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+import nibabel as nib
+
+
+def check_tracts_same_format(parser, tractogram_1, tractogram_2):
+    """
+    Assert that two filepaths have the same valid extension.
+    :param parser: argparse.ArgumentParser object
+    :param tractogram_1: Tractogram filename #1
+    :param tractogram_2: Tractogram filename #2
+    """
+    if not nib.streamlines.is_supported(tractogram_1):
+        parser.error('Format of "{}" not supported.'.format(tractogram_1))
+    if not nib.streamlines.is_supported(tractogram_2):
+        parser.error('Format of "{}" not supported.'.format(tractogram_2))
+
+    ext_1 = os.path.splitext(tractogram_1)[1]
+    ext_2 = os.path.splitext(tractogram_2)[1]
+    if not ext_1 == ext_2:
+        parser.error(
+            'Input and output tractogram file must use the same format.')
