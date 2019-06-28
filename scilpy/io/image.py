fb6fcfc1f2071cf4f024f805f08f4e317a98b679
frheault <francois.m.rheault@usherbrooke>
2019-06-28 09:07:12 -0700

    Fix JC comment post-PR

--- scilpy/io/image.py
+++ scilpy/io/image.py
@@ -5,12 +5,12 @@ from scilpy.utils.nibabel_tools import get_data
 
 
 def get_reference_info(reference):
-    """Get basic informations from a reference NIFTI file.
+    """Get basic information from a reference NIFTI file.
     :param reference: Nibabel.nifti or filepath (nii or nii.gz)
     """
 
     # TODO remove/replace after stateful_tractogram PR is merged
-    _, nib_file = get_data(reference, return_object=True)
+    nib_file = nib.load(reference)
     reference_shape = nib_file.get_shape()
     reference_affine = nib_file.affine
 
