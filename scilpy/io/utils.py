fb66c35080d6efaf9ca522ef9654f6356c2e91cc
frheault <francois.m.rheault@usherbrooke.ca>
2019-07-31 17:53:17 -0400

    Transfert of scripts, supporting StatefulTractogram

--- scilpy/io/utils.py
+++ scilpy/io/utils.py
@@ -4,10 +4,34 @@
 import os
 import six
 
+from dipy.io.streamline import load_tractogram
 import nibabel as nib
 from nibabel.streamlines import TrkFile
 
 from scilpy.utils.bvec_bval_tools import DEFAULT_B0_THRESHOLD
+
+
+def add_reference(parser):
+    parser.add_argument('--reference',
+                        help='Reference anatomy for Tck/Vtk/Dpy file support.\n'
+                        '(.nii or .nii.gz)')
+
+
+def load_tractogram_with_reference(parser, args, filepath,
+                                   bbox_check=True):
+    _, ext = os.path.splitext(filepath)
+    if ext == '.trk':
+        sft = load_tractogram(filepath, 'same')
+    elif ext in ['.tck', '.fib', '.vtk', '.dpy']:
+        if args.reference is None:
+            parser.error('--reference is required for this file format '
+                         '{}.'.format(filepath))
+        sft = load_tractogram(filepath, args.reference,
+                              bbox_valid_check=bbox_check)
+    else:
+        parser.error('{} is an unsupported file format'.format(filepath))
+    
+    return sft
 
 
 def add_overwrite_arg(parser):
---
+++ scripts/scil_convert_tractogram.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+
+from dipy.io.streamline import save_tractogram
+from dipy.io.utils import create_tractogram_header
+import nibabel as nib
+import numpy as np
+
+from scilpy.io.utils import (add_overwrite_arg, add_reference,
+                             assert_inputs_exist, assert_outputs_exists,
+                             load_tractogram_with_reference)
+
+DESCRIPTION = """
+Conversion of '.tck', '.trk', '.fib', '.vtk' files using updated file format
+standard. TRK file always needs a reference file, a NIFTI, for conversion.
+The FIB file format is in fact a VTK, MITK Diffusion supports it.
+"""
+
+
+def buildArgsParser():
+    p = argparse.ArgumentParser(description=DESCRIPTION,
+                                formatter_class=argparse.RawTextHelpFormatter)
+
+    p.add_argument('in_tractogram', metavar='IN_TRACTOGRAM',
+                   help='Tractogram filename. Format must be \n'
+                        'readable by the Nibabel.')
+
+    p.add_argument('output_name', metavar='OUTPUT_NAME',
+                   help='Output filename. Format must be \n'
+                        'writable by Nibabel.')
+
+    add_reference(p)
+
+    add_overwrite_arg(p)
+
+    return p
+
+
+def main():
+    parser = buildArgsParser()
+    args = parser.parse_args()
+
+    assert_inputs_exist(parser, [args.in_tractogram], [args.reference])
+
+    in_extension = os.path.splitext(args.in_tractogram)[1]
+    out_extension = os.path.splitext(args.output_name)[1]
+
+    if in_extension == out_extension:
+        parser.error('Input and output cannot be of the same file format')
+
+    assert_outputs_exists(parser, args, args.output_name)
+
+    sft = load_tractogram_with_reference(parser, args, args.in_tractogram,
+                                         bbox_check=False)
+    save_tractogram(sft, args.output_name, bbox_valid_check=False)
+
+
+if __name__ == "__main__":
+    main()
