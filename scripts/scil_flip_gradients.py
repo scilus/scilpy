57c146223af60cf7f4a9ec2b8278b98146f67b8a
Guillaume Theaud <guillaume.theaud@gmail.com>
2019-07-31 14:29:15 -0400

    PEP8 fix parser

--- scripts/scil_flip_gradients.py
+++ scripts/scil_flip_gradients.py
@@ -8,34 +8,34 @@ from scilpy.utils.util import str_to_index
 
 
 def build_arg_parser():
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-                                     description='Flip one or more axes of the '
-                                                 ' encoding scheme matrix.')
+    p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                description='Flip one or more axes of the '
+                                            ' encoding scheme matrix.')
 
-    parser.add_argument('encoding_file', action='store', metavar='encoding_file',
-                        type=str,
-                        help='Path to encoding file.')
+    p.add_argument('encoding_file', action='store', metavar='encoding_file',
+                   type=str,
+                   help='Path to encoding file.')
 
-    parser.add_argument('flipped_encoding', action='store',
-                        metavar='flipped_encoding', type=str,
-                        help='Path to the flipped encoding file.')
+    p.add_argument('flipped_encoding', action='store',
+                   metavar='flipped_encoding', type=str,
+                   help='Path to the flipped encoding file.')
 
-    parser.add_argument('axes', action='store', metavar='dimension',
-                        choices=['x', 'y', 'z'], nargs='+',
-                        help='The axes you want to flip. eg: to flip the x '
-                             ' and y axes use: x y')
+    p.add_argument('axes', action='store', metavar='dimension',
+                   choices=['x', 'y', 'z'], nargs='+',
+                   help='The axes you want to flip. eg: to flip the x '
+                        ' and y axes use: x y')
 
-    gradients_type = parser.add_mutually_exclusive_group(required=True)
+    gradients_type = p.add_mutually_exclusive_group(required=True)
     gradients_type.add_argument('--fsl', dest='fsl_bvecs', action='store_true',
                                 help='Specify fsl format')
     gradients_type.add_argument('--mrtrix', dest='fsl_bvecs',
                                 action='store_false',
                                 help='Specify mrtrix format')
 
-    parser.add_argument('-f', action='store_true', dest='overwrite',
-                        help='Force (overwrite output file). [%(default)s]')
+    p.add_argument('-f', action='store_true', dest='overwrite',
+                   help='Force (overwrite output file). [%(default)s]')
 
-    return parser
+    return p
 
 
 def main():
