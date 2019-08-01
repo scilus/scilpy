d1527ea12c734813fc4f1b511be4507d279ba9a2
frheault <francois.m.rheault@usherbrooke.ca>
2019-08-01 16:14:14 -0400

    Testing with float ratio, flake8

--- scripts/scil_perform_majority_vote.py
+++ scripts/scil_perform_majority_vote.py
@@ -5,17 +5,17 @@ import argparse
 
 import numpy as np
 import nibabel as nib
+from nibabel.streamlines.array_sequence import ArraySequence
 from dipy.io.stateful_tractogram import Space, StatefulTractogram
 from dipy.io.streamline import save_tractogram
 from dipy.io.utils import is_header_compatible, get_reference_info
 from scipy.sparse import dok_matrix
 
-
 from scilpy.io.utils import (add_overwrite_arg, add_reference,
                              assert_inputs_exist,
                              assert_outputs_exists,
                              load_tractogram_with_reference)
-from scilpy.tractanalysis import compute_robust_tract_counts_map
+from scilpy.tractanalysis.robust_streamlines_metrics import compute_robust_tract_counts_map
 from scilpy.utils.streamlines import perform_streamlines_operation
 from scilpy.utils.streamlines import intersection
 
@@ -33,22 +33,22 @@ def _build_args_parser():
         description=DESCRIPTION)
 
     p.add_argument('bundles', nargs='+',
-                   help='Input bundles filename')
+                   help='Input bundles filename.')
 
     add_reference(p)
 
-    p.add_argument('--ratio_streamlines', type=int, default=2,
+    p.add_argument('--ratio_streamlines', type=float, default=0.5,
                    help='Minimum vote to be considered for streamlines '
-                   '[%(default)s]')
-    p.add_argument('--ratio_voxels', type=int, default=2,
+                   '[%(default)s].')
+    p.add_argument('--ratio_voxels', type=float, default=0.5,
                    help='Minimum vote to be considered for voxels'
-                   '[%(default)s]')
+                   '[%(default)s].')
 
     p.add_argument('--same_tractogram', action='store_true',
                    help='All bundles need to be from the same tractogram,\n'
-                   'will generate a voting for streamlines too')
+                   'will generate a voting for streamlines too.')
     p.add_argument('--output_prefix', default='voting_',
-                   help='Output prefix, [%(default)s]')
+                   help='Output prefix, [%(default)s].')
 
     add_overwrite_arg(p)
 
@@ -68,9 +68,10 @@ def main():
 
     fusion_streamlines = []
     for name in args.bundles:
-        fusion_streamlines.extend(load_tractogram_with_reference(parser,
-                                                                 args,
-                                                                 name))
+        fusion_streamlines.extend(
+            load_tractogram_with_reference(parser, args, name).streamlines)
+
+    fusion_streamlines = ArraySequence(fusion_streamlines)
     if args.reference:
         reference_file = args.reference
     else:
@@ -85,9 +86,9 @@ def main():
         if not is_header_compatible(reference_file, name):
             raise ValueError('Both headers are not the same')
         sft = load_tractogram_with_reference(parser, args, name)
-        bundle = list(sft.streamlines)
+        bundle = sft.get_streamlines_copy()
         sft.to_vox()
-        bundle_vox_space = list(sft.streamlines)
+        bundle_vox_space = sft.get_streamlines_copy()
         binary = compute_robust_tract_counts_map(bundle_vox_space, dimensions)
         volume[binary > 0] += 1
 
@@ -100,16 +101,18 @@ def main():
     if args.same_tractogram:
         real_indices = []
         for i in range(len(fusion_streamlines)):
-            if np.sum(streamlines_vote[i]) >= args.ratio_streamlines:
+            ratio_value = int(args.ratio_streamlines*len(args.bundles))
+            print(ratio_value)
+            if np.sum(streamlines_vote[i]) >= ratio_value:
                 real_indices.append(i)
-        
+
         new_streamlines = fusion_streamlines[real_indices]
 
         sft = StatefulTractogram(new_streamlines, reference_file, Space.RASMM)
         save_tractogram(sft, output_streamlines_filename)
 
-    volume[volume < args.ratio_voxels] = 0
-    volume[volume >= args.ratio_voxels] = 1
+    volume[volume < int(args.ratio_streamlines*len(args.bundles))] = 0
+    volume[volume >= int(args.ratio_streamlines*len(args.bundles))] = 1
     nib.save(nib.Nifti1Image(volume.astype(np.uint8), transformation),
              output_voxels_filename)
 
