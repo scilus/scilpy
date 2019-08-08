e3f8b6c6f5595077c83b0648e465ac0ecf02c96f
arnaudbore <arnaud.bore@gmail.com>
2019-08-08 10:32:50 -0400

    add new script compute_non_zero_voxels

--- scilpy/utils/image.py
+++ scilpy/utils/image.py
@@ -4,9 +4,37 @@
 from dipy.align.imaffine import AffineMap
 import nibabel as nib
 import numpy as np
+import six
 
 from scilpy.io.image import get_reference_info
 from scilpy.utils.nibabel_tools import get_data
+
+
+def count_non_zero_voxels(image):
+    """
+    Count number of non zero voxels
+
+    Parameters:
+    -----------
+    image: string
+        Path to the image
+    """
+    if isinstance(image, six.string_types):
+        nb_object = nb.load(image)
+    else:
+        nb_object = image
+
+    data = nb_object.get_data(caching='unchanged')
+
+    # Count the number of non-zero voxels.
+    if len(data.shape) >= 4:
+        axes_to_sum = np.arange(3, len(data.shape))
+        nb_voxels = np.count_nonzero(np.sum(np.absolute(data),
+                                            axis=tuple(axes_to_sum)))
+    else:
+        nb_voxels = np.count_nonzero(data)
+
+    return nb_voxels
 
 
 def transform_anatomy(transfo, reference, moving, filename_to_save):
