7f09dc663b26a7bd2b0bd2d505eccfc28fc5a95c
arnaudbore <arnaud.bore@gmail.com>
2019-08-08 09:50:43 -0400

    add NODDI scripts

---
+++ scripts/scil_compute_NODDI.py
@@ -0,0 +1,186 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Compute NODDI [1] maps using AMICO.
+"""
+
+import argparse
+import os
+import shutil
+import tempfile
+
+try:
+    import amico
+except ImportError as e:
+    e.args += ("AMICO not installed and configured. "
+               "Could use a precompiled container.",)
+    raise e
+import numpy as np
+
+from scilpy.io.utils import (add_overwrite_arg, assert_inputs_exist,
+                             assert_outputs_exists)
+
+amico.core.setup()
+
+EPILOG = """
+Reference:
+    [1] Zhang H, Schneider T, Wheeler-Kingshott CA, Alexander DC.
+        NODDI: practical in vivo neurite orientation dispersion
+        and density imaging of the human brain.
+        NeuroImage. 2012 Jul 16;61:1000-16.
+"""
+
+
+def _build_arg_parser():
+    p = argparse.ArgumentParser(
+        description=__doc__, epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    p.add_argument('dwi',
+                   help='DWI file acquired with a NODDI compatible protocol')
+
+    p.add_argument('--mask',
+                   help='Mask filename')
+
+    g = p.add_argument_group('Gradients / scheme')
+    g.add_argument('--bval',
+                   help='Bval filename, in FSL format')
+    g.add_argument('--bvec',
+                   help='Bvec filename, in FSL format')
+    g.add_argument('--scheme_file',
+                   help='If a scheme file already exists, '
+                        'can replace --bval/--bvec.')
+
+    p.add_argument('--para_diff', type=float, default=1.7e-3,
+                   help='Axial diffusivity (AD) in the CC. [%(default)s]')
+    p.add_argument('--iso_diff', type=float, default=3e-3,
+                   help='Mean diffusivity (MD) in ventricles. [%(default)s]')
+    p.add_argument('--lambda1', default=2, type=float,
+                   help='First regularization parameter. [%(default)s]')
+    p.add_argument('--lambda2', default=1e-3, type=float,
+                   help='Second regularization parameter. [%(default)s]')
+
+    p.add_argument('--output_dir',
+                   help='Output directory for the NODDI results. '
+                        '[%(default)s]')
+
+    p.add_argument('--processes', type=int, default=1,
+                   help='Number of processes used to compute NODDI. Right now,'
+                        'better performance with 1. [%(default)s]')
+
+    add_overwrite_arg(p)
+
+    return p
+
+
+def main():
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+
+    required_in = [args.dwi]
+
+    if not any([args.bval, args.bvec, args.scheme_file]):
+        parser.error('Need to provide either [--bval, --bvec] or '
+                     '--scheme_file.')
+
+    if (args.bval or args.bvec) and args.scheme_file:
+        parser.error('Can only provide [--bval, --bvec] or --scheme_file.')
+
+    use_scheme_file = False
+    if not args.scheme_file:
+        if (args.bval and not args.bvec) or (args.bvec and not args.bval):
+            parser.error('Need to specify both bvec and bval.')
+        required_in.extend([args.bval, args.bvec])
+    else:
+        required_in.append(args.scheme_file)
+        use_scheme_file = True
+
+    assert_inputs_exist(parser,
+                        required_in,
+                        [args.mask])
+
+    out_dir = ''
+    if args.output_dir:
+        if not os.path.isdir(args.output_dir):
+            parser.error("Output directory doesn't exist.")
+        out_dir = args.output_dir
+
+    basic_out_files = ['FIT_dir.nii.gz', 'FIT_ICVF.nii.gz',
+                       'FIT_ISOVF.nii.gz', 'FIT_OD.nii.gz']
+    out_files = [os.path.join(out_dir, f) for f in basic_out_files]
+
+    assert_outputs_exists(parser, args, out_files)
+
+    if args.processes <= 0:
+        parser.error('Number of processes cannot be <= 0.')
+    elif args.processes > 1:
+        import multiprocessing
+        if args.processes > multiprocessing.cpu_count():
+            parser.error('Max number of processes is {}. Got {}.'.format(
+                multiprocessing.cpu_count(), args.processes
+            ))
+
+    # Load the data
+    ae = amico.Evaluation('./', './')
+
+    # Generage a scheme file from the bvals and bvecs files
+    if use_scheme_file:
+        scheme_filename = args.scheme_file
+    else:
+        scheme_filename = tempfile.mkstemp(suffix='.scheme',
+                                           text=True,
+                                           dir='././')
+        scheme_filename = scheme_filename[1]
+        amico.util.fsl2scheme(args.bval, args.bvec, scheme_filename)
+
+    # Load the data
+    ae.load_data(dwi_filename=args.dwi,
+                 scheme_filename=scheme_filename,
+                 mask_filename=args.mask)
+
+    # Compute the response functions
+    ae.set_model("NODDI")
+
+    intra_vol_frac = np.linspace(0.1, 0.99, 12)
+    intra_orient_distr = np.hstack((np.array([0.03, 0.06]),
+                                    np.linspace(0.09, 0.99, 10)))
+
+    ae.model.set(args.para_diff, args.iso_diff,
+                 intra_vol_frac, intra_orient_distr, False)
+    ae.set_solver(lambda1=args.lambda1, lambda2=args.lambda2)
+
+    ae.generate_kernels(regenerate=True)
+
+    # Load the precomputed kernels at high resolution
+    ae.load_kernels()
+
+    # Set number of processes
+    solver_params = ae.get_config('solver_params')
+    solver_params['numThreads'] = args.processes
+    ae.set_config('solver_params', solver_params)
+
+    # Model fit
+    ae.fit()
+
+    # Save the results
+    ae.save_results()
+
+    # Copy the output files
+    amico_def_out = [os.path.join('AMICO/NODDI/', f) for f in basic_out_files]
+
+    for in_f, out_f in zip(amico_def_out, out_files):
+        shutil.move(in_f, out_f)
+
+    if args.output_dir != "AMICO":
+        shutil.rmtree("./AMICO")
+
+    # Moving back to the original directory
+    if not use_scheme_file:
+        os.unlink(scheme_filename)
+
+    shutil.rmtree("./kernels")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/scil_compute_NODDI_priors.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Compute the axial and mean diffusivity priors for NODDI.
+"""
+
+from __future__ import division, print_function
+import argparse
+import logging
+
+import nibabel as nib
+import numpy as np
+
+from scilpy.io.utils import (
+    add_overwrite_arg, assert_inputs_exist, assert_outputs_exists)
+
+EPILOG = """
+Reference:
+    [1] Zhang H, Schneider T, Wheeler-Kingshott CA, Alexander DC.
+        NODDI: practical in vivo neurite orientation dispersion
+        and density imaging of the human brain.
+        NeuroImage. 2012 Jul 16;61:1000-16.
+"""
+
+
+def _build_arg_parser():
+    p = argparse.ArgumentParser(
+        description=__doc__, epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    p.add_argument('fa',
+                   help='Path to the FA volume.')
+    p.add_argument('ad',
+                   help='Path to the axial diffusivity (AD) volume.')
+    p.add_argument('md',
+                   help='Path to the mean diffusivity (MD) volume.')
+
+    p.add_argument(
+        '--fa_max', type=float, default='0.1',
+        help='Maximal threshold of FA (voxels under that threshold are '
+             'considered in the ventricles). [%(default)s]')
+    p.add_argument(
+        '--md_min', dest='md_t',  type=float, default='0.003',
+        help='Minimal threshold of MD in mm2/s (voxels above that threshold '
+             'are considered for in the ventricles). [%(default)s]')
+    p.add_argument(
+        '--fa_min', type=float, default='0.7',
+        help='Minimal threshold of FA (voxels above that threshold are '
+             'considered in the single fiber mask). [%(default)s]')
+
+    p.add_argument(
+        '--roi_radius', default=20, type=int,
+        help='Radius of the region used to estimate the priors. The roi will '
+             'be a cube spanning from ROI_CENTER in each direction. '
+             '[%(default)s]')
+    p.add_argument(
+        '--roi_center', metavar='tuple(3)', nargs="+", type=int,
+        help='Center of the roi of size roi_radius used to estimate the '
+             'priors. [center of the 3D volume]')
+
+    p.add_argument(
+        '--output_1fiber', metavar='file',
+        help='Output path for the text file containing the single '
+             'fiber average value of AD. If not set, the file will not be '
+             'saved.')
+    p.add_argument(
+        '--mask_output_1fiber', metavar='file',
+        help='Output path for single fiber mask. If not set, the mask will '
+             'not be saved.')
+    p.add_argument(
+        '--output_ventricles', metavar='file',
+        help='Output path for the text file containing the ventricles average '
+             'value of MD. If not set, the file will not be saved.')
+    p.add_argument(
+        '--mask_output_ventricles', metavar='file',
+        help='Output path for the ventricule mask. If not set, the mask will '
+             'not be saved.')
+
+    add_overwrite_arg(p)
+    p.add_argument('-v', action='store_true', dest='verbose',
+                   help='Use verbose output. Default: false.')
+    return p
+
+
+def load(path):
+    img = nib.load(path)
+    return img.get_data(), img.affine, img.header.get_zooms()[:3]
+
+
+def save(data, affine, output):
+    img = nib.Nifti1Image(np.array(data, 'float32'),  affine)
+    nib.save(img, output)
+
+
+def main():
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+
+    assert_inputs_exist(parser, [args.ad, args.fa, args.md])
+    assert_outputs_exists(parser, args, [],
+                          [args.mask_output_1fiber,
+                           args.mask_output_ventricles,
+                           args.output_ventricles,
+                           args.output_1fiber])
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    fa, aff, hdr = load(args.fa)
+    md, _, _ = load(args.md)
+    ad, _, _ = load(args.ad)
+
+    mask_cc = np.zeros(fa.shape)
+    mask_vent = np.zeros(fa.shape)
+
+    # center
+    if args.roi_center is None:
+        ci, cj, ck = np.array(fa.shape[:3]) // 2
+    else:
+        if len(args.roi_center) != 3:
+            parser.error("roi_center needs to receive 3 values")
+        else:
+            ci, cj, ck = args.roi_center
+
+    w = args.roi_radius
+    roi_ad = ad[int(ci - w): int(ci + w),
+                int(cj - w): int(cj + w),
+                int(ck - w): int(ck + w)]
+    roi_md = md[int(ci - w): int(ci + w),
+                int(cj - w): int(cj + w),
+                int(ck - w): int(ck + w)]
+    roi_fa = fa[int(ci - w): int(ci + w),
+                int(cj - w): int(cj + w),
+                int(ck - w): int(ck + w)]
+
+    logging.debug('fa_min, fa_max, md_t: {}, {}, {}'.format(
+        args.fa_min, args.fa_max, args.md_t))
+
+    indices = np.where((roi_fa > args.fa_min) & (roi_fa < 0.95))
+    N = roi_ad[indices].shape[0]
+
+    logging.debug('Number of voxels found in single fiber area: {}'.format(N))
+
+    cc_avg = np.mean(roi_ad[indices])
+    cc_std = np.std(roi_ad[indices])
+
+    indices[0][:] += ci - w
+    indices[1][:] += cj - w
+    indices[2][:] += ck - w
+    mask_cc[indices] = 1
+
+    indices = np.where((roi_md > args.md_t) & (roi_fa < args.fa_max))
+    N = roi_md[indices].shape[0]
+
+    logging.debug('Number of voxels found in ventricles: {}'.format(N))
+
+    vent_avg = np.mean(roi_md[indices])
+    vent_std = np.std(roi_md[indices])
+
+    indices[0][:] += ci - w
+    indices[1][:] += cj - w
+    indices[2][:] += ck - w
+    mask_vent[indices] = 1
+
+    if args.mask_output_ventricles:
+        save(mask_vent, aff, args.mask_output_ventricles)
+
+    if args.mask_output_1fiber:
+        save(mask_cc, aff, args.mask_output_1fiber)
+
+    if args.output_1fiber:
+        with open(args.output_1fiber, "w") as text_file:
+            text_file.write(str(cc_avg))
+
+    if args.output_ventricles:
+        with open(args.output_ventricles, "w") as text_file:
+            text_file.write(str(vent_avg))
+
+    print("Average AD in single fiber areas: {} +- {}".format(cc_avg, cc_std))
+    print("Average MD in ventricles: {} +- {}".format(vent_avg, vent_std))
+
+
+if __name__ == "__main__":
+    main()
