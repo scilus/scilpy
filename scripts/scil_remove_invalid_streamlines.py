71cc9108f44c21d0bf4de29ca4f87583f8d801a7
frheault <francois.m.rheault@usherbrooke>
2019-09-16 20:35:05 -0400

    Fix script following master update

--- scripts/scil_remove_invalid_streamlines.py
+++ scripts/scil_remove_invalid_streamlines.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import os
 
 from dipy.io.streamline import save_tractogram
 
 from scilpy.io.utils import (add_overwrite_arg, add_reference,
-                             assert_inputs_exist, assert_outputs_exists,
+                             assert_inputs_exist, assert_outputs_exist,
                              load_tractogram_with_reference)
 
 DESCRIPTION = """
@@ -23,11 +22,11 @@ def _build_args_parser():
 
     p.add_argument('in_tractogram', metavar='IN_TRACTOGRAM',
                    help='Tractogram filename. Format must be one of \n'
-                        'trk, tck, vtk, fib, dpy')
+                        'trk, tck, vtk, fib, dpy.')
 
     p.add_argument('output_name', metavar='OUTPUT_NAME',
                    help='Output filename. Format must be one of \n'
-                        'trk, tck, vtk, fib, dpy')
+                        'trk, tck, vtk, fib, dpy.')
 
     add_reference(p)
 
@@ -40,12 +39,8 @@ def main():
     parser = _build_args_parser()
     args = parser.parse_args()
 
-    assert_inputs_exist(parser, [args.in_tractogram], [args.reference])
-
-    in_extension = os.path.splitext(args.in_tractogram)[1]
-    out_extension = os.path.splitext(args.output_name)[1]
-
-    assert_outputs_exists(parser, args, args.output_name)
+    assert_inputs_exist(parser, args.in_tractogram, args.reference)
+    assert_outputs_exist(parser, args, args.output_name)
 
     sft = load_tractogram_with_reference(parser, args, args.in_tractogram,
                                          bbox_check=False)
