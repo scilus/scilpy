9d8994057255791bbeb84450661c033a544fd0da
arnaudbore <arnaud.bore@gmail.com>
2019-09-24 15:08:26 -0400

    add possibility to have more than 10 shells

--- scilpy/viz/sampling_scheme.py
+++ scilpy/viz/sampling_scheme.py
@@ -5,6 +5,7 @@ from tempfile import mkstemp
 
 from dipy.data import get_sphere
 from fury import actor, window
+import fury
 
 vtkcolors = [window.colors.blue,
              window.colors.red,
@@ -20,10 +21,8 @@ vtkcolors = [window.colors.blue,
 def plot_each_shell(ms, use_sym=True, use_sphere=True, same_color=False,
                     rad=0.025, opacity=1.0, ofile=None, ores=(300, 300)):
 
-    #if len(ms)>10:
-#        vtkcolors = fury.colormap.distinguishable_colormap(nb_colors=len(ms))
-#    else:
-#        print(len(ms))
+    if len(ms)>10:
+        vtkcolors = fury.colormap.distinguishable_colormap(nb_colors=len(ms))
 
     if use_sphere:
         sphere = get_sphere('symmetric724')
@@ -58,6 +57,9 @@ def plot_each_shell(ms, use_sym=True, use_sphere=True, same_color=False,
 
 def plot_proj_shell(ms, use_sym=True, use_sphere=True, same_color=False,
                     rad=0.025, opacity=1.0, ofile=None, ores=(300, 300)):
+
+    if len(ms)>10:
+        vtkcolors = fury.colormap.distinguishable_colormap(nb_colors=len(ms))
 
     ren = window.Renderer()
     ren.SetBackground(1, 1, 1)
