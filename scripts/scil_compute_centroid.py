7dd6c9d0db868e820bf5dc982ff91201f896ca0e
arnaudbore <arnaud.bore@gmail.com>
2020-01-22 15:50:02 -0500

    remove distance threshold to get centroid

--- scripts/scil_compute_centroid.py
+++ scripts/scil_compute_centroid.py
@@ -3,6 +3,7 @@
 
 import argparse
 
+import numpy as np
 from dipy.io.stateful_tractogram import Space, StatefulTractogram
 from dipy.io.streamline import save_tractogram
 from dipy.segment.clustering import QuickBundles
@@ -25,11 +26,6 @@ def _build_arg_parser():
                    help='Fiber bundle file.')
     p.add_argument('out_centroid',
                    help='Output centroid streamline filename.')
-
-    p.add_argument('--distance_thres',
-                   type=float, default=200,
-                   help='The maximum distance from a bundle for a streamline '
-                        'to be still considered as part of it.')
     p.add_argument('--nb_points',
                    type=int, default=20,
                    help='Number of points defining the centroid streamline.')
@@ -39,16 +35,13 @@ def _build_arg_parser():
     return p
 
 
-def get_centroid_streamline(streamlines, nb_points, distance_threshold):
+def get_centroid_streamline(streamlines, nb_points):
     resample_feature = ResampleFeature(nb_points=nb_points)
     quick_bundle = QuickBundles(
-        threshold=distance_threshold,
+        threshold=np.inf,
         metric=AveragePointwiseEuclideanMetric(resample_feature))
     clusters = quick_bundle.cluster(streamlines)
     centroid_streamlines = clusters.centroids
-
-    if len(centroid_streamlines) > 1:
-        raise Exception('Multiple centroids found')
 
     return centroid_streamlines
 
@@ -60,9 +53,6 @@ def main():
     assert_inputs_exist(parser, args.in_bundle)
     assert_outputs_exist(parser, args, args.out_centroid)
 
-    if args.distance_thres < 0.0:
-        parser.error('--distance_thres {} should be '
-                     'positive'.format(args.distance_thres))
     if args.nb_points < 2:
         parser.error('--nb_points {} should be >= 2'
                      .format(args.nb_points))
@@ -70,8 +60,7 @@ def main():
     sft = load_tractogram_with_reference(parser, args, args.in_bundle)
 
     centroid_streamlines = get_centroid_streamline(sft.streamlines,
-                                                   args.nb_points,
-                                                   args.distance_thres)
+                                                   args.nb_points)
 
     sft = StatefulTractogram(centroid_streamlines, sft, Space.RASMM)
 
