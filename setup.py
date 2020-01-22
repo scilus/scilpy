cd30bb1d8b6d77a52b15cfeec5c3eb4759e45420
arnaudbore <arnaud.bore@gmail.com>
2020-01-22 13:10:42 -0500

    pep8 fix

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extensions = [Extension('scilpy.tractanalysis.uncompress',
                         include_dirs=[numpy.get_include()]),
               Extension('scilpy.tractanalysis.compute_tract_counts_map',
                         ['scilpy/tractanalysis/compute_tract_counts_map.pyx'],
-                        include_dirs=[numpy.get_include()]),]
+                        include_dirs=[numpy.get_include()])]
 
 opts['ext_modules'] = cythonize(extensions)
 
