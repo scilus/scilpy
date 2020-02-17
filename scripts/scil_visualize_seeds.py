587a9204b89490e4bf6fbe3d1ec9b09cba373159
Antoine Théberge <antoine.theberge@usherbrooke.ca>
2020-02-17 14:14:19 -0500

    Fixed viz tool

--- scripts/scil_visualize_seeds.py
+++ scripts/scil_visualize_seeds.py
@@ -8,6 +8,7 @@ in a tractogram
 
 import argparse
 
+from dipy.io.streamline import load_tractogram
 from fury import window, actor
 from nibabel.streamlines import detect_format, TrkFile
 
@@ -40,9 +41,11 @@ def main():
         raise ValueError("Invalid input streamline file format " +
                          "(must be trk): {0}".format(args.tractogram_filename))
 
-    # Load files and data
-    trk = TrkFile.load(args.tractogram)
-    tractogram = trk.tractogram
+    # Load files and data. TRKs can have 'same' as reference
+    tractogram = load_tractogram(args.tractogram, 'same')
+    # Seeds are always saved in VOX (?)
+    tractogram.to_vox()
+
     streamlines = tractogram.streamlines
     if 'seeds' not in tractogram.data_per_streamline:
         parser.error('Tractogram does not contain seeds')
