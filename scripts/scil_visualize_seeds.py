2ad8987953c6cba27add79b5c235fbb77d80391c
Antoine Théberge <antoine.theberge@usherbrooke.ca>
2020-02-25 14:39:27 -0500

    Add comment to viz

--- scripts/scil_visualize_seeds.py
+++ scripts/scil_visualize_seeds.py
@@ -43,7 +43,8 @@ def main():
 
     # Load files and data. TRKs can have 'same' as reference
     tractogram = load_tractogram(args.tractogram, 'same')
-    # Streamlines are saved in RASMM but seeds are saved in VOX (?)
+    # Streamlines are saved in RASMM but seeds are saved in VOX
+    # This might produce weird behavior with non-iso
     tractogram.to_vox()
 
     streamlines = tractogram.streamlines
