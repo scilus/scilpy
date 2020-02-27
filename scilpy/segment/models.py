98f6bdaf7398289d79f1575dfd156b6e656917c3
frheault <francois.m.rheault@usherbrooke>
2020-02-26 19:22:29 -0500

    Refactoring, testing and flake8

--- scilpy/segment/models.py
+++ scilpy/segment/models.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import os
-import itertools
-import time
-import sys
+import logging
 
 import numpy as np
-import nibabel as nib
 
 from dipy.align.bundlemin import distance_matrix_mdf
-from dipy.tracking.streamline import set_number_of_points, apply_affine
-from dipy.segment.clustering import QuickBundles
+from dipy.tracking.streamline import set_number_of_points
 
-from dipy.segment.clustering import qbx_and_merge
-QBx_included = True
-
-from dipy.align.streamlinear import (StreamlineLinearRegistration,
-                                     BundleMinDistanceMetric)
 
 def add_streamline(a, b, nb_points):
     a_sub = set_number_of_points(a, nb_points)
@@ -38,38 +28,36 @@ def remove_similar_streamlines(streamlines, threshold=5, do_avg=False):
     if len(streamlines) == 1:
         return streamlines
 
-    # Simple trick to make it faster than using 40-60 points
     sample_15_streamlines = set_number_of_points(streamlines, 15)
     distance_matrix = distance_matrix_mdf(sample_15_streamlines,
                                           sample_15_streamlines)
 
-    current_id = 0
+    current_indice = 0
     avg_streamlines = []
     while True:
-        indices = np.where(distance_matrix[current_id] < threshold)[0]
+        sim_indices = np.where(distance_matrix[current_indice] < threshold)[0]
 
         pop_count = 0
         if do_avg:
-            avg_streamline = sample_15_streamlines[current_id]
-        if len(indices) > 1:
-            for ind in indices:
-                # Every streamlines similar to yourself (excluding yourself)
-                # should be deleted from the set of desired streamlines
-                if not current_id == ind:
-                    kicked_out = streamlines.pop(ind-pop_count)
-                    if do_avg:
-                        avg_streamline = add_streamline(kicked_out,
-                                                        avg_streamline, 15)
-                    distance_matrix = np.delete(distance_matrix, ind-pop_count,
-                                                axis=0)
-                    distance_matrix = np.delete(distance_matrix, ind-pop_count,
-                                                axis=1)
-                    pop_count += 1
+            avg_streamline = sample_15_streamlines[current_indice]
+        # Every streamlines similar to yourself (excluding yourself)
+        # should be deleted from the set of desired streamlines
+        for ind in sim_indices:
+            if not current_indice == ind:
+                kicked_out = streamlines.pop(ind-pop_count)
+                if do_avg:
+                    avg_streamline = add_streamline(kicked_out,
+                                                    avg_streamline, 15)
+                distance_matrix = np.delete(distance_matrix, ind-pop_count,
+                                            axis=0)
+                distance_matrix = np.delete(distance_matrix, ind-pop_count,
+                                            axis=1)
+                pop_count += 1
         if do_avg:
             avg_streamlines.append(avg_streamline)
-        current_id += 1
+        current_indice += 1
         # Once you reach the end of the remaining streamlines
-        if current_id >= len(streamlines):
+        if current_indice >= len(streamlines):
             break
 
     if do_avg:
@@ -82,9 +70,8 @@ def subsample_clusters(cluster_map, streamlines, min_distance,
                        min_cluster_size, average_streamlines=False,
                        verbose=False):
     output_streamlines = []
-    # if verbose:
-       # # print 'Total streamlines in tractogram', len(streamlines)
-       # # print 'Total clusters in tractogram', len(cluster_map)
+    logging.debug('%s streamlines in tractogram', len(streamlines))
+    logging.debug('%s clusters in tractogram', len(cluster_map))
 
     # Each cluster is processed independently
     for i in range(len(cluster_map)):
--- scripts/scil_remove_similar_streamlines.py
+++ scripts/scil_remove_similar_streamlines.py
@@ -2,17 +2,21 @@
 
 import argparse
 from itertools import repeat, chain
+import logging
 import multiprocessing
-import os
 import random
 from time import time
 
-import numpy as np
-import nibabel as nib
+from dipy.io.stateful_tractogram import Space, StatefulTractogram
+from dipy.io.streamline import save_tractogram
 
+
+from scilpy.io.streamlines import load_tractogram_with_reference
 from scilpy.segment.models import subsample_clusters
-from scilpy.io.utils import (add_overwrite_arg, add_verbose_arg,
-assert_inputs_exist,
+from scilpy.io.utils import (add_overwrite_arg,
+                             add_verbose_arg,
+                             add_reference_arg,
+                             assert_inputs_exist,
                              assert_outputs_exist)
 from dipy.segment.clustering import qbx_and_merge
 
@@ -22,6 +26,7 @@ same clusters using a MDF threshold. Can be used with big bundle thanks to
 QBx, but if clusters (even some of them have 5000+ streamlines the time
 needed to create a n**2 will grow out of control ...
 """
+
 
 def multiprocess_subsampling(args):
     streamlines = args[0]
@@ -45,23 +50,29 @@ def _buildArgsParser():
     p.add_argument('in_bundle',
                    help='Path of the input bundle.')
 
-    p.add_argument('min_distance', type=float, 
+    p.add_argument('min_distance', type=float,
                    help='Distance threshold for 2 streamlines to be '
-                        'considered similar (mm)')
+                        'considered similar (mm).')
 
-    p.add_argument('out_bundle', 
+    p.add_argument('out_bundle',
                    help='Path of the output tractography file')
 
     p.add_argument('--clustering_thr', type=float, default=6,
                    help='Clustering threshold for QB/QBx (mm), during '
-                        'the first approximation.')
-
-    p.add_argument('--processes', type=int, default=1,
-                   help='Number of desired processes [%(default)s].')
+                        'the first approximation [%(default)s].')
+    p.add_argument('--min_cluster_size', type=int, default=5,
+                   help='Minimum cluster size for the first iteration '
+                        '[%(default)s].')
     p.add_argument('--convergence', action='store', type=int, default=100,
                    help='Streamlines count difference threshold to stop '
-                        're-running the algorithm.')
+                        're-running the algorithm [%(default)s].')
+    p.add_argument('--avg_similar', action='store_true',
+                   help='Average similar streamlines rather than removing them'
+                        '[%(default)s]. Requires a small min_distance.')
+    p.add_argument('--processes', type=int, default=1,
+                   help='Number of desired processes [%(default)s].')
 
+    add_reference_arg(p)
     add_overwrite_arg(p)
     add_verbose_arg(p)
 
@@ -73,69 +84,65 @@ def main():
     args = parser.parse_args()
 
     # Check if the files exist
-    if not os.path.isfile(args.input_bundle):
-        parser.error('"{0}" must be a file!'.format(args.input_bundle))
+    assert_inputs_exist(parser, args.in_bundle)
+    assert_outputs_exist(parser, args, args.out_bundle)
 
-    if os.path.isfile(args.output_bundle) and not args.force_overwrite:
-        parser.error('"{0}" already exist! Use -f to overwrite it.'.format(
-            args.output_bundle))
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
 
-    min_distance = args.min_distance
-    clustering_thr = args.clustering_thr
     nbr_cpu = args.processes
 
-    trk_file = nib.streamlines.load(args.input_bundle)
-    streamlines = list(trk_file.streamlines)
+    sft = load_tractogram_with_reference(parser, args, args.in_bundle)
+    streamlines = list(sft.streamlines)
     original_length = len(streamlines)
-    # print 'Loaded '+str(original_length)+' streamlines...'
+    logging.debug('Loaded %s streamlines...', original_length)
 
     pool = multiprocessing.Pool(nbr_cpu)
     timer = time()
-    # print 'Lauching subsampling on '+str(nbr_cpu)+' processes...'
+
+    logging.debug('Lauching subsampling on %s processes.', nbr_cpu)
     while True:
+        if len(streamlines) < 1000:
+            logging.warning('Subsampling less than 1000 streamlines is risky.')
+            break
         current_iteration_length = len(streamlines)
         skip = int(len(streamlines) / nbr_cpu) + 1
 
         # Cheap trick to avoid duplication in memory, the pop remove from
         # one list to append it to the other, slower but allows bigger bundles
-        if nbr_cpu > 1 and len(streamlines) > 8000:
-            split_streamlines_list = []
+        split_streamlines_list = []
+        for i in range(nbr_cpu):
+            split_streamlines_list.append(streamlines[0:skip])
+            del streamlines[0:skip]
 
-            for i in range(nbr_cpu):
-                split_streamlines_list.append(streamlines[0:skip])
-                del streamlines[0:skip]
+        resulting_streamlines = pool.map(multiprocess_subsampling,
+                                         zip(split_streamlines_list,
+                                             repeat(args.min_distance),
+                                             repeat(args.clustering_thr),
+                                             repeat(args.min_cluster_size),
+                                             repeat(args.avg_similar)))
 
-            resulting_streamlines = pool.map(multiprocess_subsampling,
-                                             zip(split_streamlines_list,
-                                                 repeat(min_distance),
-                                                 repeat(clustering_thr), 
-                                                 repeat(1),
-                                                 repeat(False)))
-
-            # Fused all subprocesses' result together
-            streamlines = list(chain(*resulting_streamlines))
-
+        # Fused all subprocesses' result together
+        streamlines = list(chain(*resulting_streamlines))
         difference_length = current_iteration_length - len(streamlines)
-
-        # print 'Difference (before - after):', \
-            # difference_length, 'streamlines were removed'
+        logging.debug('Difference (before - after): %s streamlines were removed',
+                      difference_length)
+        print
         if difference_length < args.convergence:
-            # print 'The smart-subsampling converged,' \
-                #   '( below '+str(args.convergence)+')'
+            logging.debug('The smart-subsampling converged, below %s '
+                          'different streamlines.', args.convergence)
             break
         else:
-            # print 'Threshold of convergence was not achieved, ' \
-                #   'Need another run...\n'
+            logging.debug('Threshold of convergence was not achieved.'
+                          ' Need another run...\n')
+            args.min_cluster_size = 1
             random.shuffle(streamlines)
 
     # After convergence, we can simply save the output
-    # print
-    # print 'Before -> After:', str(original_length)+' -> '+str(len(streamlines)), \
-        'in {:2f} sec.'.format(time() - timer)
-    new_tractogram = nib.streamlines.Tractogram(streamlines,
-                                                affine_to_rasmm=np.eye(4))
-    nib.streamlines.save(new_tractogram, args.output_bundle,
-                         header=trk_file.header)
+    logging.debug('Before (%s)-> After (%s), total runtime of %s sec.',
+                  original_length, len(streamlines), round(time() - timer, 3))
+    new_sft = StatefulTractogram(streamlines, sft, Space.RASMM)
+    save_tractogram(new_sft, args.out_bundle)
 
 
 if __name__ == "__main__":
