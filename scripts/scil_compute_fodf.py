7ce6bffd07aa72a2544ebacee9b31a1c88285390
Emmanuelle Renauld <emmanuelle.renauld@usherbrooke.ca>
2020-03-12 14:08:00 -0400

    changed fdata to dataobj

--- scripts/scil_compute_fodf.py
+++ scripts/scil_compute_fodf.py
@@ -101,7 +101,7 @@ def main():
 
     full_frf = np.loadtxt(args.frf_file)
     vol = nib.load(args.input)
-    data = vol.get_fdata()
+    data = vol.dataobj
     bvals, bvecs = read_bvals_bvecs(args.bvals, args.bvecs)
 
     if args.mask is None:
