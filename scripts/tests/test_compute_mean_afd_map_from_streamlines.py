1cbade12286df1da2777d45c78ff47b0727a86e7
arnaudbore <arnaud.bore@gmail.com>
2020-03-21 16:12:19 -0400

    fix extension

--- scripts/tests/test_compute_mean_afd_map_from_streamlines.py
+++ scripts/tests/test_compute_mean_afd_map_from_streamlines.py
@@ -3,5 +3,5 @@
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_compute_mean_afd_map_from_streamlines', '--help')
+    ret = script_runner.run('scil_compute_mean_afd_map_from_streamlines.py', '--help')
     assert ret.success
