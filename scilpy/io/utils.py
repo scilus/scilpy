473dc51d1d3c3677e6e1401194b158d955798253
frheault <francois.m.rheault@usherbrooke>
2020-03-24 18:45:21 -0400

    Create script frame, minor tryout

--- scilpy/io/utils.py
+++ scilpy/io/utils.py
@@ -218,7 +218,8 @@ def assert_output_dirs_exist_and_empty(parser, args, *dirs, create_dir=False):
     for cur_dir in dirs:
         if not os.path.isdir(cur_dir):
             if not create_dir:
-                parser.error('Output directory {} doesn\'t exist.'.format(cur_dir))
+                parser.error(
+                    'Output directory {} doesn\'t exist.'.format(cur_dir))
             else:
                 os.makedirs(cur_dir, exist_ok=True)
         if os.listdir(cur_dir):
@@ -265,3 +266,27 @@ def read_info_from_mb_bdo(filename):
     radius = np.asarray(radius, dtype=np.float32)
     center = np.asarray(center, dtype=np.float32)
     return geometry, radius, center
+
+
+def load_matrix_in_any_format(filepath):
+    _, ext = os.path.splitext(filepath)
+    if ext == '.txt':
+        data = np.loadtxt(filepath)
+    elif ext == '.npy':
+        data = np.load(filepath)
+    else:
+        parser.error('Extension {} is not supported'.format(ext))
+        raise ValueError
+
+    return data
+
+
+def save_matrix_in_any_format(filepath, output_data):
+    _, ext = os.path.splitext(filepath)
+    if ext == '.txt':
+        np.savetxt(filepath, output_data)
+    elif ext == '.npy' or ext == '':
+        np.save(filepath, output_data)
+    else:
+        parser.error('Extension {} is not supported'.format(ext))
+        raise ValueError
