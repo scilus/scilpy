fb8ec1ac711c52a0741d894ebd9b61fa0a2bd917
frheault <francois.m.rheault@usherbrooke>
2020-03-31 11:34:37 -0400

    Added alternating color, also explore the help (optional)

--- scripts/scil_search_keywords.py
+++ scripts/scil_search_keywords.py
@@ -7,28 +7,34 @@ search will be the intersection of all provided keywords, found either in the
 script name or in its docstring.
 """
 
-from argparse import ArgumentParser, RawTextHelpFormatter
+import argparse
 import ast
 import inspect
+import os
 import pathlib
 import re
 
 import numpy as np
 import scilpy
-from scilpy.io.utils import add_verbose_arg
 
 RED = '\033[31m'
 BOLD = '\033[1m'
-ENDC = '\033[0m'
+ENDC_1 = '\033[0m'
+ENDC_2 = '\033[37m'
+spacing = '=================='
 
 
 def _build_arg_parser():
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=RawTextHelpFormatter)
-    parser.add_argument('keywords', nargs='+',
-                        help="Search keywords")
-    add_verbose_arg(parser)
-    return parser
+    p = argparse.ArgumentParser(description=__doc__,
+                                formatter_class=argparse.RawTextHelpFormatter)
+    p.add_argument('keywords', nargs='+',
+                   help='Search the provided keywords.')
+
+    p.add_argument('--show_docstring', action='store_true',
+                   help='Display the script full docstring.')
+    p.add_argument('--show_help', action='store_true',
+                   help='Display the script full argparser (much slower).')
+    return p
 
 
 def main():
@@ -36,47 +42,65 @@ def main():
     args = parser.parse_args()
 
     scilpy_init_file = inspect.getfile(scilpy)  # scilpy/scilpy/__init__.py
-    script_dir = pathlib.Path(scilpy_init_file).parent / "../scripts"
+    script_dir = pathlib.Path(scilpy_init_file).parent / '../scripts'
     matches = []
 
-    kw_subs = [re.compile("(" + re.escape(kw) + ")", re.IGNORECASE)
+    kw_subs = [re.compile('(' + re.escape(kw) + ')', re.IGNORECASE)
                for kw in args.keywords]
 
+    counter = 0
     for script in sorted(script_dir.glob('*.py')):
         filename = script.name
-        docstring = _get_docstring(str(script))
 
-        if args.verbose:
+        if args.show_help:
             # Display full docstring
-            display_info = docstring
+            help_print = os.popen('{} --help'.format(script))
+            display_info = help_print.read()
         else:
             # Extract first sentence by finding the first dot
-            display_info = _extract_first_sentence(docstring)
-            display_info = display_info.replace("\n", " ")
+            docstring = _get_docstring(str(script))
+            if args.show_docstring:
+                display_info = docstring
+            else:
+                display_info = _extract_first_sentence(docstring)
+                display_info = display_info.replace('\n', ' ')
 
         # Test intersection of all keywords, either in filename or docstring
-        if not _test_matching_keywords(args.keywords, [filename, docstring]):
+        if not _test_matching_keywords(args.keywords, [filename, display_info]):
             continue
 
         matches.append(filename)
+        display_info = display_info or 'No docstring available!'
 
         # new_key calls regex group \1 to keep the same case
-        new_key = "{}\\1{}".format(RED + BOLD, ENDC)
+        # Alternate light gray and white for easier reading
+        if counter % 2 == 0:
+            color_scheme = ENDC_1
+        else:
+            color_scheme = ENDC_2
+
+        display_info = color_scheme + display_info
+        new_key = '{}\\1{}'.format(RED + BOLD, ENDC_1+color_scheme)
+
         for regex in kw_subs:
             filename = regex.sub(new_key, filename)
             display_info = regex.sub(new_key, display_info)
 
-        display_info = display_info or "No docstring available!"
-
-        if args.verbose:
-            print("=====", filename, "====")
+        if args.show_docstring:
+            print(color_scheme, spacing, filename, spacing)
             print('"{}"'.format(display_info))
             print()
+        elif args.show_help:
+            print(color_scheme, spacing, filename, spacing)
+            print(display_info)
+            print()
         else:
-            print(filename, '"{}"'.format(display_info))
+            print(color_scheme+filename, '"{}"'.format(display_info))
+
+        counter += 1
 
     if not matches:
-        print("No results found!")
+        print('No results found!')
 
 
 def _test_matching_keywords(keywords, texts):
@@ -143,7 +167,7 @@ def _get_docstring(script):
     with open(script, 'r') as reader:
         file_contents = reader.read()
     module = ast.parse(file_contents)
-    docstring = ast.get_docstring(module) or ""
+    docstring = ast.get_docstring(module) or ''
     return docstring
 
 
