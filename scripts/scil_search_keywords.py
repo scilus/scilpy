24c8b40734fbb8335fdb7c4f1145b1af25fafb91
Philippe Poulin <ppoulin91@gmail.com>
2020-04-02 11:33:42 -0400

    ENH: scil_search_keywords - Always show docstring, option to use parser

--- scripts/scil_search_keywords.py
+++ scripts/scil_search_keywords.py
@@ -9,13 +9,11 @@ script name or in its docstring.
 
 import argparse
 import ast
-import inspect
 import os
 import pathlib
 import re
 
 import numpy as np
-import scilpy
 
 RED = '\033[31m'
 BOLD = '\033[1m'
@@ -28,12 +26,12 @@ def _build_arg_parser():
     p = argparse.ArgumentParser(description=__doc__,
                                 formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument('keywords', nargs='+',
-                   help='Search the provided keywords.')
+                   help='Search the provided list of keywords.')
 
-    p.add_argument('--show_docstring', action='store_true',
-                   help='Display the script full docstring.')
-    p.add_argument('--show_help', action='store_true',
-                   help='Display the script full argparser (much slower).')
+    p.add_argument('--search_parser', action='store_true',
+                   help='Search through and display the full script argparser '
+                        'instead of looking only at the docstring. (warning: '
+                        'much slower).')
     return p
 
 
@@ -52,25 +50,20 @@ def main():
     for script in sorted(script_dir.glob('*.py')):
         filename = script.name
 
-        if args.show_help:
-            # Display full docstring
-            help_print = os.popen('{} --help'.format(script))
-            display_info = help_print.read()
+        if args.search_parser:
+            # Run the script's argparser
+            help_print = os.popen('{} --help'.format(script.absolute()))
+            search_text = help_print.read()
         else:
-            # Extract first sentence by finding the first dot
-            docstring = _get_docstring(str(script))
-            if args.show_docstring:
-                display_info = docstring
-            else:
-                display_info = _extract_first_sentence(docstring)
-                display_info = display_info.replace('\n', ' ')
+            # Fetch the docstring
+            search_text = _get_docstring(str(script))
 
         # Test intersection of all keywords, either in filename or docstring
-        if not _test_matching_keywords(args.keywords, [filename, display_info]):
+        if not _test_matching_keywords(args.keywords, [filename, search_text]):
             continue
 
         matches.append(filename)
-        display_info = display_info or 'No docstring available!'
+        search_text = search_text or 'No docstring available!'
 
         # new_key calls regex group \1 to keep the same case
         # Alternate light gray and white for easier reading
@@ -79,23 +72,16 @@ def main():
         else:
             color_scheme = ENDC_2
 
-        display_info = color_scheme + display_info
-        new_key = '{}\\1{}'.format(RED + BOLD, ENDC_1+color_scheme)
+        search_text = color_scheme + search_text
+        new_key = '{}\\1{}'.format(RED + BOLD, END_COLOR + color_scheme)
 
         for regex in kw_subs:
             filename = regex.sub(new_key, filename)
-            display_info = regex.sub(new_key, display_info)
+            search_text = regex.sub(new_key, search_text)
 
-        if args.show_docstring:
-            print(color_scheme, spacing, filename, spacing)
-            print('"{}"'.format(display_info))
-            print()
-        elif args.show_help:
-            print(color_scheme, spacing, filename, spacing)
-            print(display_info)
-            print()
-        else:
-            print(color_scheme+filename, '"{}"'.format(display_info))
+        print(color_scheme, SPACING, filename, SPACING)
+        print(search_text)
+        print()
 
         counter += 1
 
@@ -129,26 +115,6 @@ def _test_matching_keywords(keywords, texts):
         matches.append(key_match)
 
     return np.all(matches)
-
-
-def _extract_first_sentence(text):
-    """Extract the first sentence of a string by finding the first dot. If
-    there is no dot, return the full string.
-
-    Parameters
-    ----------
-    text : str
-        Text to parse.
-
-    Returns
-    -------
-    first_sentence : str
-        The first sentence, or the full text if no dot was found.
-
-    """
-    first_dot_idx = text.find('.') + 1 or None
-    sentence = text[:first_dot_idx]
-    return sentence
 
 
 def _get_docstring(script):
