d49865bb8185d0c8adffd8e97439810127a8f45c
frheault <francois.m.rheault@usherbrooke>
2020-04-19 10:43:54 -0400

    Added description, references and docstring

--- scripts/scil_compare_connectivity.py
+++ scripts/scil_compare_connectivity.py
@@ -2,12 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """
-Performs a network-based statistical comparison for populations X and Y using
-a t-statistic threshold of alpha. All matrices must have the same shape and be
-ordered in the same way.
+Performs a network-based statistical comparison for populations G1 and G2 using
+a t-statistic threshold of alpha. All matrices must have the same shape.
 
-For more details visit:
-https://brainconn.readthedocs.io/en/latest/generated/brainconn.nbs.nbs_bct.html
+For example, if you have streamline count weighted matrices for a MCI and a
+control group and you want to investiguate difference in their connectomme:
+    scil_compare_connectivity.py --g1 MCI/*_sc.npy --g2 CTL/*_sc.npy
+
+--filtering_mask will simply multiply the binary mask to with all input
+matrices before performing the statistical compariso.
+
+For more details visit (notes after the docstring):
+https://github.com/aestrivex/bctpy/wiki#network-based-statistic
 """
 
 import argparse
@@ -22,10 +28,12 @@ from scilpy.io.utils import (add_overwrite_arg, add_verbose_arg,
 
 
 EPILOG = """
-References:
-    [1] Rubinov, Mikail, and Olaf Sporns. "Complex network measures of brain
-        connectivity: uses and interpretations." Neuroimage 52.3 (2010):
-        1059-1069.
+[1] Rubinov, Mikail, and Olaf Sporns. "Complex network measures of brain
+    connectivity: uses and interpretations." Neuroimage 52.3 (2010):
+    1059-1069.
+[2] Zalesky, Andrew, Alex Fornito, and Edward T. Bullmore. "Network-based
+    statistic: identifying differences in brain networks." Neuroimage 53.4 
+    (2010): 1197-1207.
 """
 
 
@@ -36,20 +44,25 @@ def _build_arg_parser():
         epilog=EPILOG)
 
     p.add_argument('--in_g1', nargs='+', required=True,
-                   help='')
+                   help='List of matrices for the first population (.npy).')
     p.add_argument('--in_g2', nargs='+', required=True,
-                   help='')
+                   help='List of matrices for the second population (.npy).')
 
     p.add_argument('--t_value', type=float, default=3,
-                   help='')
+                   help='Minimum t-value used as threshold [%(default)s].')
     p.add_argument('--nb_permutations', type=int, default=1000,
-                   help='')
+                   help='Number of permutations used to estimate the empirical '
+                        'null distribution [%(default)s].')
     p.add_argument('--tail', choices=['left', 'right', 'both'], default='both',
-                   help='')
+                   help='Enables specification of an alternative hypothesis:\n'
+                        'left: mean of g1 < mean of g2,\n'
+                        'right: mean of g2 < mean of g1,\n'
+                        'both: both means are unequal (default).')
     p.add_argument('--paired', action='store_true',
-                   help='')
+                   help='Use paired sample t-test instead of population t-test.\n'
+                        '--in_g1 and --in_g2 must be ordered the same way.')
     p.add_argument('--filtering_mask',
-                   help='Binary filtering mask to apply before computing the '
+                   help='Binary filtering mask (.npy) to apply before computing the '
                         'measures.')
     add_verbose_arg(p)
     add_overwrite_arg(p)
