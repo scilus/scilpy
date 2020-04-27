aac607436d51030afc8e73dce89c33ad9eb22815
frheault <francois.m.rheault@usherbrooke>
2020-04-27 11:49:19 -0400

    Proper saving of output

--- scripts/scil_compare_connectivity.py
+++ scripts/scil_compare_connectivity.py
@@ -18,13 +18,16 @@ https://github.com/aestrivex/bctpy/wiki#network-based-statistic
 
 import argparse
 import logging
+import os
 
 import bct
 import numpy as np
 
 from scilpy.io.utils import (add_overwrite_arg, add_verbose_arg,
-                             assert_inputs_exist,
-                             load_matrix_in_any_format)
+                             assert_inputs_exist, assert_outputs_exist,
+                             assert_output_dirs_exist_and_empty,
+                             load_matrix_in_any_format,
+                             save_matrix_in_any_format)
 
 
 EPILOG = """
@@ -32,7 +35,7 @@ EPILOG = """
     connectivity: uses and interpretations." Neuroimage 52.3 (2010):
     1059-1069.
 [2] Zalesky, Andrew, Alex Fornito, and Edward T. Bullmore. "Network-based
-    statistic: identifying differences in brain networks." Neuroimage 53.4 
+    statistic: identifying differences in brain networks." Neuroimage 53.4
     (2010): 1197-1207.
 """
 
@@ -42,6 +45,9 @@ def _build_arg_parser():
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         epilog=EPILOG)
+
+    p.add_argument('out_matrix',
+                   help='Output matrix (.npy) containing the edges p-value.')
 
     p.add_argument('--in_g1', nargs='+', required=True,
                    help='List of matrices for the first population (.npy).')
@@ -64,6 +70,10 @@ def _build_arg_parser():
     p.add_argument('--filtering_mask',
                    help='Binary filtering mask (.npy) to apply before computing the '
                         'measures.')
+
+    p.add_argument('--save_all', metavar='OUT_DIR',
+                   help='Save all the subcomponents in a text format in the'
+                        'specified output directory.')
     add_verbose_arg(p)
     add_overwrite_arg(p)
 
@@ -76,6 +86,11 @@ def main():
 
     assert_inputs_exist(parser, args.in_g1+args.in_g2,
                         args.filtering_mask)
+    assert_outputs_exist(parser, args, args.out_matrix)
+
+    if args.save_all:
+        assert_output_dirs_exist_and_empty(parser, args, args.save_all,
+                                           create_dir=True)
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -84,6 +99,7 @@ def main():
         filtering_mask = load_matrix_in_any_format(args.filtering_mask)
     else:
         filtering_mask = 1
+
     matrices_g1 = [load_matrix_in_any_format(i)*filtering_mask
                    for i in args.in_g1]
     matrices_g2 = [load_matrix_in_any_format(i)*filtering_mask
@@ -92,15 +108,36 @@ def main():
                               axis=0, start=3)
     matrices_g2 = np.rollaxis(np.array(matrices_g2),
                               axis=0, start=3)
-    pval, _, _ = bct.nbs.nbs_bct(matrices_g1, matrices_g2,
-                                 thresh=args.t_value,
-                                 k=args.nb_permutations,
-                                 tail=args.tail,
-                                 paired=args.paired,
-                                 verbose=args.verbose)
+    pval, adj, _ = bct.nbs.nbs_bct(matrices_g1, matrices_g2,
+                                   thresh=args.t_value,
+                                   k=args.nb_permutations,
+                                   tail=args.tail,
+                                   paired=args.paired,
+                                   verbose=args.verbose)
+    logging.debug(
+        '{} components has been found in the networks'.format(len(pval)))
+    if args.save_all:
+        for i in range(len(pval)):
+            coord = np.array(np.where(adj == i)).T
+            results = np.zeros((len(coord), 3))
+            results[:, 0:2] = coord
+            results[:, 2] = pval[i]
+            filename = os.path.join(args.save_all,
+                                    'component_{}.txt'.format(i))
+            # Manual array saving to support int for coord and float for pval
+            with open(filename, 'a') as the_file:
+                for j in range(len(results)):
+                    the_file.write('{} {} {}\n'.format(int(results[j, 0]),
+                                                     int(results[j, 1]),
+                                                     round(results[j, 2], 5)))
+            logging.debug('Components #{} had {} elements and a pvalue of {}'.format(
+                i, len(coord), pval[i]))
 
-    print('The statistical significance (pvalue) of the difference between '
-          'both group is {}'.format(round(pval, 5)))
+    matrix = np.zeros(adj.shape)
+    for i in range(len(pval)):
+        matrix[adj == i] = pval[i]
+
+    save_matrix_in_any_format(args.out_matrix, matrix)
 
 
 if __name__ == '__main__':
