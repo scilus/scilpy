f0c0c81a06a5ed157d803155c8cfcfacdba9f361
karanphil <philippe.karan@usherbrooke.ca>
2020-05-07 11:26:14 -0400

    WIP : fixing NuFO

--- scripts/scil_compute_fodf_metrics.py
+++ scripts/scil_compute_fodf_metrics.py
@@ -199,8 +199,9 @@ def main():
         nib.save(nib.Nifti1Image(rgb_map.astype('uint8'), affine), args.rgb)
 
     if args.peaks:
-        nib.save(nib.Nifti1Image(reshape_peaks_for_visualization(peak_dirs),
+        # nib.save(nib.Nifti1Image(reshape_peaks_for_visualization(peak_dirs),
                                  affine), args.peaks)
+        nib.save(nib.Nifti1Image(peak_indices, vol.affine), args.peaks)
 
     if args.visu:
         if nufo_map.max() > nufo_map.min():
