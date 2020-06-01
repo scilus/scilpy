988b5e570311a9cc401d7275d07bda9e1e20ebef
frheault <francois.m.rheault@usherbrooke.ca>
2020-06-01 13:51:46 -0400

    Switch to python 3

--- scripts/scil_flip_gradients.py
+++ scripts/scil_flip_gradients.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Flip one or more axes of the gradient sampling matrix. It will be saved in
--- scripts/scil_flip_volume.py
+++ scripts/scil_flip_volume.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 """
 Flip the volume according to the specified axis.
