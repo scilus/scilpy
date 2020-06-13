76232c3508dc1c5d6a85c14da5db73fc608cebef
Guillaume Theaud <guillaume.theaud@gmail.com>
2020-06-13 11:50:11 -0400

    Test automock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath("../.."))
 
-autodoc_mock_imports = ['numpy', 'dipy', 'fury', 'nibabel', "scipy", "scilpy"]
+autodoc_mock_imports = ['numpy', 'dipy', 'fury', 'nibabel', "scipy"]
 
 # -- Project information -----------------------------------------------------
 
