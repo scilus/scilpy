bcc3ba739274c60c89f0ebf553f7ea4627cfccb4
frheault <francois.m.rheault@usherbrooke.ca>
2020-06-16 19:38:58 -0400

    Fetch only specific zip

--- scripts/tests/test_flip_streamlines.py
+++ scripts/tests/test_flip_streamlines.py
@@ -8,7 +8,7 @@ from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
 
 
 # If they already exist, this only takes 5 seconds (check md5sum)
-fetch_data(get_testing_files_dict())
+fetch_data(get_testing_files_dict(), keys='surface_vtk_fib.zip')
 tmp_dir = tempfile.TemporaryDirectory()
 
 
