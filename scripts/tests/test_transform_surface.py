d2dfd7ec081c371bbb7f949a0584491dc54f8bf7
frheault <francois.m.rheault@usherbrooke.ca>
2020-06-16 19:12:16 -0400

    Individual test

--- scripts/tests/test_transform_surface.py
+++ scripts/tests/test_transform_surface.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import tempfile
+
+from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
+
+
+# If they already exist, this only takes 5 seconds (check md5sum)
+fetch_data(get_testing_files_dict())
+tmp_dir = tempfile.TemporaryDirectory()
+
 
 def test_help_option(script_runner):
     ret = script_runner.run('scil_transform_surface.py', '--help')
+    assert ret.success
+
+
+def test_execution_surface_vtk_fib(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    input_surf = os.path.join(get_home(), 'surface_vtk_fib',
+                              'lhpialt.vtk')
+    input_aff = os.path.join(get_home(), 'surface_vtk_fib',
+                             'affine.txt')
+    ret = script_runner.run('scil_transform_surface.py', input_surf, input_aff,
+                            'lhpialt_lin.vtk')
     assert ret.success
