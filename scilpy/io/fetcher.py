9d37e4b14890aef3477bd95ff41a61c82157bd2c
frheault <francois.m.rheault@usherbrooke.ca>
2020-06-24 15:43:56 -0400

    Bias test added

--- scilpy/io/fetcher.py
+++ scilpy/io/fetcher.py
@@ -40,7 +40,7 @@ def get_testing_files_dict():
              '075deda4532042192c4103df4371ecb4'],
             'processing.zip':
             ['1caaKoAChyPs5c4WemQWUsR-efD_q2z_b',
-             'ffaddcbe7f6d67e83ed82c4a148b0c6a'],
+             '57aee810f2f5c687df48de65935bb527'],
             'surface_vtk_fib.zip':
             ['1c9KMNFeSkyYDgu3SH_aMf0kduIlpt7cN',
              '946beb4271b905a2bd69ad2d80136ca9'],
