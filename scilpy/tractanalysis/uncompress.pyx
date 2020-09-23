6d23603810b5af93daf9ab21d101fa08bd359472
frheault <francois.m.rheault@usherbrooke.ca>
2020-09-23 18:52:01 -0400

    Remove resize

--- scilpy/tractanalysis/uncompress.pyx
+++ scilpy/tractanalysis/uncompress.pyx
@@ -51,7 +51,7 @@ def uncompress(streamlines, return_mapping=False):
 
         # Multiplying by 6 is simply a heuristic to avoiding resizing too many
         # times. In my bundles tests, I had either 0 or 1 resize.
-        cnp.npy_intp max_points = (streamlines.get_data().size / 3) * 6
+        cnp.npy_intp max_points = (streamlines.get_data().size / 3)
 
     new_array_sequence = nib.streamlines.array_sequence.ArraySequence()
     new_array_sequence._lengths.resize(nb_streamlines)
