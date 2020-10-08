30c21fa615316c05ff49b503ccc2ec62ff76f31c
arnaudbore <arnaud.bore@gmail.com>
2020-10-08 10:41:51 -0400

    Update test_compute_NODDI.py
    change in_mask -> mask

--- scripts/tests/test_compute_NODDI.py
+++ scripts/tests/test_compute_NODDI.py
@@ -27,7 +27,7 @@ def test_execution_commit_amico(script_runner):
     in_mask = os.path.join(get_home(), 'commit_amico',
                            'mask.nii.gz')
     ret = script_runner.run('scil_compute_NODDI.py', in_dwi,
-                            in_bval, in_bvec, '--in_mask', in_mask,
+                            in_bval, in_bvec, '--mask', in_mask,
                             '--out_dir', 'noddi/', '--b_thr', '30',
                             '--para_diff', '0.0017', '--iso_diff', '0.003',
                             '--lambda1', '0.5', '--lambda2', '0.001',
