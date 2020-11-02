468c39003e50db0800eaecc2e344247f3fcd95ea
Antoine Théberge <theberge.antoine.cem@gmail.com>
2020-11-02 18:14:51 -0500

    ENH: Lazy load tractograms

--- scripts/scil_visualize_bundles.py
+++ scripts/scil_visualize_bundles.py
@@ -6,6 +6,7 @@ import glob
 import nibabel as nib
 import os
 import random
+import itertools
 
 import numpy as np
 
@@ -19,11 +20,17 @@ def _build_arg_parser():
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument('in_bundles',
                    help='List of tractography files supported by nibabel.')
-    p.add_argument('--random_coloring', metavar='SEED', type=int,
-                   help='Assign a random color to bundles')
+    coloring_group = p.add_mutually_exclusive_group()
+    coloring_group.add_argument('--random_coloring', metavar='SEED', type=int,
+                                help='Assign a random color to bundles.')
+    coloring_group.add_argument('--color_list', type=str, metavar='FILE',
+                                help='File containing colors for each bundle.')
+    p.add_argument('--shape', type=str,
+                   choices=['line', 'tube'], default='tube',
+                   help='Display streamlines either as lines or tubes.')
     p.add_argument('--subsample', metavar='N', type=int, default=1,
                    help='Only visualize 1 in N streamlines. This is useful' +
-                        ' in case tractograms are too heavy')
+                        ' in case tractograms are too heavy.')
 
     add_overwrite_arg(p)
     return p
@@ -46,17 +53,27 @@ def main():
     else:
         bundle_filenames = glob.glob(args.in_bundles)
 
+    # TODO: Use scilpy io verif functions
+    if len(bundle_filenames) < 1:
+        parser.error('{} : No files found !'.format(args.in_bundles))
+
     scene = window.Scene()
     for filename in bundle_filenames:
-        streamlines = nib.streamlines.load(filename).tractogram.streamlines
+        try:
+            streamlines = nib.streamlines.load(
+                filename, lazy_load=True).tractogram.streamlines
+        except ValueError:
+            print('Skipping {}'.format(filename))
+            continue
 
         color = None
         if args.random_coloring:
             np.random.seed(int(args.random_coloring))
             color = random_rgb()
 
-        line_actor = actor.streamtube(streamlines[::args.subsample],
-                                      colors=color)
+        line_actor = actor.streamtube(
+            list(itertools.islice(streamlines, 0, None, args.subsample)),
+            colors=color)
         scene.add(line_actor)
 
     showm = window.ShowManager(scene, reset_camera=True)
