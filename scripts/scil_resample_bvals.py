b331b7588857b9a2edc04e80445adc365a659ce5
Maxime Descoteaux <maxime.descoteaux@gmail.com>
2020-12-31 13:48:42 -0500

    skeleton of a script

---
+++ scripts/scil_resample_bvals.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Resample b-values on specific b-value shells for sampling schemes where
+b-values of a shell are not all identicall. The --tolerance
+argument needs to be adjusted to vary the accepted interval around the 
+targetted b-value. 
+
+For example, a b-value of 2000 and a tolerance of 20 will resample all 
+volumes with a b-values from 1980 to 2020 to the value of 2000.
+
+scil_resample_bvals.py grad.bval 0 1000 2000 new_grad.bval --tolerance 20
+"""
+
+import argparse
+import logging
+
+from dipy.io import read_bvals_bvecs
+import nibabel as nib
+import numpy as np
+
+from scilpy.io.utils import (add_overwrite_arg, add_verbose_arg,
+                             assert_inputs_exist, assert_outputs_exist)
+from scilpy.utils.bvec_bval_tools import identify_shells
+
+
+def _build_arg_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('bvals',
+                        help='The b-values in FSL format.')
+
+    parser.add_argument('bvals_to_extract', nargs='+',
+                        metavar='bvals-to-extract', type=int,
+                        help='The list of b-values to extract. For example '
+                             '0 1000 2000.')
+
+    parser.add_argument('output_bvals',
+                        help='The name of the output b-values.')
+
+    parser.add_argument('--tolerance', '-t',
+                        metavar='INT', type=int, default=20,
+                        help='The tolerated gap between the b-values to '
+                             'extract\nand the actual b-values.')
+
+    add_verbose_arg(parser)
+    add_overwrite_arg(parser)
+
+    return parser
+
+
+def main():
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+
+    assert_inputs_exist(parser, [args.bvals])
+    assert_outputs_exist(parser, args, [args.output_bvals])
+
+    bvals,  = read_bvals_bvecs(args.bvals)
+
+    # Find the volume indices that correspond to the shells to extract.
+    tol = args.tolerance
+
+    centroids,shell_indices = identify_shells(bvals, tol, True)
+
+    print(centroids)
+    print(shell_indices)
+
+    logging.info("Selected indices: {}".format(shell_indices))
+
+    np.savetxt(args.output_bvals, new_bvals, '%d')
+
+
+if __name__ == "__main__":
+    main()
