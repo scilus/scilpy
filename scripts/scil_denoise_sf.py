b1442508bd210e9ca747ebd980fa85c2aab6f300
charles.poirier <charles.poirier@usherbrooke.ca>
2021-01-28 16:08:05 -0500

    RF:  Remove out_dir arg + load input as float64

--- scripts/scil_denoise_sf.py
+++ scripts/scil_denoise_sf.py
@@ -23,7 +23,7 @@ from dipy.data import SPHERE_FILES
 from scilpy.io.utils import (add_overwrite_arg,
                              assert_inputs_exist,
                              add_sh_basis_args,
-                             assert_output_dirs_exist_and_empty)
+                             assert_outputs_exist)
 
 from scilpy.denoise.asym_enhancement import local_asym_gaussian_filtering
 
@@ -37,9 +37,6 @@ def _build_arg_parser():
 
     p.add_argument('out_sh',
                    help='File name for averaged signal.')
-
-    p.add_argument('--out_dir', default='results',
-                   help='Output directory. Default is current directory.')
 
     p.add_argument('--out_mask', default='mask.nii.gz',
                    help='File name for output mask. [%(default)s]')
@@ -83,12 +80,12 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     # Checking args
-    assert_output_dirs_exist_and_empty(parser, args, args.out_dir)
+    assert_outputs_exist(parser, args, [args.out_sh, args.out_mask])
     assert_inputs_exist(parser, args.in_sh)
 
     # Prepare data
     sh_img = nib.load(args.in_sh)
-    data = sh_img.get_fdata(dtype=np.float)
+    data = sh_img.get_fdata(dtype=np.float64)
 
     logging.info('Executing locally asymmetric Gaussian filtering.')
     filtered_sh = local_asym_gaussian_filtering(
@@ -99,13 +96,12 @@ def main():
         dot_sharpness=args.sharpness,
         sigma=args.sigma)
 
-    out_sh = os.path.join(args.out_dir, args.out_sh)
-    nib.save(nib.Nifti1Image(filtered_sh, sh_img.affine), out_sh)
+    nib.save(nib.Nifti1Image(filtered_sh, sh_img.affine), args.out_sh)
 
     # Generate mask by applying threshold on input SH amplitude
     mask = generate_mask(data, args.mask_eps)
-    out_mask = os.path.join(args.out_dir, args.out_mask)
-    nib.save(nib.Nifti1Image(mask.astype(np.uint8), sh_img.affine), out_mask)
+    nib.save(nib.Nifti1Image(mask.astype(np.uint8), sh_img.affine),
+             args.out_mask)
 
 
 if __name__ == "__main__":
