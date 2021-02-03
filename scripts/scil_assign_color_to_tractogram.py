1c9c0ba825dda9dd433d6711ed8ddeed00d7b387
frheault <francois.m.rheault@usherbrooke.ca>
2021-02-03 12:14:10 -0600

    New output for bundle_mean_std and coloring

--- scripts/scil_assign_color_to_tractogram.py
+++ scripts/scil_assign_color_to_tractogram.py
@@ -19,6 +19,7 @@ import os
 from dipy.io.streamline import save_tractogram
 import json
 import numpy as np
+import matplotlib.pyplot as plt
 
 from scilpy.io.streamlines import load_tractogram_with_reference
 from scilpy.io.utils import (assert_inputs_exist,
@@ -43,6 +44,17 @@ def _build_arg_parser():
     p1.add_argument('--dict_colors',
                     help='Dictionnary mapping basename to color.\n'
                          'Same convention as --fill_color.')
+    p1.add_argument('--use_dps', metavar='DPS_KEY',
+                    help='Use the data_per_streamline (scalar) for coloring, '
+                         'linear from min to max.')
+    p1.add_argument('--use_dpp', metavar='DPP_KEY',
+                    help='Use the data_per_point (scalar) for coloring, '
+                         'linear from min to max.')
+    p.add_argument('--colormap', default='jet',
+                   help='Select the colormap for colored trk (dps/dpp) '
+                        '[%(default)s].')
+    p.add_argument('--log', action='store_true',
+                   help='Apply a base 10 logarithm for colored trk (dps/dpp).')
 
     g2 = p.add_argument_group(title='Output options')
     p2 = g2.add_mutually_exclusive_group()
@@ -81,6 +93,8 @@ def main():
         out_filenames.append('{}{}{}'.format(base, args.out_suffix, '.trk'))
     assert_outputs_exist(parser, args, out_filenames)
 
+    sft = load_tractogram_with_reference(parser, args, filename)
+    cmap = plt.get_cmap(args.colormap)
     for i, filename in enumerate(args.in_tractograms):
         base, ext = os.path.splitext(filename)
         out_filename = out_filenames[i]
@@ -96,25 +110,52 @@ def main():
                     color = dict_colors[key]
         elif args.fill_color is not None:
             color = args.fill_color
+        elif args.use_dps:
+            data = np.squeeze(sft.data_per_streamline[args.use_dps])
+            # I believe it works well for gaussian distribution, but
+            # COMMIT has very weird outliers values
+            if args.use_dps == 'commit_weights':
+                data = np.clip(data, np.quantile(data, 0.05),
+                               np.quantile(data, 0.95))
+            if args.log:
+                data[data > 0] = np.log10(data[data > 0])
+            data -= np.min(data)
+            data = data / np.max(data) if np.max(data) > 0 else data
+            color = cmap(data)[:, 0:3] * 255
+        elif args.use_dpp:
+            data = np.squeeze(sft.data_per_point[args.use_dpp]._data)
+            if args.log:
+                data[data > 0] = np.log10(data[data > 0])
+            data += np.min(data)
+            data = data / np.max(data) if np.max(data) > 0 else data
+            color = cmap(data)[:, 0:3] * 255
+
         if color is None:
             color = '0x000000'
 
-        if len(color) == 7:
-            args.fill_color = '0x' + args.fill_color.lstrip('#')
+        if isinstance(color, str):
+            if len(color) == 7:
+                args.fill_color = '0x' + args.fill_color.lstrip('#')
 
-        if len(color) == 8:
-            color_int = int(color, 0)
-            red = color_int >> 16
-            green = (color_int & 0x00FF00) >> 8
-            blue = color_int & 0x0000FF
-        else:
-            parser.error('Hexadecimal RGB color should be formatted as '
-                         '"#RRGGBB" or 0xRRGGBB.')
-
-        sft = load_tractogram_with_reference(parser, args, filename)
-        tmp = [np.tile([red, green, blue],
-                       (len(i), 1)) for i in sft.streamlines]
-        sft.data_per_point["color"] = tmp
+            if len(color) == 8:
+                color_int = int(color, 0)
+                red = color_int >> 16
+                green = (color_int & 0x00FF00) >> 8
+                blue = color_int & 0x0000FF
+            else:
+                parser.error('Hexadecimal RGB color should be formatted as '
+                             '"#RRGGBB" or 0xRRGGBB.')
+            tmp = [np.tile([red, green, blue], (len(i), 1))
+                   for i in sft.streamlines]
+            sft.data_per_point['color'] = tmp
+        elif len(color) == len(sft):
+            tmp = [np.tile([color[i][0], color[i][1], color[i][2]],
+                           (len(sft.streamlines[i]), 1))
+                   for i in range(len(sft.streamlines))]
+            sft.data_per_point['color'] = tmp
+        elif len(color) == len(sft.streamlines._data):
+            sft.data_per_point['color'] = sft.streamlines
+            sft.data_per_point['color']._data = color
         save_tractogram(sft, out_filename)
 
 
