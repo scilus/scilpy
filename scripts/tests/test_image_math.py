072dc4b1dca379b7deb4850db59acea531a727bf
durm2107 <michael.durand-chorel@usherbrooke.ca>
2021-02-05 13:32:58 -0500

    Added a test for testing concatenation of a 4D image

--- scripts/tests/test_image_math.py
+++ scripts/tests/test_image_math.py
@@ -57,3 +57,15 @@ def test_execution_concat(script_runner):
                             in_img_1, in_img_2, in_img_3, in_img_4, in_img_5,
                             in_img_6, 'concat_ids.nii.gz')
     assert ret.success
+
+
+def test_execution_concat_4D(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_img_1 = os.path.join(get_home(), 'atlas', 'ids', '10.nii.gz')
+    in_img_2 = os.path.join(get_home(), 'atlas', 'ids', '8_10.nii.gz')
+    in_img_3 = os.path.join(get_home(), 'atlas', 'ids', '12.nii.gz')
+    in_img_4 = os.path.join(get_home(), 'atlas', 'ids', '8_10.nii.gz')
+    ret = script_runner.run('scil_image_math.py', 'concatenate',
+                            in_img_1, in_img_2, in_img_3, in_img_4,
+                            'concat_ids.nii.gz')
+    assert ret.success
