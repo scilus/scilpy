eeb88c23e837bbbae04cdbe84df38f7e25173c18
Arnaud Bore <arnaud.bore@gmail.com>
2021-02-10 10:02:43 -0500

    comment test that uses vtl

--- scripts/tests/test_visualize_bundles_mosaic.py
+++ scripts/tests/test_visualize_bundles_mosaic.py
@@ -16,15 +16,15 @@ def test_help_option(script_runner):
     ret = script_runner.run('scil_visualize_bundles_mosaic.py', '--help')
     assert ret.success
 
+# Tests including VTK do not work on a server without a display
+# def test_image_create(script_runner):
+#     os.chdir(os.path.expanduser(tmp_dir.name))
+#     in_vol = os.path.join(
+#         get_home(), 'bundles', 'fibercup_atlas', 'bundle_all_1mm.nii.gz')
 
-def test_image_create(script_runner):
-    os.chdir(os.path.expanduser(tmp_dir.name))
-    in_vol = os.path.join(
-        get_home(), 'bundles', 'fibercup_atlas', 'bundle_all_1mm.nii.gz')
+#     in_bundle = os.path.join(
+#         get_home(), 'bundles', 'fibercup_atlas', 'subj_1', 'bundle_0.trk')
 
-    in_bundle = os.path.join(
-        get_home(), 'bundles', 'fibercup_atlas', 'subj_1', 'bundle_0.trk')
-
-    ret = script_runner.run('scil_visualize_bundles_mosaic.py',
-                            in_vol, in_bundle, 'out.png')
-    assert ret.success
+#     ret = script_runner.run('scil_visualize_bundles_mosaic.py',
+#                             in_vol, in_bundle, 'out.png')
+#     assert ret.success
