728579af6cc0549ebdc44a865954346286068f50
Arnaud Bore <arnaud.bore@gmail.com>
2021-02-10 10:14:01 -0500

    comment fetch

--- scripts/tests/test_visualize_bundles_mosaic.py
+++ scripts/tests/test_visualize_bundles_mosaic.py
@@ -4,7 +4,7 @@
 import os
 import tempfile
 
-from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
+# from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
 
 
 # If they already exist, this only takes 5 seconds (check md5sum)
