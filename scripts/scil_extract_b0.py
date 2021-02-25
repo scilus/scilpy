2bbd32fe1e5e5989ea67e003127316e604cca559
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2021-02-25 01:04:06 -0500

    Extract b0 volumes from image, either : all the b0s, the mean of b0s or the first. Works also on clusters of b0s : can extract the first b0 of each cluster or the mean of each cluster.

--- scripts/scil_extract_b0.py
+++ scripts/scil_extract_b0.py
@@ -19,7 +19,8 @@ import numpy as np
 
 from scilpy.io.utils import (add_verbose_arg, assert_inputs_exist,
                              add_force_b0_arg)
-from scilpy.utils.bvec_bval_tools import check_b0_threshold
+from scilpy.utils.bvec_bval_tools import (check_b0_threshold, extract_b0,
+                                          B0ExtractionStrategy)
 from scilpy.utils.filenames import split_name_with_nii
 
 logger = logging.getLogger(__file__)
@@ -47,38 +48,36 @@ def _build_arg_parser():
                             'the output file.')
     group.add_argument('--mean', action='store_true', help='Extract mean b0.')
 
+    p.add_argument('--cluster', action='store_true',
+                   help='Apply the extraction strategy to clusters of '
+                        'continuous b0 volumes in the data')
+
+    p.add_argument('--block-size', '-s',
+                   metavar='INT', type=int,
+                   help='Loads the data using this block size. '
+                        'Useful\nwhen the data is too large to be '
+                        'loaded in memory.')
+
+    p.add_argument('--single-image', action='store_true',
+                   help='If output b0 volume has multiple time points, only '
+                        'outputs a single image instead of a numbered series '
+                        'of images.')
+
     add_force_b0_arg(p)
     add_verbose_arg(p)
 
     return p
 
 
-def _keep_time_step(dwi, time, output):
-    image = nib.load(dwi)
-    data = image.get_fdata(dtype=np.float32)
-
+def _split_time_steps(b0, affine, header, output):
     fname, fext = split_name_with_nii(os.path.basename(output))
 
-    multi_b0 = len(time) > 1
-    for t in time:
-        t_data = data[..., t]
-
+    multiple_b0 = b0.shape[-1] > 1
+    for t in range(b0.shape[-1]):
         out_name = os.path.join(
             os.path.dirname(os.path.abspath(output)),
-            '{}_{}{}'.format(fname, t, fext)) if multi_b0 else output
-        nib.save(nib.Nifti1Image(t_data, image.affine, image.header),
-                 out_name)
-
-
-def _mean_in_time(dwi, time, output):
-    image = nib.load(dwi)
-
-    data = image.get_fdata(dtype=np.float32)
-    data = data[..., time]
-    data = np.mean(data, axis=3, dtype=data.dtype)
-
-    nib.save(nib.Nifti1Image(data, image.affine, image.header),
-             output)
+            '{}_{}{}'.format(fname, t, fext)) if multiple_b0 else output
+        nib.save(nib.Nifti1Image(b0[..., t], affine, header), out_name)
 
 
 def main():
@@ -103,14 +102,22 @@ def main():
 
     logger.info('Number of b0 images in the data: {}'.format(len(b0_idx)))
 
+    strategy = B0ExtractionStrategy.FIRST
     if args.mean:
-        logger.info('Using mean of indices {} for b0'.format(b0_idx))
-        _mean_in_time(args.in_dwi, b0_idx, args.out_b0)
+        strategy = B0ExtractionStrategy.MEAN
+    elif args.all:
+        strategy = B0ExtractionStrategy.ALL
+
+    image = nib.load(args.in_dwi)
+
+    b0_volumes = extract_b0(
+        image, gtab.b0s_mask, args.cluster, strategy, args.block_size)
+
+    if len(b0_volumes.shape) > 3 and not args.single_image:
+        _split_time_steps(b0_volumes, image.affine, image.header, args.out_b0)
     else:
-        if not args.all:
-            b0_idx = [b0_idx[0]]
-        logger.info("Keeping {} for b0".format(b0_idx))
-        _keep_time_step(args.in_dwi, b0_idx, args.out_b0)
+        nib.save(nib.Nifti1Image(b0_volumes, image.affine, image.header),
+                 args.out_b0)
 
 
 if __name__ == '__main__':
