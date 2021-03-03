11484180c95d0c818af90956d7a954eaf8fefbf1
yuzheww <yuzhe.wang@vanderbilt.edu>
2021-03-03 13:45:03 +0800

    add span, curl, diam, elon, set stats

--- scripts/scil_evaluate_bundles_individual_measures.py
+++ scripts/scil_evaluate_bundles_individual_measures.py
@@ -4,7 +4,6 @@
 """
 Evaluate basic measurements of bundles, all at once.
 All tractograms must be in the same space (aligned to one reference)
-
 The computed measures are:
 volume, volume_endpoints, streamlines_count, avg_length, std_length,
 min_length, max_length, mean_curvature
@@ -57,10 +56,12 @@ def compute_measures(filename_tuple):
     nbr_streamlines = len(sft)
     if not nbr_streamlines:
         logging.warning('{} is empty'.format(filename_tuple[0]))
-        return dict(zip(['volume', 'volume_endpoints', 'streamlines_count',
+        return dict(zip(['volume', 'volume_endpoints', 'streamlines_count', 'total_length',
                          'avg_length', 'std_length', 'min_length', 'max_length',
-                         'mean_curvature'], [0, 0, 0, 0, 0, 0, 0, 0]))
+                         'span', 'curl', 'diameter', 'elongation', 'mean_curvature'],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
 
+    # list of length of all streamlines
     length_list = list(length(list(sft.streamlines)))
     length_avg = float(np.average(length_list))
     length_std = float(np.std(length_list))
@@ -73,18 +74,35 @@ def compute_measures(filename_tuple):
     density = compute_tract_counts_map(streamlines, dimensions)
     endpoints_density = get_endpoints_density_map(streamlines, dimensions)
 
+    # add new stats
+    # sum of length of all streamlines
+    length_sum = float(np.sum(length_list))
+    span_list = list(map(compute_span, list(sft.streamlines)))
+    span = float(np.average(span_list))
+    curl = length_avg / span
+    volume = np.count_nonzero(density) * np.product(voxel_size)
+    diameter = 2 * np.sqrt(volume / (np.pi * length_avg))
+    elon = length_avg / diameter
+
     curvature_list = np.zeros((nbr_streamlines,))
     for i in range(nbr_streamlines):
         curvature_list[i] = mean_curvature(sft.streamlines[i])
 
-    return dict(zip(['volume', 'volume_endpoints', 'streamlines_count',
+    # add one entry to dict
+    return dict(zip(['volume', 'volume_endpoints', 'streamlines_count', 'total_length',
                      'avg_length', 'std_length', 'min_length', 'max_length',
-                     'mean_curvature'],
-                    [np.count_nonzero(density)*np.product(voxel_size),
-                     np.count_nonzero(endpoints_density) *
-                     np.product(voxel_size),
-                     nbr_streamlines, length_avg, length_std, length_min,
-                     length_max, float(np.mean(curvature_list))]))
+                     'span', 'curl', 'diameter', 'elongation', 'mean_curvature'],
+                    [volume, np.count_nonzero(endpoints_density) * np.product(voxel_size),
+                     nbr_streamlines, length_sum, length_avg, length_std, length_min,
+                     length_max, span, curl, diameter, elon, float(np.mean(curvature_list)) ]))
+
+
+def compute_span(streamline_cords):
+    xyz = np.asarray(streamline_cords)
+    if xyz.shape[0] < 2:
+        return 0
+    dists = np.sqrt((np.diff([xyz[0], xyz[-1]], axis=0)**2).sum(axis=1))
+    return np.sum(dists)
 
 
 def main():
@@ -118,7 +136,21 @@ def main():
                     output_measures_dict[measure_name] = []
                 output_measures_dict[measure_name].append(
                     measure_dict[measure_name])
-
+    if len(bundles_references_tuple_extended) > 1:
+        # calculate total length of all input bundles
+        set_total_length = np.sum(output_measures_dict['total_length'])
+        # calculate total streamlines of all input bundles
+        set_streamlines_count = np.sum(output_measures_dict['streamlines_count'])
+        # calculate avg length of all input bundles
+        set_avg_length = set_total_length / set_streamlines_count
+        # add a map to the output
+        output_measures_dict['set_stats'] = {}
+        output_measures_dict['set_stats']['set_total_length'] = set_total_length
+        output_measures_dict['set_stats']['set_streamlines_count'] = float(set_streamlines_count)
+        output_measures_dict['set_stats']['set_avg_length'] = set_avg_length
+        # output_measures_dict['set_avg_length'] = []
+        # output_measures_dict['set_avg_length'].append(set_avg_length)
+    # if len(bundles_references_tuple_extended) > 1:
     with open(args.out_json, 'w') as outfile:
         json.dump(output_measures_dict, outfile,
                   indent=args.indent, sort_keys=args.sort_keys)
