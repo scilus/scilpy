e34125b14abc3869203f6f8ce601efc07f9acfad
hermela11 <hermela.g.gebremariam@vanderbilt.edu>
2021-03-18 14:14:58 -0500

    ssd and corrcoef

--- scripts/scil_evaluate_connectivity_pairwise_agreement_measures.py
+++ scripts/scil_evaluate_connectivity_pairwise_agreement_measures.py
@@ -2,17 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Evaluate pair-wise similarity measures of bundles.
-All tractograms must be in the same space (aligned to one reference)
+Evaluate pair-wise similarity measures of connectivity matrix.
 
-For the voxel representation the computed similarity measures are:
-bundle_adjacency_voxels, dice_voxels, w_dice_voxels, density_correlation
-volume_overlap, volume_overreach
-The same measures are also evluated for the endpoints.
-
-For the streamline representation the computed similarity measures are:
-bundle_adjacency_streamlines, dice_streamlines, streamlines_count_overlap,
-streamlines_count_overreach
+The computed similarity measures are:
+sum of square difference and pearson correlation coefficent
 """
 
 import argparse
@@ -30,6 +23,7 @@ from scilpy.io.utils import (add_json_args,
                              assert_outputs_exist,
                              load_matrix_in_any_format)
 from scilpy.tractanalysis.reproducibility_measures import compute_dice_voxel
+
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
@@ -55,25 +49,19 @@ def main():
     assert_inputs_exist(parser, args.in_matrices)
     assert_outputs_exist(parser, args, [args.out_json])
 
-    # The input can be 2, 10 or 100+ matrices
     all_matrices = []
     for filename in args.in_matrices:
         tmp_mat = load_matrix_in_any_format(filename)
         all_matrices.append(tmp_mat / np.max(tmp_mat))
 
-    # SSD of first two as an example
-    ssd = np.sum((all_matrices[0] - all_matrices[1])**2)
-    output_measures_dict = {'SSD': [ssd]}
+    output_measures_dict = {'SSD': [], 'Correlation': []}
+    pairs = list(itertools.combinations(all_matrices, r=2))
+    for i in pairs:
+        ssd = np.sum((pairs[0] - pairs[1]) ** 2)
+        output_measures_dict['SSD'].append(ssd)
+        corrcoef = np.corrcoef(all_matrices[0], all_matrices[1])  # only calculates for the first two arrays
+        output_measures_dict['Correlation'].append(corrcoef)
 
-    # But for the real script we need ALL the pairs:
-    # 2 input means one pair, 3 input means 2 pairs (1-2, 1-3, 2-3)
-    # N input means Nx(N-1) / 2 pairs
-
-    # Single compare means you compare everyone against a single one.
-    # All that pairing is in the scil_evaluate_bundles_pairwise_agreement_measures.py
-
-    # The output is a dictionnary with keys being the metrics (SSD, Correlation)
-    # The entries are list of results on length NUMBER_OF_PAIRS
     with open(args.out_json, 'w') as outfile:
         json.dump(output_measures_dict, outfile,
                   indent=args.indent, sort_keys=args.sort_keys)
