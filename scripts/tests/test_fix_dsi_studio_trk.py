2cf3f95bcadd64a1a628e6a17cdd9af5e65c0d1d
frheault <francois.m.rheault@usherbrooke.ca>
2021-06-01 14:20:04 -0500

    Add simple test

---
+++ scripts/tests/test_fix_dsi_studio_trk.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import tempfile
+
+from scilpy.io.fetcher import fetch_data, get_home, get_testing_files_dict
+
+# If they already exist, this only takes 5 seconds (check md5sum)
+fetch_data(get_testing_files_dict(), keys=['tracking.zip'])
+tmp_dir = tempfile.TemporaryDirectory()
+
+
+def test_help_option(script_runner):
+    ret = script_runner.run('scil_fix_dsi_studio_trk.py', '--help')
+    assert ret.success
+
