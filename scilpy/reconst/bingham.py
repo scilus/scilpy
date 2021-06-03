d85180612c9831bd5afe6cb6b27e00d3d6c54126
Charles Poirier <charles.poirier@usherbrooke.ca>
2021-06-03 16:18:45 -0400

    Bingham fit peak directions + new metrics

--- scilpy/reconst/bingham.py
+++ scilpy/reconst/bingham.py
@@ -3,8 +3,10 @@
 from math import cos, radians
 import numpy as np
 
+from dipy.direction import peak_directions
 
-class BinghamFunction(object):
+
+class BinghamDistribution(object):
     def __init__(self, f0, mu1, mu2, k1, k2):
         self.f0 = f0  # scaling factor
         self.mu1 = mu1.reshape((1, 3))  # vec3
@@ -13,31 +15,47 @@ class BinghamFunction(object):
         self.k2 = k2  # scalar
 
     def evaluate(self, vertices):
-        if vertices.shape[0] != 3:
-            vertices = vertices.T
-        bu = np.exp(- self.k1 * self.mu1.dot(vertices)**2.
-                    - self.k2 * self.mu2.dot(vertices)**2.)
+        bu = np.exp(- self.k1 * self.mu1.dot(vertices.T)**2.
+                    - self.k2 * self.mu2.dot(vertices.T)**2.)
         bu *= self.f0
 
         return bu.reshape((-1))  # (1, N)
 
+    def peak_direction(self):
+        return np.cross(self.mu1, self.mu2)
 
-def bingham_fit_peak(sf, peak, sphere, max_angle=6.):
-    dot_prod = sphere.vertices.dot(peak)
+
+class MultiPeakBingham(object):
+    def __init__(self):
+        self.lobes = []
+
+    def add_lobe(self, bingham_function):
+        self.lobes.append(bingham_function)
+
+    def evaluate(self, vertices):
+        sf = np.zeros(len(vertices))
+        for lobe in self.lobes:
+            sf += lobe.evaluate(vertices)
+        return sf
+
+
+def bingham_fit_peak(sf, peak, sphere, max_angle=6., verbose=False):
+    # abs for twice the number of pts to fit
+    dot_prod = np.abs(sphere.vertices.dot(peak))
     min_dot = cos(radians(max_angle))
 
     p = sphere.vertices[dot_prod > min_dot].astype('float64')
     v = sf[dot_prod > min_dot].reshape((-1, 1)).astype('float64')  # (N, 1)
     x, y, z = (p[:, 0:1], p[:, 1:2], p[:, 2:])
 
+    # create an orientation matrix to approximate mu0, mu1 and mu2
     T = np.zeros((3, 3), dtype='float64')
-
-    T[0, 0] = np.sum(np.multiply(x**2, v))
-    T[1, 1] = np.sum(np.multiply(y**2, v))
-    T[2, 2] = np.sum(np.multiply(z**2, v))
-    T[1, 0] = np.sum(np.multiply(np.multiply(x, y), v))
-    T[2, 0] = np.sum(np.multiply(np.multiply(x, z), v))
-    T[2, 1] = np.sum(np.multiply(np.multiply(y, z), v))
+    T[0, 0] = np.sum(x**2 * v)
+    T[1, 1] = np.sum(y**2 * v)
+    T[2, 2] = np.sum(z**2 * v)
+    T[1, 0] = np.sum(x * y * v)
+    T[2, 0] = np.sum(x * z * v)
+    T[2, 1] = np.sum(y * z * v)
     T[0, 1] = T[1, 0]
     T[0, 2] = T[2, 0]
     T[1, 2] = T[2, 1]
@@ -47,9 +65,6 @@ def bingham_fit_peak(sf, peak, sphere, max_angle=6.):
     ordered = np.argsort(eval)
     mu1 = evec[:, ordered[1]].reshape((3, 1))
     mu2 = evec[:, ordered[0]].reshape((3, 1))
-
-    # mu1 = np.array([1., 0., 0.]).reshape((3, 1))
-    # mu2 = np.array([0., 1., 0.]).reshape((3, 1))
     f0 = v.max()
 
     A = np.zeros((len(v), 2), dtype=float)  # (N, 2)
@@ -60,7 +75,63 @@ def bingham_fit_peak(sf, peak, sphere, max_angle=6.):
     k = np.abs(np.linalg.inv(A.T.dot(A)).dot(A.T).dot(B))
     k1 = k[0]
     k2 = k[1]
+    if k[0] > k[1]:
+        k1 = k[1]
+        k2 = k[0]
+        mu2 = evec[:, ordered[1]].reshape((3, 1))
+        mu1 = evec[:, ordered[0]].reshape((3, 1))
 
-    print('f0: {0}\nmu1:\n {1}\nmu2:\n {2}\nk1: {3}\nk2: {4}'
-          .format(f0, mu1, mu2, k1, k2))
-    return BinghamFunction(f0, mu1, mu2, k1, k2)
+    if verbose:
+        print('f0: {0}\nmu1:\n {1}\nmu2:\n {2}\nk1: {3}\nk2: {4}'
+              .format(f0, mu1, mu2, k1, k2))
+
+    return BinghamDistribution(f0, mu1, mu2, k1, k2)
+
+
+def bingham_fit_multi_peaks(odf, sphere, max_angle=15.):
+    """
+    Peak extraction followed by Bingham fit for each peak
+    """
+    multi_peaks_bingham = MultiPeakBingham()
+    peaks, _, _ = peak_directions(odf, sphere, relative_peak_threshold=0.,
+                                  min_separation_angle=max_angle)
+
+    for peak in peaks:  # peaks can be fitted in parallel
+        peak_fit = bingham_fit_peak(odf, peak, sphere,
+                                    max_angle=max_angle,
+                                    verbose=False)
+        multi_peaks_bingham.add_lobe(peak_fit)
+
+    return multi_peaks_bingham
+
+
+def compute_fiber_density(bingham_lobe, sphere):
+    """
+    Fiber density (FS) is the integral of the bingham function
+    over the sphere.
+    """
+    delta = []
+    for edge in sphere.edges:
+        delta.append(np.abs(sphere.vertices[edge[0]] -
+                            sphere.vertices[edge[1]]))
+    delta = np.mean(delta)
+    sf = np.ones(len(sphere.vertices))  # bingham_lobe.evaluate(sphere.vertices)
+    # we approximate the area under the area under the curve
+    # with a sum of cone elements of radius delta
+    # TODO: Replace with better approximation or analytical formula
+    fd = np.sum(sf * np.pi * (0.5 * delta)**2 / 3.)
+    return fd
+
+
+def compute_fiber_spread(bingham_lobe, sphere):
+    """
+    Fiber spread (FS) characterizes the spread of the lobe.
+    The higher FS is, the wider the lobe.
+    """
+    fd = compute_fiber_density(bingham_lobe, sphere)
+    afd_max = bingham_lobe.f0
+    return fd / afd_max
+
+
+def compute_complexity(multi_peaks_bingham, sphere):
+    pass
--- scripts/scil_compute_bingham.py
+++ scripts/scil_compute_bingham.py
@@ -5,8 +5,11 @@ import numpy as np
 import argparse
 from fury import window, actor
 from dipy.data import get_sphere
+from dipy.sims.voxel import multi_tensor_odf
 
-from scilpy.reconst.bingham import BinghamFunction, bingham_fit_peak
+from scilpy.reconst.bingham import (compute_fiber_density,
+                                    compute_fiber_spread,
+                                    bingham_fit_multi_peaks)
 
 
 def _build_arg_parser():
@@ -14,35 +17,89 @@ def _build_arg_parser():
     return p
 
 
-def visualize(sf, sphere):
+def visualize(sf, sphere, show_axes=True):
     odf_slicer = actor.odf_slicer(sf[None, None, None, ...],
                                   scale=1., norm=False,
                                   sphere=sphere)
     scene = window.Scene()
     scene.add(odf_slicer)
-    scene.add(actor.axes())
+    if show_axes:
+        scene.add(actor.axes())
 
     window.show(scene)
 
 
+def visualize_overlay(sf, bingham_fit, sphere):
+    scene = window.Scene()
+    sf_slicer = actor.odf_slicer(sf[None, None, None, ...],
+                                 scale=1., norm=False,
+                                 sphere=sphere, opacity=0.4,
+                                 colormap=[255, 255, 255])
+    scene.add(sf_slicer)
+
+    for lobe in bingham_fit.lobes:
+        sf_approx = lobe.evaluate(sphere.vertices)
+        approx_slicer = actor.odf_slicer(sf_approx[None, None, None, ...],
+                                         scale=1., norm=False,
+                                         sphere=sphere, opacity=0.4)
+        scene.add(approx_slicer)
+
+    window.show(scene)
+
+
+def gen_odf(sphere):
+    mevals = np.array(([0.0050, 0.0002, 0.0003],
+                       [0.0015, 0.0003, 0.0003],
+                       [0.0015, 0.0003, 0.0003]))
+    angles = [(0, 0), (60, 0), (60, 60)]
+    odf = multi_tensor_odf(sphere.vertices, mevals, angles, [100, 0, 0])
+    odf /= odf.max()
+    return odf
+
+
+def mean_relative_error(sf, sf_approx):
+    sf[sf < 0.] = 0.
+    sf_approx[sf_approx < 0.] = 0.
+    error = np.abs(sf - sf_approx)
+    error = error[sf > 0.] / sf[sf > 0.]
+    return np.mean(error)
+
+
+def log_mean_rel_error(sf, sf_approx):
+    print('Mean relative error:', mean_relative_error(sf, sf_approx))
+
+
 def main():
     parser = _build_arg_parser()
-    f0 = 1.
-    k1, k2 = (5., 5.)
-    mu1 = np.array([2., 0., 0.])
-    mu2 = np.array([0., 1., 0.])
-
-    bingham = BinghamFunction(f0, mu1, mu2, k1, k2)
     sphere = get_sphere('symmetric724').subdivide(2)
 
-    sf = bingham.evaluate(sphere.vertices)
-    bingham_approx = bingham_fit_peak(sf, np.array([0.0, 0.0, 1.0]), sphere)
-    sf_approx = bingham_approx.evaluate(sphere.vertices)
+    # f0 = 15.
+    # k1, k2 = (5., 25.)
+    # mu1 = np.array([0.5, 0.5, 0.])
+    # mu2 = np.array([0., 1., 0.])
 
-    visualize(sf, sphere)
-    visualize(sf_approx, sphere)
+    # bingham = BinghamFunction(f0, mu1, mu2, k1, k2)
 
-    print('Mean error:', np.mean(np.abs(sf_approx - sf)))
+    # sf = bingham.evaluate(sphere.vertices)
+    # bingham_approx = bingham_fit_peak(sf, np.array([0.0, 0.0, 1.0]),
+    #                                   sphere, max_angle=20)
+    # sf_approx = bingham_approx.evaluate(sphere.vertices)
+
+    # visualize(sf, sphere)
+    # visualize(sf_approx, sphere)
+
+    # log_mean_rel_error(sf, sf_approx)
+
+    odf = gen_odf(sphere)
+
+    bingham_odf_fit = bingham_fit_multi_peaks(odf, sphere)
+    visualize_overlay(odf, bingham_odf_fit, sphere)
+
+    lobe = bingham_odf_fit.lobes[0]
+    fd = compute_fiber_density(lobe, sphere)
+    fs = compute_fiber_spread(lobe, sphere)
+    print('fd: ', fd)
+    print('fs: ', fs)
     return
 
 
