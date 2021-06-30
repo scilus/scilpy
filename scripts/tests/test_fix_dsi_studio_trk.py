d3baa3675f79de14264bc0f7663abe4ef7396886
frheault <francois.m.rheault@usherbrooke.ca>
2021-06-30 08:40:29 -0500

    fetch removed

--- scripts/tests/test_fix_dsi_studio_trk.py
+++ scripts/tests/test_fix_dsi_studio_trk.py
@@ -6,10 +6,6 @@ import tempfile
 
 from scilpy.io.fetcher import fetch_data, get_home, get_testing_files_dict
 
-# If they already exist, this only takes 5 seconds (check md5sum)
-fetch_data(get_testing_files_dict(), keys=['tracking.zip'])
-tmp_dir = tempfile.TemporaryDirectory()
-
 
 def test_help_option(script_runner):
     ret = script_runner.run('scil_fix_dsi_studio_trk.py', '--help')
