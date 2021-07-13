71c1c12614f303f2a85a2f69f18c178dce52a8af
charles.poirier <charles.poirier@usherbrooke.ca>
2021-07-13 16:15:07 -0400

    TEST: Test edge_mode argument

--- scripts/tests/test_execute_asymmetric_filtering.py
+++ scripts/tests/test_execute_asymmetric_filtering.py
@@ -47,3 +47,25 @@ def test_asym_input(script_runner):
     ret = script_runner.run('scil_execute_asymmetric_filtering.py', in_fodf,
                             'out_2.nii.gz', '--sphere', 'repulsion100', '-f')
     assert ret.success
+
+
+def test_edge_mode_wall(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_fodf = os.path.join(get_home(), 'tracking', 'fodf.nii.gz')
+
+    # We use a low resolution sphere to reduce execution time
+    ret = script_runner.run('scil_execute_asymmetric_filtering.py', in_fodf,
+                            'out_3.nii.gz', '--sphere', 'repulsion100',
+                            '--edge_mode', 'wall', '--sh0_th', '0.0', '-f')
+    assert ret.success
+
+
+def test_edge_mode_wall_without_sh0_th(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_fodf = os.path.join(get_home(), 'tracking', 'fodf.nii.gz')
+
+    # We use a low resolution sphere to reduce execution time
+    ret = script_runner.run('scil_execute_asymmetric_filtering.py', in_fodf,
+                            'out_4.nii.gz', '--sphere', 'repulsion100',
+                            '--edge_mode', 'wall', '-f')
+    assert not(ret.success)
