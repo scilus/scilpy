08ec70f0fad2f1cfce12232a426cc7cdd25d439a
frheault <francois.m.rheault@usherbrooke.ca>
2021-08-03 16:17:39 -0500

    Add options to merge groups

--- scripts/scil_combine_labels.py
+++ scripts/scil_combine_labels.py
@@ -48,11 +48,9 @@ def _build_arg_parser():
     o_ids = p.add_mutually_exclusive_group()
     o_ids.add_argument('--out_labels_ids', type=int, nargs='+', default=[],
                        help='Give a list of labels indices for output images.')
-
     o_ids.add_argument('--unique', action='store_true',
                        help='Output id with unique labels,'
                             ' excluding first background value.')
-
     o_ids.add_argument('--group_in_m', action='store_true',
                        help='Add (x*1000000) to each volume labels,'
                             ' where x is the input volume order number.')
@@ -60,6 +58,9 @@ def _build_arg_parser():
     p.add_argument('--background', type=int, default=0,
                    help='Background id, excluded from output [%(default)s],\n'
                         ' the value is used as output background value.')
+    p.add_argument('--merge_groups', action='store_true',
+                   help='Each group for the -v option will be merge as a single'
+                        'labels.')
     add_overwrite_arg(p)
     return p
 
@@ -113,9 +114,13 @@ def main():
     # Prepare output indices
     if args.out_labels_ids:
         out_labels = args.out_labels_ids
-        if len(out_labels) != len(np.hstack(indices_per_volume)):
+        if not args.merge_groups \
+                and len(out_labels) != len(np.hstack(indices_per_volume)):
             logging.error("--out_labels_ids, requires the same amount"
                           " of total given input indices")
+        elif len(out_labels) != len(args.volume_ids):
+            logging.error("--out_labels_ids, requires the same amount"
+                          " of total given groups (to merge)")
     elif args.unique:
         stack = np.hstack(filtered_ids_per_vol)
         ids = np.arange(len(stack) + 1)
@@ -127,7 +132,10 @@ def main():
             m_list.append(prefix + np.asarray(filtered_ids_per_vol[i]))
         out_labels = np.hstack(m_list)
     else:
-        out_labels = np.hstack(filtered_ids_per_vol)
+        if args.merge_groups:
+            out_labels = np.arange(len(args.volume_ids))+1
+        else:
+            out_labels = np.hstack(filtered_ids_per_vol)
 
     if len(np.unique(out_labels)) != len(out_labels):
         logging.error("The same output label number was used "
@@ -138,14 +146,22 @@ def main():
     resulting_labels = (np.ones_like(data_list[0], dtype=np.uint16)
                         * args.background)
     for i in range(len(image_files)):
-        # Add Given labels for each volume
+        # Add given labels for each volume
         for index in filtered_ids_per_vol[i]:
-            mask = data_list[i] == index
-            resulting_labels[mask] = out_labels[current_id]
-            current_id += 1
+            if args.merge_groups:
+                for j, curr_volume_ids in enumerate(args.volume_ids):
+                    if str(index) in curr_volume_ids[1:]:
+                        where_at = j
+                mask = data_list[i] == index
+                resulting_labels[mask] = out_labels[where_at]
+            else:
+                mask = data_list[i] == index
+                resulting_labels[mask] = out_labels[current_id]
+                current_id += 1
 
-            if np.count_nonzero(mask) == 0:
-                logging.warning("Label {} was not in the volume".format(index))
+                if np.count_nonzero(mask) == 0:
+                    logging.warning(
+                        "Label {} was not in the volume".format(index))
 
     # Save final combined volume
     nib.save(nib.Nifti1Image(resulting_labels, first_img.affine,
