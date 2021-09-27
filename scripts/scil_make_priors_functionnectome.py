e1a3db74678ba649fb0e5daa6efa0e49548fc10d
Victor Paul-Émile Nozais <victor.nozais@u-bordeaux.fr>
2021-09-27 11:03:32 -0400

    New algo: iterate over streamlines, much faster

--- scripts/scil_make_priors_functionnectome.py
+++ scripts/scil_make_priors_functionnectome.py
@@ -8,6 +8,9 @@ The brain template defines which voxels are used to generate the probability map
 
 For region-wise priors, the region masks must be provided as separate files,
 with each file named as the region it contains (e.g. 'SupFront_Left.nii.gz')
+
+# TODO Add a 'Resume' option (with numbered tmp files)
+# TODO Improve RAM usage by sharing the tractogram between processes
 """
 
 import nibabel as nib
@@ -16,7 +19,8 @@ import os
 import glob
 import argparse
 import time
-from itertools import compress
+from shutil import copyfile
+# from itertools import compress
 from multiprocessing import Pool
 from scilpy.io.utils import (add_overwrite_arg,
                              assert_inputs_exist,
@@ -59,8 +63,8 @@ def _build_arg_parser():
     return p
 
 
-def save_tmp_map(vol, file, affine, emptyVol=False):
-    if os.path.isfile(file) and not emptyVol:
+def save_tmp_map(vol, file, affine):
+    if os.path.isfile(file):
         vol = vol.astype('float32')
         tmp_i = nib.load(file)
         tmp_vol = tmp_i.get_fdata()
@@ -72,7 +76,10 @@ def save_tmp_map(vol, file, affine, emptyVol=False):
 def voxelize_tractogram(sft):
     '''
     Rounds all the points of the streamlines to the voxel they belong to and
-    removes duplicate points
+    removes duplicate points.
+
+    To avoid missing voxels, the tractogram step-size should be smaller than
+    the voxels. (no auto-correction for that implemented yet)  # TODO
     '''
     sft.to_vox()
     sft.to_corner()
@@ -92,31 +99,11 @@ def tupled_streamlines(sft):
     return [list(zip(st.T[0], st.T[1], st.T[2])) for st in sft.streamlines]
 
 
-def voxeled_visiation_mapping(area_mask, streamlines, out_Ftmp, affine, tupled):
-    ''' sft must be a voxelised sft (cf. voxelize_tractogram)'''
-    # 1
-    ind_vox = np.argwhere(area_mask)
-    if tupled:
-        ind_vox = [tuple(ind) for ind in ind_vox]
-        indstrm = [any(ind in strm for ind in ind_vox) for strm in streamlines]
-        area_strm = list(compress(streamlines, indstrm))
-        # 2
-        vol_strm = np.zeros(area_mask.shape, dtype=np.float32)
-        for strm in area_strm:
-            for v in strm:
-                vol_strm[v] = 1
-    else:
-        indstrm = [any(any(np.equal(vox, strm).all(1)) for vox in ind_vox) for strm in streamlines]
-        area_strm = streamlines[indstrm]
-        # 2
-        vol_strm = np.zeros(area_mask.shape, dtype=np.float32)
-        for strm in area_strm:
-            vol_strm[tuple(strm.T)] = 1
-    # 3
-    save_tmp_map(vol_strm, out_Ftmp, affine)
-
-
 def visitation_mapping(area_mask, sft, out_Ftmp, affine):
+    '''
+    Creates a binary volume of all streamlines passing trhough the ROI/mask
+    and save it
+    '''
     # 1
     area_strm, _ = filter_grid_roi(sft, area_mask, 'any', False)
     # 2
@@ -125,33 +112,9 @@ def visitation_mapping(area_mask, sft, out_Ftmp, affine):
     save_tmp_map(vol_strm, out_Ftmp, affine)
 
 
-def vox_multi(vox_batch, sft, base_im, outdir, voxeled=True, tupled=True):
-    """
-    Iterates over the voxels, creating the appropriate mask and file-name, and
-    runs visitation_mapping()
-
-    """
-    if tupled and not voxeled:
-        raise ValueError('The tractogram is not voxeled, but shoud be for tupling')
-    base_mask = np.zeros(base_im.shape, dtype=bool)
-    if tupled:
-        streamlines = tupled_streamlines(sft)
-    else:
-        streamlines = sft.streamlines
-    for vox in vox_batch:
-        vox_mask = base_mask.copy()
-        vox_mask[tuple(vox)] = True
-        out_name = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox)
-        out_Ftmp = os.path.join(outdir, out_name)
-        if voxeled:
-            voxeled_visiation_mapping(vox_mask, streamlines, out_Ftmp, base_im.affine, tupled)
-        else:
-            visitation_mapping(vox_mask, sft, out_Ftmp, base_im.affine)
-
-
 def tmp2final(tmp_list, nb_trk, outdir, affine):
     for tmp_F in tmp_list:
-        tmp_i = nib.load(tmp_F)
+        tmp_i = nib.load(tmp_F, dtype=np.float32)
         tmp_vol = tmp_i.get_fdata()
         out_vol = tmp_vol/nb_trk
         out_name = os.path.basename(tmp_F).replace('_tmp', '')
@@ -161,61 +124,37 @@ def tmp2final(tmp_list, nb_trk, outdir, affine):
         os.remove(tmp_F)
 
 
-def Parcelate_tractogram(sft, templ_v, nb_parcel=16):
+def loop_on_strm(streamlines, voxel_list, nstart=0):
     '''
-    Divide the input tractogram into sub-tractograms by parcelating the input
-    templ_v into similarly sized sub volumes and filtering the tractogram by
-    these subvolumes. Might not be well adapted for GM masks in input (for
-    those, chose nb_parcel=8).
-    (Different sub-tractograms can share streamlines)
+    streamlines must be from a voxelised sft (cf. voxelize_tractogram) and tupled
+    Returns a list of dict with each voxel a key and each value the index of
+    the streamlines in that voxel
 
-    Parameters
-    ----------
-    sft : Stateful tractogram
-        Tractogram to parcelate.
-    templ_v : int
-        Mask of all the voxels filtering the tractogram.
-    nb_parcel : TYPE
-        Number of sub-tractogram to generate. Chose powers of 2 for even parcels
+    nstart: The index of the first streamline in streamlines (for parallel proc)
 
-    Returns
-    -------
-    list_subTract : list
-        List of the generated sub-tractograms.
-    list_parcels : list
-        List of the masks corresponding to each sub-tractogram
-
+    voxel_list should be tupled
     '''
-    list_parcels = [templ_v]
-    if nb_parcel == 1:
-        list_subTract = [sft]
-    else:
-        dirList = ['x', 'y', 'z']  # direction for the cut (x, y or z)
-        swapCount = 0  # Counts swap in cutting direction
-        for cutCount in range(1, nb_parcel):
-            dirCut = dirList[swapCount]
-            parcelOri = list_parcels.pop(0)  # Takes the current biggest parcel...
-            baryc = np.argwhere(parcelOri).mean(0).round().astype(int)
-            split1 = np.zeros(parcelOri.shape, dtype=bool)
-            split2 = np.zeros(parcelOri.shape, dtype=bool)
-            if dirCut == 'x':
-                split1[:baryc[0], ...] = parcelOri[:baryc[0], ...]
-                split2[baryc[0]:, ...] = parcelOri[baryc[0]:, ...]
-            elif dirCut == 'y':
-                split1[:, :baryc[1], :] = parcelOri[:, :baryc[1], :]
-                split2[:, baryc[1]:, :] = parcelOri[:, baryc[1]:, :]
-            elif dirCut == 'z':
-                split1[..., :baryc[2]] = parcelOri[..., :baryc[2]]
-                split2[..., baryc[2]:] = parcelOri[..., baryc[2]:]
-            list_parcels.extend([split1, split2])  # ... and divide it in 2
-            if (cutCount & (cutCount+1) == 0):
-                # If power of 2 (bit manipulation), change the dierction of the cut
-                swapCount = (swapCount+1) % len(dirList)  # Circular indexing
+    vox_dict = {v: [] for v in voxel_list}
+    for ind, strm in enumerate(streamlines):
+        for vox in strm:
+            try:
+                vox_dict[vox].append(ind+nstart)
+            except KeyError:  # When the streamline voxel is out of the template
+                pass
+    return vox_dict
 
-        list_subTract = []
-        for parcels in list_parcels:
-            list_subTract.append(filter_grid_roi(sft, parcels, 'any', False)[0])
-    return list_subTract, list_parcels
+
+def strm_multi(vdict, stream_tupled, out_dir, templ_i):
+    for vox in vdict:
+        out_name = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox)
+        out_Ftmp = os.path.join(out_dir, out_name)
+        vox_vol = np.zeros(templ_i.shape, dtype=np.float32)
+        for strmInd in vdict[vox]:
+            for v in stream_tupled[strmInd]:
+                vox_vol[v] = 1
+        save_tmp_map(vox_vol, out_Ftmp, templ_i.affine)
+
+# %%
 
 
 def main():
@@ -232,11 +171,10 @@ def main():
     assert_output_dirs_exist_and_empty(parser, args, args.out_dir)
 
     templ_i = nib.load(args.in_template_file)
-    # base_mask = np.zeros(templ_i.shape, dtype=bool)
 
     if args.in_reg_dir is None:
         templ_v = templ_i.get_fdata().astype(bool)
-        # vox_list = np.argwhere(templ_v)
+        max_file_nb = templ_v.sum()
     else:
         list_reg_files = glob.glob(os.path.join(args.in_reg_dir, '*.nii*'))
 
@@ -254,44 +192,53 @@ def main():
         trk_sft = filter_streamlines_by_length(trk_sft, 25, 250)
         trk_sft = hack_invalid_streamlines(trk_sft)
         trk_sft.to_vox()
-        voxeled = True
-        tupled = True
-        if voxeled:
+        if args.in_reg_dir is None:
+            # For voxel-wise priors, "voxelizes" the tractograms
             trk_sft = voxelize_tractogram(trk_sft)
         tractomask = np.where(compute_tract_counts_map(trk_sft.streamlines, templ_i.shape),
                               True, False)
         empty_vol = templ_v > tractomask  # Voxels in templ but not in the tractogram
         templ_v = templ_v*tractomask  # Removing voxels with no streamlines
         if args.in_reg_dir is None:
-            print('Preparing sub-tractograms to speed-up voxel-wise process...')
-            nsub = 16
-            sub_sfts, parcels = Parcelate_tractogram(trk_sft, templ_v, nsub)
-            print(f'{nsub} sub-tractograms generated.')
-            for n, (sub_trk_sft, parcel) in enumerate(zip(sub_sfts, parcels)):
-                print(f'Starting process of sub-tragrogram {n+1}/{nsub}')
-                vox_list = np.argwhere(parcel)
-                if nb_proc == 1:
-                    print('Starting')
-                    vox_multi(vox_list, sub_trk_sft, templ_i, args.out_dir, voxeled, tupled)
-                else:  # Parallelize
-                    vox_batchs = np.array_split(vox_list, nb_proc)
-                    print('Starting parallel process')
-                    with Pool(processes=nb_proc) as pool:
-                        poolCheck = pool.starmap_async(vox_multi, zip(vox_batchs,
-                                                                      [sub_trk_sft]*nb_proc,
-                                                                      [templ_i]*nb_proc,
-                                                                      [args.out_dir]*nb_proc,
-                                                                      [voxeled]*nb_proc,
-                                                                      [tupled]*nb_proc))
-                        while not poolCheck.ready():
-                            time.sleep(1)
+            voxel_list = np.argwhere(templ_v)
+            voxel_list = [tuple(ind) for ind in voxel_list]
+            stream_tupled = tupled_streamlines(trk_sft)
+            vox_dict = loop_on_strm(stream_tupled, voxel_list)
+            if nb_proc == 1:
+                print('Starting process')
+                strm_multi(vox_dict, stream_tupled, args.out_dir, templ_i)
+            else:  # Parallelize
+                vox_batchs = np.array_split(list(vox_dict), nb_proc)
+                vox_dict_batchs = [{tuple(v): vox_dict[tuple(v)] for v in vb} for vb in vox_batchs]
+                print('Starting parallel process')
+                with Pool(processes=nb_proc) as pool:
+                    poolCheck = pool.starmap_async(strm_multi, zip(vox_dict_batchs,
+                                                                   [stream_tupled]*nb_proc,
+                                                                   [args.out_dir]*nb_proc,
+                                                                   [templ_i]*nb_proc))
+                    while not poolCheck.ready():
+                        time.sleep(1)
+
             print('"Saving" maps of voxels with no tract')
-            vox_list = np.argwhere(empty_vol)
-            vox_vol = np.zeros(templ_i.shape, dtype=bool)
-            for vox in vox_list:
-                out_name = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox)
-                out_Ftmp = os.path.join(args.out_dir, out_name)
-                save_tmp_map(vox_vol, out_Ftmp, templ_i.affine, True)
+            vox_list0 = np.argwhere(empty_vol)
+            nb_of_files = len(os.listdir(args.out_dir))
+            if len(vox_list0) and nb_of_files < max_file_nb:
+                vox_vol0 = np.zeros(templ_i.shape, dtype=bool)
+                n0 = 0
+                vox0 = vox_list0[n0]
+                out_name0 = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox0)
+                out_Ftmp0 = os.path.join(args.out_dir, out_name0)
+                while os.path.isfile(out_Ftmp0) and n0 < len(vox_list0):
+                    n0 += 1
+                    vox0 = vox_list0[n0]
+                    out_name0 = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox0)
+                    out_Ftmp0 = os.path.join(args.out_dir, out_name0)
+                save_tmp_map(vox_vol0, out_Ftmp0, templ_i.affine)
+                for vox in vox_list0[n0:]:
+                    out_name = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox)
+                    out_Ftmp = os.path.join(args.out_dir, out_name)
+                    if not os.path.isfile(out_Ftmp):
+                        copyfile(out_Ftmp0, out_Ftmp)
 
         else:  # Regionwise priors
             for reg_F in list_reg_files:
@@ -311,13 +258,120 @@ def main():
     else:  # Parallel proc
         tmpF_batch = np.array_split(tmp_list, nb_proc)
         with Pool(processes=nb_proc) as pool:
-            pool.starmap(tmp2final, zip(tmpF_batch,
-                                        [len(trk_list)]*nb_proc,
-                                        [args.out_dir]*nb_proc,
-                                        [templ_i.affine]*nb_proc,))
+            pool.starmap_async(tmp2final, zip(tmpF_batch,
+                                              [len(trk_list)]*nb_proc,
+                                              [args.out_dir]*nb_proc,
+                                              [templ_i.affine]*nb_proc,))
     print('Done. Process over.')
     print(f'Total elapsed time: {time.time()-t0} sec')
 
 
 if __name__ == "__main__":
     main()
+
+# %% Old functions not usefull anymore
+
+
+# def Parcelate_tractogram(sft, templ_v, nb_parcel=16):
+#     '''
+#     Divide the input tractogram into sub-tractograms by parcelating the input
+#     templ_v into similarly sized sub volumes and filtering the tractogram by
+#     these subvolumes. Might not be well adapted for GM masks in input (for
+#     those, chose nb_parcel=8).
+#     (Different sub-tractograms can share streamlines)
+
+#     Parameters
+#     ----------
+#     sft : Stateful tractogram
+#         Tractogram to parcelate.
+#     templ_v : int
+#         Mask of all the voxels filtering the tractogram.
+#     nb_parcel : TYPE
+#         Number of sub-tractogram to generate. Chose powers of 2 for even parcels
+
+#     Returns
+#     -------
+#     list_subTract : list
+#         List of the generated sub-tractograms.
+#     list_parcels : list
+#         List of the masks corresponding to each sub-tractogram
+
+#     '''
+#     list_parcels = [templ_v]
+#     if nb_parcel == 1:
+#         list_subTract = [sft]
+#     else:
+#         dirList = ['x', 'y', 'z']  # direction for the cut (x, y or z)
+#         swapCount = 0  # Counts swap in cutting direction
+#         for cutCount in range(1, nb_parcel):
+#             dirCut = dirList[swapCount]
+#             parcelOri = list_parcels.pop(0)  # Takes the current biggest parcel...
+#             baryc = np.argwhere(parcelOri).mean(0).round().astype(int)
+#             split1 = np.zeros(parcelOri.shape, dtype=bool)
+#             split2 = np.zeros(parcelOri.shape, dtype=bool)
+#             if dirCut == 'x':
+#                 split1[:baryc[0], ...] = parcelOri[:baryc[0], ...]
+#                 split2[baryc[0]:, ...] = parcelOri[baryc[0]:, ...]
+#             elif dirCut == 'y':
+#                 split1[:, :baryc[1], :] = parcelOri[:, :baryc[1], :]
+#                 split2[:, baryc[1]:, :] = parcelOri[:, baryc[1]:, :]
+#             elif dirCut == 'z':
+#                 split1[..., :baryc[2]] = parcelOri[..., :baryc[2]]
+#                 split2[..., baryc[2]:] = parcelOri[..., baryc[2]:]
+#             list_parcels.extend([split1, split2])  # ... and divide it in 2
+#             if (cutCount & (cutCount+1) == 0):
+#                 # If power of 2 (bit manipulation), change the dierction of the cut
+#                 swapCount = (swapCount+1) % len(dirList)  # Circular indexing
+
+#         list_subTract = []
+#         for parcels in list_parcels:
+#             list_subTract.append(filter_grid_roi(sft, parcels, 'any', False)[0])
+#     return list_subTract, list_parcels
+
+
+# def voxeled_visitation_mapping(area_mask, streamlines, out_Ftmp, affine, tupled):
+#     ''' sft must be a voxelised sft (cf. voxelize_tractogram)'''
+#     # 1
+#     ind_vox = np.argwhere(area_mask)
+#     if tupled:
+#         ind_vox = [tuple(ind) for ind in ind_vox]
+#         indstrm = [any(ind in strm for ind in ind_vox) for strm in streamlines]
+#         area_strm = list(compress(streamlines, indstrm))
+#         # 2
+#         vol_strm = np.zeros(area_mask.shape, dtype=np.float32)
+#         for strm in area_strm:
+#             for v in strm:
+#                 vol_strm[v] = 1
+#     else:
+#         indstrm = [any(any(np.equal(vox, strm).all(1)) for vox in ind_vox) for strm in streamlines]
+#         area_strm = streamlines[indstrm]
+#         # 2
+#         vol_strm = np.zeros(area_mask.shape, dtype=np.float32)
+#         for strm in area_strm:
+#             vol_strm[tuple(strm.T)] = 1
+#     # 3
+#     save_tmp_map(vol_strm, out_Ftmp, affine)
+
+
+# def vox_multi(vox_batch, sft, base_im, outdir, voxeled=True, tupled=True):
+#     """
+#     Iterates over the voxels, creating the appropriate mask and file-name, and
+#     runs visitation_mapping()
+
+#     """
+#     if tupled and not voxeled:
+#         raise ValueError('The tractogram is not voxeled, but shoud be for tupling')
+#     base_mask = np.zeros(base_im.shape, dtype=bool)
+#     if tupled:
+#         streamlines = tupled_streamlines(sft)
+#     else:
+#         streamlines = sft.streamlines
+#     for vox in vox_batch:
+#         vox_mask = base_mask.copy()
+#         vox_mask[tuple(vox)] = True
+#         out_name = 'probaMap_{:02d}_{:02d}_{:02d}_tmp.nii.gz'.format(*vox)
+#         out_Ftmp = os.path.join(outdir, out_name)
+#         if voxeled:
+#             voxeled_visitation_mapping(vox_mask, streamlines, out_Ftmp, base_im.affine, tupled)
+#         else:
+#             visitation_mapping(vox_mask, sft, out_Ftmp, base_im.affine)
