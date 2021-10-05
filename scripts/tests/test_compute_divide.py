6f83be7fd1c6baec3152719b45a9cc88d4ed97c3
Philippe Karan <karp2601@dinf-0051-05b.DInf.fsci.usherbrooke.ca>
2021-10-05 13:46:20 -0400

    Updating compute_divide test

--- scripts/tests/test_compute_divide.py
+++ scripts/tests/test_compute_divide.py
@@ -6,7 +6,7 @@ import tempfile
 
 from scilpy.io.fetcher import fetch_data, get_home, get_testing_files_dict
 
-fetch_data(get_testing_files_dict(), keys=['commit_amico.zip'])
+fetch_data(get_testing_files_dict(), keys=['btensor_testdata.zip'])
 tmp_dir = tempfile.TemporaryDirectory()
 
 
@@ -15,23 +15,105 @@ def test_help_option(script_runner):
     assert ret.success
 
 
-def test_execution_processing(script_runner):
+def test_nb_btensors_check(script_runner):
     os.chdir(os.path.expanduser(tmp_dir.name))
-    in_dwi = os.path.join(get_home(), 'commit_amico',
-                          'dwi.nii.gz')
-    in_bval = os.path.join(get_home(), 'commit_amico',
-                           'dwi.bval')
-    in_bvec = os.path.join(get_home(), 'commit_amico',
-                           'dwi.bvec')
-    mask = os.path.join(get_home(), 'commit_amico',
-                        'mask.nii.gz')
-    fa = os.path.join(get_home(), 'commit_amico',
+    in_dwi_lin = os.path.join(get_home(), 'btensor',
+                          'dwi_linear.nii.gz')
+    in_bval_lin = os.path.join(get_home(), 'btensor',
+                           'linear.bvals')
+    in_bvec_lin = os.path.join(get_home(), 'btensor',
+                           'linear.bvecs')
+    in_dwi_plan = os.path.join(get_home(), 'btensor',
+                          'dwi_planar.nii.gz')
+    in_bval_plan = os.path.join(get_home(), 'btensor',
+                           'planar.bvals')
+    in_bvec_plan = os.path.join(get_home(), 'btensor',
+                           'planar.bvecs')
+    fa = os.path.join(get_home(), 'btensor',
                       'fa.nii.gz')
 
-    ret = script_runner.run('scil_compute_divide.py', '--in_dwi_linear',
-                            in_dwi, '--in_bval_linear', in_bval,
-                            '--in_bvec_linear', in_bvec,
-                            '--mask', mask, '--fa', fa, '--do_weight_bvals',
+    ret = script_runner.run('scil_compute_divide.py', '--in_dwis',
+                            in_dwi_lin, '--in_bvals', in_bval_lin,
+                            '--in_bvecs', in_bvec_lin, '--in_bdeltas', '1',
+                            '--fa', fa, '--do_weight_bvals',
                             '--do_weight_pa', '--do_multiple_s0',
                             '--redo_weight_bvals', '--processes', '1', '-f')
-    assert ret.success
+    assert (not ret.success)
+
+    ret = script_runner.run('scil_compute_divide.py', '--in_dwis',
+                            in_dwi_lin, in_dwi_lin, '--in_bvals', in_bval_lin,
+                            in_bval_lin, '--in_bvecs', in_bvec_lin,
+                            in_bvec_lin, '--in_bdeltas', '1', '1',
+                            '--fa', fa, '--do_weight_bvals',
+                            '--do_weight_pa', '--do_multiple_s0',
+                            '--redo_weight_bvals', '--processes', '1', '-f')
+    assert (not ret.success)
+
+
+def test_inputs_check(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_dwi_lin = os.path.join(get_home(), 'btensor',
+                          'dwi_linear.nii.gz')
+    in_bval_lin = os.path.join(get_home(), 'btensor',
+                           'linear.bvals')
+    in_bvec_lin = os.path.join(get_home(), 'btensor',
+                           'linear.bvecs')
+    in_dwi_plan = os.path.join(get_home(), 'btensor',
+                          'dwi_planar.nii.gz')
+    in_bval_plan = os.path.join(get_home(), 'btensor',
+                           'planar.bvals')
+    in_bvec_plan = os.path.join(get_home(), 'btensor',
+                           'planar.bvecs')
+    fa = os.path.join(get_home(), 'btensor',
+                      'fa.nii.gz')
+
+    ret = script_runner.run('scil_compute_divide.py', '--in_dwis',
+                            in_dwi_lin, in_dwi_plan, '--in_bvals', in_bval_lin,
+                            '--in_bvecs', in_bvec_lin, '--in_bdeltas', '1',
+                            '--fa', fa, '--do_weight_bvals',
+                            '--do_weight_pa', '--do_multiple_s0',
+                            '--redo_weight_bvals', '--processes', '1', '-f')
+    assert (not ret.success)
+
+    ret = script_runner.run('scil_compute_divide.py', '--in_dwis',
+                            in_dwi_lin, in_dwi_plan, '--in_bvals',
+                            in_bval_lin, in_bval_plan, '--in_bvecs',
+                            in_bvec_lin, in_bvec_plan, '--in_bdeltas', '1',
+                            '-0.5', '0', '--fa', fa, '--do_weight_bvals',
+                            '--do_weight_pa', '--do_multiple_s0',
+                            '--redo_weight_bvals', '--processes', '1', '-f')
+    assert (not ret.success)
+
+
+def test_execution_processing(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_dwi_lin = os.path.join(get_home(), 'btensor',
+                          'dwi_linear.nii.gz')
+    in_bval_lin = os.path.join(get_home(), 'btensor',
+                           'linear.bvals')
+    in_bvec_lin = os.path.join(get_home(), 'btensor',
+                           'linear.bvecs')
+    in_dwi_plan = os.path.join(get_home(), 'btensor',
+                          'dwi_planar.nii.gz')
+    in_bval_plan = os.path.join(get_home(), 'btensor',
+                           'planar.bvals')
+    in_bvec_plan = os.path.join(get_home(), 'btensor',
+                           'planar.bvecs')
+    in_dwi_sph = os.path.join(get_home(), 'btensor',
+                          'dwi_spherical.nii.gz')
+    in_bval_sph = os.path.join(get_home(), 'btensor',
+                           'spherical.bvals')
+    in_bvec_sph = os.path.join(get_home(), 'btensor',
+                           'spherical.bvecs')
+    fa = os.path.join(get_home(), 'btensor',
+                      'fa.nii.gz')
+
+    ret = script_runner.run('scil_compute_divide.py', '--in_dwis',
+                            in_dwi_lin, in_dwi_plan, in_dwi_sph,
+                            '--in_bvals', in_bval_lin, in_bval_plan,
+                            in_bval_sph, '--in_bvecs', in_bvec_lin,
+                            in_bvec_plan, in_bvec_sph, '--in_bdeltas',
+                            '1', '-0.5', '0', '--fa', fa, '--do_weight_bvals',
+                            '--do_weight_pa', '--do_multiple_s0',
+                            '--redo_weight_bvals', '--processes', '1', '-f')
+    assert (ret.success)
