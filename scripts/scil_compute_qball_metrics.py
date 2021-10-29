f761acf8288f5ad54dab2ff3f7000c282a1089e2
Charles Poirier <charles.poirier@usherbrooke.ca>
2021-10-29 15:46:08 -0400

    Uniformize argument parser

--- scripts/scil_compute_qball_metrics.py
+++ scripts/scil_compute_qball_metrics.py
@@ -26,66 +26,65 @@ from dipy.io import read_bvals_bvecs
 from dipy.direction.peaks import (peaks_from_model,
                                   reshape_peaks_for_visualization)
 from dipy.reconst.shm import QballModel, CsaOdfModel, anisotropic_power
-from scilpy.io.utils import (add_overwrite_arg, add_sh_basis_args,
-                             assert_inputs_exist, assert_outputs_exist,
-                             add_force_b0_arg)
+from scilpy.io.utils import (add_overwrite_arg, add_processes_arg,
+                             add_sh_basis_args, assert_inputs_exist,
+                             assert_outputs_exist, add_force_b0_arg,
+                             validate_nbr_processes)
+from scilpy.io.image import get_data_as_mask
 from scilpy.utils.bvec_bval_tools import (normalize_bvecs, is_normalized_bvecs,
                                           check_b0_threshold)
+
+
+DEFAULT_SMOOTH = 0.006
 
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    p.add_argument('input', metavar='input',
+    p.add_argument('in_dwi',
                    help='Path of the input diffusion volume.')
-    p.add_argument('bvals', metavar='bvals',
+    p.add_argument('in_bval',
                    help='Path of the bvals file, in FSL format.')
-    p.add_argument('bvecs', metavar='bvecs',
+    p.add_argument('in_bvec',
                    help='Path of the bvecs file, in FSL format.')
 
     add_overwrite_arg(p)
-    p.add_argument(
-        '--sh_order', metavar='int', default=4, type=int,
-        help='Spherical harmonics order. Must be a positive even number. '
-             'Default : 4')
-    p.add_argument(
-        '--mask', metavar='file',
-        help='Path to a binary mask. Only data inside the mask will be used '
-             'for computations and reconstruction (Default: None).')
-    p.add_argument(
-        '--use_qball', action='store_true',
-        help='If set, qball will be used as the odf reconstruction model '
-             'instead of csa (Default: False).')
-    p.add_argument(
-        '--not_all', action='store_true',
-        help='If set, will only save the files specified using the following '
-             'flags. (Default: False).')
-    p.add_argument(
-        '--processes', dest='nbr_processes', metavar='NBR', type=int,
-        help='Number of sub processes to start. Default : cpu count')
+    p.add_argument('--sh_order', default=4, type=int,
+                   help='Spherical harmonics order. Must be a positive even '
+                        'number [%(default)s].')
+    p.add_argument('--mask',
+                   help='Path to a binary mask. Only data inside the mask will'
+                        ' be used for computations and reconstruction '
+                        '[%(default)s].')
+    p.add_argument('--use_qball', action='store_true',
+                   help='If set, qball will be used as the odf reconstruction'
+                        ' model instead of CSA.')
+    p.add_argument('--not_all', action='store_true',
+                   help='If set, will only save the files specified using the '
+                        'following flags.')
 
     g = p.add_argument_group(title='File flags')
-    g.add_argument(
-        '--gfa', metavar='file', default='',
-        help='Output filename for the generalized fractional anisotropy.')
-    g.add_argument(
-        '--peaks', metavar='file', default='',
-        help='Output filename for the extracted peaks.')
-    g.add_argument(
-        '--peak_indices', metavar='file', default='',
-        help='Output filename for the generated peaks indices on the sphere.')
-    g.add_argument(
-        '--sh', metavar='file', default='',
-        help='Output filename for the spherical harmonics coefficients.')
-    g.add_argument(
-        '--nufo', metavar='file', default='',
-        help='Output filename for the NUFO map.')
-    g.add_argument(
-        '--a_power', metavar='file', default='',
-        help='Output filename for the anisotropic power map.')
+    g.add_argument('--gfa', default='',
+                   help='Output filename for the generalized fractional '
+                        'anisotropy [gfa.nii.gz].')
+    g.add_argument('--peaks', default='',
+                   help='Output filename for the extracted peaks '
+                        '[peaks.nii.gz].')
+    g.add_argument('--peak_indices', default='',
+                   help='Output filename for the generated peaks '
+                        'indices on the sphere [peaks_indices.nii.gz].')
+    g.add_argument('--sh', default='',
+                   help='Output filename for the spherical harmonics '
+                        'coefficients [sh.nii.gz].')
+    g.add_argument('--nufo', default='',
+                   help='Output filename for the NUFO map [nufo.nii.gz].')
+    g.add_argument('--a_power', default='',
+                   help='Output filename for the anisotropic power map'
+                        '[anisotropic_power.nii.gz].')
 
     add_force_b0_arg(p)
     add_sh_basis_args(p)
+    add_processes_arg(p)
 
     return p
 
@@ -108,22 +107,18 @@ def main():
         parser.error('When using --not_all, you need to specify at least ' +
                      'one file to output.')
 
-    assert_inputs_exist(parser, [args.input, args.bvals, args.bvecs])
+    assert_inputs_exist(parser, [args.in_dwi, args.in_bval, args.in_bvec])
     assert_outputs_exist(parser, args, arglist)
+    validate_nbr_processes(parser, args)
 
     nbr_processes = args.nbr_processes
-    parallel = True
-    if nbr_processes is not None:
-        if nbr_processes <= 0:
-            nbr_processes = None
-        elif nbr_processes == 1:
-            parallel = False
+    parallel = nbr_processes > 1
 
     # Load data
-    img = nib.load(args.input)
+    img = nib.load(args.in_dwi)
     data = img.get_fdata()
 
-    bvals, bvecs = read_bvals_bvecs(args.bvals, args.bvecs)
+    bvals, bvecs = read_bvals_bvecs(args.in_bval, args.in_bvec)
 
     if not is_normalized_bvecs(bvecs):
         logging.warning('Your b-vectors do not seem normalized...')
@@ -134,15 +129,16 @@ def main():
 
     sphere = get_sphere('symmetric724')
 
-    if args.mask is None:
-        mask = None
-    else:
-        mask = nib.load(args.mask).get_data().astype(np.bool)
+    mask = None
+    if args.mask:
+        mask = get_data_as_mask(nib.load(args.mask))
 
     if args.use_qball:
-        model = QballModel(gtab, sh_order=int(args.sh_order), smooth=0.006)
+        model = QballModel(gtab, sh_order=args.sh_order,
+                           smooth=DEFAULT_SMOOTH)
     else:
-        model = CsaOdfModel(gtab, sh_order=int(args.sh_order), smooth=0.006)
+        model = CsaOdfModel(gtab, sh_order=args.sh_order,
+                            smooth=DEFAULT_SMOOTH)
 
     odfpeaks = peaks_from_model(model=model,
                                 data=data,
