6648972228d728feacf8419db163cdf2b0ccb446
grahamlittlephd <graham.little.phd@gmail.com>
2021-11-15 11:10:52 -0500

    Added comput powder average script

--- scripts/scil_compute_powder_average.py
+++ scripts/scil_compute_powder_average.py
@@ -8,7 +8,10 @@ diffusion images.
 By default will output an average image calculated from all images with
 non-zero bvalue.
 
-specify --bvalue to output an image for a single bvalue
+specify --bvalue to output an image for a single shell
+
+Script currently does not take into account the diffusion gradient directions
+being averaged.
 """
 
 from os.path import splitext
@@ -81,14 +84,16 @@ def _build_arg_parser():
                    help='Path of the output file')
     
     add_overwrite_arg(p)
+    
     p.add_argument('--mask',dest='mask', metavar='file',
         help='Path to a binary mask.\nOnly data inside the mask will be used '
              'for powder avg. (Default: %(default)s)')
-    p.add_argument('--shell',dest='shell', metavar='int', default='',
+    
+    p.add_argument('--shell',dest='shell', type=int, default=None,
         help='bvalue (shell) to include in powder average.\nIf not specified'
              'will include all volumes with a non-zero bvalue')
     
-    p.add_argument('--shell_thr',dest='shell_thr', metavar='int', default='50',
+    p.add_argument('--shell_thr',dest='shell_thresh', type=int, default='50',
         help='Include volumes with bvalue +- the specified threshold.\n'
              'default: 50')
 
@@ -98,8 +103,11 @@ def _build_arg_parser():
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
+    inputs = [args.in_dwi, args.in_bval]
+    if args.mask:
+        inputs.append(args.mask)
 
-    assert_inputs_exist(parser, [args.in_dwi, args.in_bval])
+    assert_inputs_exist(parser, inputs)
     
     assert_outputs_exist(parser, args, args.out_avg)
 
@@ -109,7 +117,7 @@ def main():
     if args.mask is None:
         mask = None
     else:
-        mask = get_data_as_mask(nib.load(args.mask), dtype=bool)
+        mask = get_data_as_mask(nib.load(args.mask), dtype='uint8')
 
     # Read bvals
     logging.info('Performing powder average')
@@ -118,14 +126,17 @@ def main():
     # Select diffusion volumes to average
     if not(args.shell):
         # If no shell given, average all diffusion weigthed images
-        bval_idx = bvals > 0
+        bval_idx = bvals > 0 + args.shell_thresh
     else:
-        min_bval = args.shell - args.shell_idx
-        max_bval = args.shell + args.shell_idx
+        min_bval = args.shell - args.shell_thresh
+        max_bval = args.shell + args.shell_thresh
         bval_idx = np.logical_and(bvals > min_bval, bvals < max_bval)
 
     powder_avg = np.squeeze(np.mean(data[:,:,:,bval_idx],axis=3))
-    
+
+    if args.mask:
+        powder_avg = powder_avg * mask
+ 
     powder_avg_img = nib.Nifti1Image(powder_avg.astype(np.float32), affine)
     nib.save(powder_avg_img, args.out_avg)
 
