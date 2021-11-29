5e58555e5b765592135d87b45ac8c1d89e5391b8
grahamlittlephd <graham.little.phd@gmail.com>
2021-11-29 07:50:39 -0500

    minor fixes, arnaud's suggestions

--- scripts/scil_compute_powder_average.py
+++ scripts/scil_compute_powder_average.py
@@ -21,16 +21,17 @@ import argparse
 import logging
 
 import nibabel as nib
+from nibabel.tmpdirs import InTemporaryDirectory
+
 import numpy as np
 
 from dipy.io.gradients import read_bvals_bvecs
 
-# Aliased to avoid clashes with images called mode.
-from scilpy.io.image import get_data_as_mask
+from scilpy.io.image import (get_data_as_mask, assert_same_resolution)
 from scilpy.io.utils import (add_overwrite_arg, assert_inputs_exist,
                              assert_outputs_exist)
-from scilpy.utils.filenames import add_filename_suffix, split_name_with_nii
-from nibabel.tmpdirs import InTemporaryDirectory
+
+from scilpy.utils.bvec_bval_tools import get_shell_indices
 
 logger = logging.getLogger("Compute_Powder_Average")
 logger.setLevel(logging.INFO)
@@ -45,22 +46,22 @@ def _build_arg_parser():
     p.add_argument('in_bval',
                    help='Path of the bvals file, in FSL format.')
     p.add_argument('out_avg',
-                   help='Path of the output file')
+                   help='Path of the output file.')
 
     add_overwrite_arg(p)
 
-    p.add_argument('--mask', dest='mask', metavar='file',
+    p.add_argument('--mask', metavar='file',
                    help='Path to a binary mask.\nOnly data inside the '
                    'mask will be used for powder avg. (Default: %(default)s)')
 
-    p.add_argument('--shell', dest='shell', type=int, default=None,
+    p.add_argument('--shell', type=int, default=None,
                    help='bvalue (shell) to include in powder average.\nIf '
                    'not specified will include all volumes with a non-zero '
-                   'bvalue')
+                   'bvalue.')
 
-    p.add_argument('--shell_thr', dest='shell_thresh', type=int, default='50',
+    p.add_argument('--shell_thr', type=int, default='50',
                    help='Include volumes with bvalue +- the specified '
-                   'threshold.\ndefault: 50')
+                   'threshold.\ndefault: [%(default)s]')
 
     return p
 
@@ -73,7 +74,6 @@ def main():
         inputs.append(args.mask)
 
     assert_inputs_exist(parser, inputs)
-
     assert_outputs_exist(parser, args, args.out_avg)
 
     img = nib.load(args.in_dwi)
@@ -82,7 +82,9 @@ def main():
     if args.mask is None:
         mask = None
     else:
-        mask = get_data_as_mask(nib.load(args.mask), dtype='uint8')
+        mask_img = nib.load(args.mask)
+        assert_same_resolution((img, mask_img))
+        mask = get_data_as_mask(mask_img, dtype='uint8')
 
     # Read bvals (bvecs not needed at this point)
     logging.info('Performing powder average')
@@ -91,11 +93,9 @@ def main():
     # Select diffusion volumes to average
     if not(args.shell):
         # If no shell given, average all diffusion weigthed images
-        bval_idx = bvals > 0 + args.shell_thresh
+        bval_idx = bvals > 0 + args.shell_thr
     else:
-        min_bval = args.shell - args.shell_thresh
-        max_bval = args.shell + args.shell_thresh
-        bval_idx = np.logical_and(bvals > min_bval, bvals < max_bval)
+        bval_idx = get_shell_indices(bvals, args.shell, tol=args.shell_thr)
 
     powder_avg = np.squeeze(np.mean(data[:, :, :, bval_idx], axis=3))
 
@@ -104,9 +104,6 @@ def main():
 
     powder_avg_img = nib.Nifti1Image(powder_avg.astype(np.float32), affine)
     nib.save(powder_avg_img, args.out_avg)
-
-    del powder_avg_img
-
 
 if __name__ == "__main__":
     main()
