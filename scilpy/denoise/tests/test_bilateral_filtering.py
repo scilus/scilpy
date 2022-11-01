4ce893b4ec7ea8aaa7d8161e8cb875c669f870a3
Antoine Theberge <antoine.theberge@usherbrooke.ca>
2022-11-01 10:16:44 -0400

    ENH: reduce test code dup.

--- scilpy/denoise/tests/test_bilateral_filtering.py
+++ scilpy/denoise/tests/test_bilateral_filtering.py
@@ -7,9 +7,9 @@ from scilpy.tests.arrays import (
     fodf_3x3_order8_descoteaux07, fodf_3x3_order8_descoteaux07_filtered)
 
 
-def test_angle_aware_bilateral_filtering_cpu_one_process():
-    """ Test angle_aware_bilateral_filtering_cpu on a simple 3x3 grid
-    using one process.
+def _call_angle_aware_bilateral_filtering_cpu_n_processes(n_processes):
+    """ Call angle_aware_bilateral_filtering_cpu on a simple 3x3 grid
+    using an arbitrary number of processes.
     """
 
     in_sh = fodf_3x3_order8_descoteaux07
@@ -20,7 +20,7 @@ def test_angle_aware_bilateral_filtering_cpu_one_process():
     sigma_spatial = 1.0
     sigma_angular = 1.0
     sigma_range = 1.0
-    nbr_processes = 1.0
+    nbr_processes = n_processes
 
     sh_order, full_basis = get_sh_order_and_fullness(in_sh.shape[-1])
     out = angle_aware_bilateral_filtering_cpu(in_sh, sh_order,
@@ -28,6 +28,16 @@ def test_angle_aware_bilateral_filtering_cpu_one_process():
                                               sphere_str, sigma_spatial,
                                               sigma_angular, sigma_range,
                                               nbr_processes)
+
+    return out
+
+
+def test_angle_aware_bilateral_filtering_cpu_one_process():
+    """ Test angle_aware_bilateral_filtering_cpu on a simple 3x3 grid
+    using one process.
+    """
+
+    out = _call_angle_aware_bilateral_filtering_cpu_n_processes(1)
 
     assert np.allclose(out, fodf_3x3_order8_descoteaux07_filtered)
 
@@ -37,21 +47,6 @@ def test_angle_aware_bilateral_filtering_cpu_four_processes():
     using four processes.
     """
 
-    in_sh = fodf_3x3_order8_descoteaux07
-    sh_order = 8
-    sh_basis = 'descoteaux07'
-    in_full_basis = False
-    sphere_str = 'repulsion100'
-    sigma_spatial = 1.0
-    sigma_angular = 1.0
-    sigma_range = 1.0
-    nbr_processes = 4
-
-    sh_order, full_basis = get_sh_order_and_fullness(in_sh.shape[-1])
-    out = angle_aware_bilateral_filtering_cpu(in_sh, sh_order,
-                                              sh_basis, in_full_basis,
-                                              sphere_str, sigma_spatial,
-                                              sigma_angular, sigma_range,
-                                              nbr_processes)
+    out = _call_angle_aware_bilateral_filtering_cpu_n_processes(4)
 
     assert np.allclose(out, fodf_3x3_order8_descoteaux07_filtered)
