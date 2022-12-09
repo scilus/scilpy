ff6a71fff6c2f1e13955478541cb33d2fe888086
frheault <francois.m.rheault@usherbrooke.ca>
2022-12-09 11:51:42 -0500

    Dilate tested

--- scilpy/image/tests/test_labels.py
+++ scilpy/image/tests/test_labels.py
@@ -15,11 +15,12 @@ from scilpy.image.labels import (combine_labels, dilate_labels,
 
 ref_in_labels = np.zeros((10, 10, 10), dtype=np.uint16)
 for i in range(2, 8):
-    ref_in_labels[2:8, 2:8, i] = i
+    ref_in_labels[2:8, 2:8, i] = i-1
 
 ref_out_labels = deepcopy(ref_in_labels)
 for i in range(1, 8, 2):
     ref_out_labels[ref_out_labels == i] = 0
+
 
 def test_combine_labels_all_labels():
     in_labels = deepcopy(ref_in_labels)
@@ -81,3 +82,29 @@ def test_combine_labels_group_in_m():
     exp_labels[exp_labels == 6] = 6 + 1e4
 
     assert_equal(out_labels, exp_labels)
+
+
+def test_dilate_labels_with_mask():
+    in_labels = deepcopy(ref_in_labels)
+    in_mask = deepcopy(ref_in_labels)
+    in_mask[in_mask > 0] = 1
+    out_labels = dilate_labels(in_labels, 1, 2, 1,
+                               labels_to_dilate=[1, 6], labels_not_to_dilate=[3, 4],
+                               labels_to_fill=[0, 2, 5], mask=in_mask)
+
+    exp_labels = deepcopy(ref_in_labels)
+    exp_labels[exp_labels == 2] = 1
+    exp_labels[exp_labels == 5] = 6
+
+    assert_equal(out_labels, exp_labels)
+
+
+def test_dilate_labels_without_mask():
+    in_labels = deepcopy(ref_in_labels)
+    out_labels = dilate_labels(in_labels, 1, 2, 1,
+                               labels_to_dilate=[1, 6],
+                               labels_not_to_dilate=[3, 4, 5],
+                               labels_to_fill=[0], mask=None)
+
+    for i, val in enumerate([544, 156, 36, 36, 36, 36, 156]):
+        assert len(out_labels[out_labels == i]) == val
