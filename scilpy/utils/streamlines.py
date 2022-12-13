f006a9cbdefeab121b93b23cae322d10639fad66
frheault <francois.m.rheault@usherbrooke.ca>
2022-12-13 10:26:31 -0500

    Initial testing

--- scilpy/utils/streamlines.py
+++ scilpy/utils/streamlines.py
@@ -109,8 +109,7 @@ def uniformize_bundle_sft(sft, axis=None, ref_bundle=None, swap=False):
     density = get_endpoints_density_map(sft.streamlines, sft.dimensions,
                                         point_to_select=3)
     indices = np.argwhere(density > 0)
-    kmeans = KMeans(n_clusters=2, random_state=0, copy_x=True,
-                    precompute_distances=True, n_init=20,
+    kmeans = KMeans(n_clusters=2, random_state=0, copy_x=True, n_init=20,
                     n_jobs=1).fit(indices)
 
     labels = np.zeros(density.shape)
