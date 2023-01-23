f5ae29040869091a61329a675f2c00242a616fdb
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-01-23 09:36:44 -0500

    Add deprecated scil_streamlines_math

---
+++ scripts/scil_streamlines_math.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    raise DeprecationWarning(
+        "This script has been renamed scil_tractogram_math.py.\n"
+        "We will try to keep the following convention: Scripts on "
+        "'streamlines' treat each streamline individually. Scripts on "
+        "'tractograms' apply the same operation on all streamlines of the "
+        "tractogram.")
+
+
+if __name__ == "__main__":
+    main()
