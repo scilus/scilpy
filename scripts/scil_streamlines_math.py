f56ec0234efd23394c72295a14238be8f0caaabf
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-03-08 12:24:45 -0500

    Print warning but still run script (streamlines_math)

--- scripts/scil_streamlines_math.py
+++ scripts/scil_streamlines_math.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import warnings
+
+from scripts.scil_tractogram_math import main as new_main
+
 
 def main():
-    raise DeprecationWarning(
-        "This script has been renamed scil_tractogram_math.py.\n"
+    warnings.simplefilter('always', DeprecationWarning)
+    warnings.warn(
+        "\n\n*** WARNING ***\n"
+        "This script will soon be renamed scil_tractogram_math.py.\n"
+        "You should change your existing pipelines accordingly.\n"
         "We will try to keep the following convention: Scripts on "
-        "'streamlines' treat each streamline individually. Scripts on "
+        "'streamlines' treat each streamline individually.\nScripts on "
         "'tractograms' apply the same operation on all streamlines of the "
-        "tractogram.")
+        "tractogram.\n\n",
+        DeprecationWarning,
+        stacklevel=3)
+
+    new_main()
 
 
 if __name__ == "__main__":
