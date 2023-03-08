0fd3cc1e44151c869dade1eb3b5990a7d581a3b9
karp2601 <karp2601@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2023-03-08 10:55:46 -0500

    Removing the error check from fit_from_model_parallel

--- scilpy/reconst/multi_processes.py
+++ scilpy/reconst/multi_processes.py
@@ -30,10 +30,6 @@ def fit_from_model_parallel(args):
             except cvx.error.SolverError:
                 coeff = np.full((len(model.n)), np.NaN)
                 sub_fit_array[i] = MSDeconvFit(model, coeff, None)
-            except ArpackNoConvergence:
-                print("toto")
-                coeff = np.full((len(model.n)), np.NaN)
-                sub_fit_array[i] = MSDeconvFit(model, coeff, None)
 
     return chunk_id, sub_fit_array
 
