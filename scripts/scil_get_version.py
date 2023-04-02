e9d86e53a5963141e61662511fe759a3564a2edf
frheault <francois.m.rheault@usherbrooke.ca>
2023-04-02 16:34:48 -0400

    Working versions for PR

--- scripts/scil_get_version.py
+++ scripts/scil_get_version.py
@@ -19,9 +19,14 @@ import logging
 import pathlib
 import re
 import subprocess
+import pkg_resources
+import datetime
 
 import git
 import numpy as np
+import pip
+import os
+import time
 
 
 def _build_arg_parser():
@@ -30,30 +35,57 @@ def _build_arg_parser():
     return p
 
 
+def _bold(string):
+    string = str(string)
+    return '\033[1m' + string + '\033[0m'
+
+
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
+
+    dists = [d for d in pkg_resources.working_set]
+    for dist in dists:
+        if dist.project_name == 'scilpy':
+            date = time.ctime(os.path.getctime(dist.location))
+            date = datetime.datetime.strptime(date, "%a %b %d %H:%M:%S %Y")
+            date = date.strftime('%Y-%m-%d')
+            print('You installed Scilpy with pip on {}'.format(_bold(date)))
+            print('The closest release is {}\n'.format(_bold(dist.version)))
 
     repo_dir = pathlib.Path(__file__).parent.parent
     repo = git.Repo(repo_dir)
     branch = repo.active_branch.name
     origin = repo.remotes.origin.url
     branch = repo.active_branch.name
+    print('Your Scilpy directory is: {}'.format(_bold(repo_dir)))
+    print('Your current Origin is: {}'.format(_bold(origin)))
+    print('Your repository is on branch: {}\n'.format(_bold(branch)))
+
     last_commit = repo.head.commit
-    print(last_commit.message)
-    print(last_commit.hexsha)
-    print(last_commit.committed_datetime)
+    print('The last commit hash is: {}'.format(_bold(last_commit.hexsha)))
+    print('The last commit author is: {}'.format(_bold(last_commit.author)))
+    date = str(last_commit.committed_datetime).split()[0]
+    print('The last commit date is: {}, by {}'.format(_bold(date),
+          _bold(last_commit.author)))
+    print('The last commit message is: {}'.format(_bold(last_commit.message)))
 
     if 'upstream' in repo.git.remote().split():
         upstream = repo.remotes.upstream.url
-        upstream_commit = git.cmd.Git().ls_remote(upstream, heads=True)
-        # print(upstream_commit)
-        count = repo.git.rev_list('--count', 'upstream/master..HEAD')
-        print(count)
-    # remote_heads = 
-    # import time
+        last_commit = git.cmd.Git().ls_remote(upstream, heads=True).split()[0]
+        count = repo.git.rev_list('--count', 'upstream/master..HEAD',
+                                  '--left-right').split()
 
-    # time.asctime(time.gmtime(headcommit.committed_date))
-    # time.strftime("%a, %d %b %Y %H:%M", time.gmtime(headcommit.committed_date))
+        print('Your upstream is set to: {}'.format(_bold(upstream)))
+        print('The last commit hash on upstream is: {}'.format(
+            _bold(last_commit)))
+
+        print('You are {} commits behind upstream/master'.format(
+
+            _bold(count[0])))
+        print('You are {} commits ahead of upstream/master'.format(
+            _bold(count[1])))
+
+
 if __name__ == '__main__':
     main()
