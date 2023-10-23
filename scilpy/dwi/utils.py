ae4523d40f3795106206ff2fbcd38f9492daa211
mdesco <maxime.descoteaux@gmail.com>
2023-10-23 14:42:27 -0400

    moving functions to module dwi

--- scilpy/dwi/utils.py
+++ scilpy/dwi/utils.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
+
+def rescale_intensity(val, slope, in_max, bc_max):
+    return in_max - slope * (bc_max - val)
+
+
+# https://github.com/stnava/ANTs/blob/master/Examples/N4BiasFieldCorrection.cxx
+def rescale_dwi(in_data, bc_data):
+    in_min = np.amin(in_data)
+    in_max = np.amax(in_data)
+    bc_min = np.amin(bc_data)
+    bc_max = np.amax(bc_data)
+
+    slope = (in_max - in_min) / (bc_max - bc_min)
+
+    chunk = np.arange(0, len(in_data), 100000)
+    chunk = np.append(chunk, len(in_data))
+    for i in range(len(chunk)-1):
+        nz_bc_data = bc_data[chunk[i]:chunk[i+1]]
+        rescale_func = np.vectorize(_rescale_intensity, otypes=[np.float32])
+
+        rescaled_data = rescale_func(nz_bc_data, slope, in_max, bc_max)
+        bc_data[chunk[i]:chunk[i+1]] = rescaled_data
+
+    return bc_data
+
 
 
--- scripts/scil_apply_bias_field_on_dwi.py
+++ scripts/scil_apply_bias_field_on_dwi.py
@@ -16,6 +16,8 @@ from scilpy.io.image import get_data_as_mask
 from scilpy.io.utils import (add_overwrite_arg,
                              assert_inputs_exist,
                              assert_outputs_exist)
+from scilpy.dwi.utils import (rescale_intensity,
+                              rescale_dwi)
 
 
 def _build_arg_parser():
@@ -33,31 +35,6 @@ def _build_arg_parser():
                         'defined by the mask.')
     add_overwrite_arg(p)
     return p
-
-
-def _rescale_intensity(val, slope, in_max, bc_max):
-    return in_max - slope * (bc_max - val)
-
-
-# https://github.com/stnava/ANTs/blob/master/Examples/N4BiasFieldCorrection.cxx
-def _rescale_dwi(in_data, bc_data):
-    in_min = np.amin(in_data)
-    in_max = np.amax(in_data)
-    bc_min = np.amin(bc_data)
-    bc_max = np.amax(bc_data)
-
-    slope = (in_max - in_min) / (bc_max - bc_min)
-
-    chunk = np.arange(0, len(in_data), 100000)
-    chunk = np.append(chunk, len(in_data))
-    for i in range(len(chunk)-1):
-        nz_bc_data = bc_data[chunk[i]:chunk[i+1]]
-        rescale_func = np.vectorize(_rescale_intensity, otypes=[np.float32])
-
-        rescaled_data = rescale_func(nz_bc_data, slope, in_max, bc_max)
-        bc_data[chunk[i]:chunk[i+1]] = rescaled_data
-
-    return bc_data
 
 
 def main():
@@ -82,7 +59,7 @@ def main():
     nuc_dwi_data = np.divide(dwi_data[nz_mask_data],
                              bias_field_data[nz_mask_data].reshape((len(nz_mask_data[0]), 1)))
 
-    rescaled_nuc_data = _rescale_dwi(dwi_data[nz_mask_data],
+    rescaled_nuc_data = rescale_dwi(dwi_data[nz_mask_data],
                                      nuc_dwi_data)
 
     dwi_data[nz_mask_data] = rescaled_nuc_data
