cbfd672c50a75728eca6835d2801dd8274e1f661
mdesco <maxime.descoteaux@gmail.com>
2023-10-23 14:34:35 -0400

    addressing Charles comments

--- scilpy/reconst/aodf.py
+++ scilpy/reconst/aodf.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-
-
 #
-# aodf := Assymetric Orientation Distribution Function (aODF)
+# aodf := Asymmetric Orientation Distribution Function (aODF)
 #
 
 import numpy as np
@@ -11,6 +9,25 @@ from dipy.reconst.shm import sph_harm_ind_list
 
 
 def compute_asymmetry_index(sh_coeffs, order, mask):
+    """
+    Compute asymmetry index (ASI) (Cetin-Karayumak et al, 2018) from
+    asymmetric ODF volume expressed in full SH basis.
+
+    Parameters
+    -----------------
+    sh_coeffs: ndarray (x, y, z, ncoeffs)
+         Input spherical harmonics coefficients.
+    order: int > 0
+         Max SH order.
+    mask: ndarray (x, y, z), bool
+         Mask inside which ASI should be computed.
+
+    Returns
+    -----------
+    asi_map: ndarray (x, y, z)
+         Asymmetry index map.
+    """
+
     _, l_list = sph_harm_ind_list(order, full_basis=True)
 
     sign = np.power(-1.0, l_list)
@@ -30,6 +47,24 @@ def compute_asymmetry_index(sh_coeffs, order, mask):
 
 
 def compute_odd_power_map(sh_coeffs, order, mask):
+    """
+    Compute odd-power map (Poirier et al, 2020) from
+    asymmetric ODF volume expressed in full SH basis.
+
+    Parameters
+    -----------------
+    sh_coeffs: ndarray (x, y, z, ncoeffs)
+         Input spherical harmonics coefficients.
+    order: int > 0
+         Max SH order.
+    mask: ndarray (x, y, z), bool
+         Mask inside which odd-power map should be computed.
+
+    Returns
+    -----------
+    odd_power_map: ndarray (x, y, z)
+         Odd-power map.
+    """    
     _, l_list = sph_harm_ind_list(order, full_basis=True)
     odd_l_list = (l_list % 2 == 1).reshape((1, 1, 1, -1))
 
