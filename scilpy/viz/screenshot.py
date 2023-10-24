574f37babb84589e548d75b2e63a7ea962ebfb17
Alex Valcourt Caron <alexvc91@gmail.com>
2023-10-24 14:39:58 -0400

    Add peaks overlay to screenshot_volumes

--- scilpy/viz/screenshot.py
+++ scilpy/viz/screenshot.py
@@ -110,8 +110,6 @@ def screenshot_peaks(img, orientation, slice_ids, size, mask_img=None):
         Slice indices.
     size : array-like
         Size of the screenshot image (pixels).
-    bg_opacity : float, optional
-        Background opacity in range [0, 1].
 
     Returns
     -------
