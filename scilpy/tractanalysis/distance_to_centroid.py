71840d9a1f00049a73ed4ea48200f2c7b715e784
frheault <francois.m.rheault@usherbrooke.ca>
2023-11-01 10:44:06 -0400

    working distance

--- scilpy/tractanalysis/distance_to_centroid.py
+++ scilpy/tractanalysis/distance_to_centroid.py
@@ -1,26 +1,144 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from scipy.ndimage import binary_dilation
 from scipy.spatial import KDTree
 
 
-def min_dist_to_centroid(bundle_pts, centroid_pts, nb_pts):
-    tree = KDTree(centroid_pts, copy_data=True)
-    dists, labels = tree.query(bundle_pts, k=1)
+def transfer_and_diffuse_labels(target_sft, source_sft, nb_pts=20,):
+    tree = KDTree(source_sft.streamlines._data, copy_data=True)
+    pts_ids = tree.query_ball_point(target_sft.streamlines._data, r=4)
 
-    dists = np.expand_dims(dists, axis=1)
-    labels = np.expand_dims(labels, axis=1)
+    max_count_labels = []
+    for pts_id in pts_ids:
+        if not pts_id:  # If no source point is close enough
+            max_count_labels.append(-1)
+            continue
+        labels = np.mod(pts_id, nb_pts) + 1
+        unique_labels, counts = np.unique(labels, return_counts=True)
+        max_count_label = unique_labels[np.argmax(counts)]
+        max_count_labels.append(max_count_label)
+    
+    labels = np.array(max_count_labels, dtype=np.uint16)
 
-    labels = np.mod(labels, nb_pts)
+    curr_ind = 0
+    for _, streamline in enumerate(target_sft.streamlines):
+        next_ind = curr_ind + len(streamline)
+        curr_labels = labels[curr_ind:next_ind]
+        labels[curr_ind:next_ind] = diffuse_labels(streamline, curr_labels)
+        curr_ind = next_ind
 
-    sum_dist = np.expand_dims(np.sum(dists, axis=1), axis=1)
-    weights = np.exp(-dists / sum_dist)
+    return labels
 
-    votes = []
-    for i in range(len(bundle_pts)):
-        vote = np.bincount(labels[i], weights=weights[i])
-        total = np.arange(np.amax(labels[i])+1)
-        winner = total[np.argmax(vote)]
-        votes.append(winner)
 
-    return np.array(votes, dtype=np.uint16), np.average(dists, axis=1)
+
+def min_dist_to_centroid(target_pts, source_pts, nb_pts=None,
+                         pre_computed_labels=None):
+    if nb_pts is None and pre_computed_labels is None:
+        raise ValueError('Either nb_pts or labels must be provided.')
+
+    tree = KDTree(source_pts, copy_data=True)
+    _, labels = tree.query(target_pts, k=1)
+
+    if pre_computed_labels is None:
+        labels = np.mod(labels, nb_pts) + 1
+    else:
+        labels = pre_computed_labels[labels]
+
+    return labels.astype(np.uint16)
+
+
+def diffuse_labels(streamline, labels):
+    """
+    Replace -1 labels in the polyline using a diffusion algorithm.
+
+    Parameters:
+        streamline (ndarray): Coordinates of the polyline.
+        labels (ndarray): Labels corresponding to the points in the polyline.
+
+    Returns:
+        ndarray: Updated labels with -1 replaced.
+    """
+    iteration = 0
+    while np.any(labels == 65535):  # Continue until no -1 labels are left
+        for i, label in enumerate(labels):
+            if label == 65535:
+                # Find closest point with a non-negative label
+                min_distance = np.inf
+                closest_label = -1
+                for j, other_label in enumerate(labels):
+                    if other_label != 65535:
+                        distance = np.linalg.norm(streamline[i]-streamline[j])
+                        if distance < min_distance:
+                            min_distance = distance
+                            closest_label = other_label
+                # Update the label
+                if iteration > 10:
+                    labels[i] = 1
+                labels[i] = closest_label
+    return labels
+
+from scipy.spatial.distance import pdist, squareform
+
+def find_medoid(points):
+    """
+    Find the medoid among a set of points.
+    
+    Parameters:
+        points (ndarray): Points in N-dimensional space.
+        
+    Returns:
+        ndarray: Coordinates of the medoid.
+    """
+    distance_matrix = squareform(pdist(points))
+    medoid_idx = np.argmin(distance_matrix.sum(axis=1))
+    return points[medoid_idx]
+
+
+def compute_shell_barycenters(labels_map):
+    """
+    Compute the barycenter for each label in a 3D NumPy array by maximizing
+    the distance to the boundary.
+    
+    Parameters:
+        labels_map (ndarray): The 3D array containing labels from 1-nb_pts.
+        
+    Returns:
+        ndarray: An array of size (nb_pts, 3) containing the barycenter
+        for each label.
+    """
+    labels = np.unique(labels_map)[1:]
+    barycenters = np.zeros((len(labels), 3))
+
+    for label in labels:
+        mask = np.zeros_like(labels_map)
+        mask[labels_map == label] = 1
+        mask_coords = np.argwhere(mask)
+
+        barycenter = find_medoid(mask_coords)
+        barycenters[label - 1] = barycenter
+
+    return barycenters
+
+
+def compute_euclidean_barycenters(labels_map):
+    """
+    Compute the euclidean barycenter for each label in a 3D NumPy array.
+
+    Parameters:
+        labels_map (ndarray): The 3D array containing labels from 1-nb_pts.
+
+    Returns:
+        ndarray: A NumPy array of shape (nb_pts, 3) containing the barycenter
+        for each label.
+    """
+    labels = np.unique(labels_map)[1:]
+    barycenters = np.zeros((len(labels), 3))
+
+    for label in labels:
+        indices = np.argwhere(labels_map == label)
+        if indices.size > 0:
+            barycenter = np.mean(indices, axis=0)
+            barycenters[label-1, :] = barycenter
+
+    return barycenters
--- scripts/scil_compute_bundle_voxel_label_map.py
+++ scripts/scil_compute_bundle_voxel_label_map.py
@@ -8,12 +8,15 @@ Each voxel will have the label of its nearest centroid point.
 The number of labels will be the same as the centroid's number of points.
 """
 
+from sklearn.svm import SVC
+from collections import defaultdict
+from time import time
 import argparse
 import os
 
-from dipy.align.streamlinear import StreamlineLinearRegistration
+# from dipy.align.streamlinear import BundleMinDistanceMetric, StreamlineLinearRegistration
 from dipy.io.streamline import save_tractogram
-from dipy.io.stateful_tractogram import StatefulTractogram, set_sft_logger_level, Space
+from dipy.io.stateful_tractogram import StatefulTractogram, set_sft_logger_level, Space, Origin
 from dipy.io.utils import is_header_compatible
 import matplotlib.pyplot as plt
 import nibabel as nib
@@ -30,7 +33,10 @@ from scilpy.io.utils import (add_overwrite_arg,
                              assert_inputs_exist,
                              assert_output_dirs_exist_and_empty)
 from scilpy.tractanalysis.streamlines_metrics import compute_tract_counts_map
-from scilpy.tractanalysis.distance_to_centroid import min_dist_to_centroid
+from scilpy.tractanalysis.distance_to_centroid import (min_dist_to_centroid,
+                                                       compute_euclidean_barycenters,
+                                                       compute_shell_barycenters,
+                                                       transfer_and_diffuse_labels)
 from scilpy.tractograms.streamline_and_mask_operations import \
     cut_outside_of_mask_streamlines
 from scilpy.tractograms.streamline_operations import resample_streamlines_num_points
@@ -38,7 +44,6 @@ from scilpy.tractograms.streamline_and_mask_operations import \
     get_head_tail_density_maps
 from scilpy.utils.streamlines import uniformize_bundle_sft
 from scilpy.viz.utils import get_colormap
-
 
 
 def _build_arg_parser():
@@ -67,55 +72,9 @@ def _build_arg_parser():
 
     return p
 
-from sklearn.svm import SVC
-from time import time
-
-from collections import defaultdict
-
-def compute_overlap_and_mapping(small_data, full_data):
-    """
-    Compute the overlap between labels in the small and full datasets
-    and generate a mapping based on maximizing this overlap.
-    
-    Parameters:
-    - small_data: np.ndarray, data from the smaller image
-    - full_data: np.ndarray, data from the full image
-    - unique_small: np.ndarray, unique labels in the smaller image
-    - unique_full: np.ndarray, unique labels in the full image
-    
-    Returns:
-    - dict, mapping from labels in the smaller image to labels in the full image
-    """
-    mapping = {}
-    unique_small = np.unique(small_data)
-    unique_full = np.unique(full_data)
-    for label_small in unique_small:
-        if label_small == 0:
-            continue  # Skip background
-        
-        overlaps = defaultdict(int)
-        mask_small = small_data == label_small
-
-        for label_full in unique_full:
-            if label_full == 0:
-                continue  # Skip background
-            
-            mask_full = full_data == label_full
-            overlap = np.sum(mask_small & mask_full)
-            
-            if overlap > 0:
-                overlaps[label_full] = overlap
-
-        if overlaps:
-            best_match = max(overlaps, key=overlaps.get)
-            mapping[label_small] = best_match
-        else:
-            # If no overlap found, continue with default increasing +1 scheme
-            mapping[label_small] = label_small + 1
-    
-    return mapping
 
 def main():
+    t0 = time()
     parser = _build_arg_parser()
     args = parser.parse_args()
     set_sft_logger_level('ERROR')
@@ -143,7 +102,8 @@ def main():
             if not is_header_compatible(sft_list[0], sft_list[-1]):
                 parser.error('Header of {} and {} are not compatible'.format(
                     args.in_bundles[0], filename))
-
+    print('Loading time', time()-t0)
+    t0 = time()
     density_list = []
     binary_list = []
     for sft in sft_list:
@@ -187,60 +147,156 @@ def main():
                                                       binary_bundle)
         if len(sft_list[i]):
             concat_sft += sft_list[i]
-
+    print('Chop time', time()-t0)
+    t0 = time()
     args.nb_pts = len(sft_centroid.streamlines[0]) if args.nb_pts is None \
         else args.nb_pts
 
     sft_centroid = resample_streamlines_num_points(sft_centroid, args.nb_pts)
     uniformize_bundle_sft(concat_sft, ref_bundle=sft_centroid[0])
-    tmp_sft = resample_streamlines_num_points(concat_sft[0:1000], args.nb_pts)
+    tmp_sft = resample_streamlines_num_points(concat_sft[0:2500], args.nb_pts)
 
-    if not args.new_labelling:
-        new_streamlines = sft_centroid.streamlines.copy()
-        sft_centroid = StatefulTractogram.from_sft([new_streamlines[0]],
-                                                   sft_centroid)
-    else:
-        srr = StreamlineLinearRegistration()
-        srm = srr.optimize(static=tmp_sft.streamlines,
-                        moving=sft_centroid.streamlines)
-        sft_centroid.streamlines = srm.transform(sft_centroid.streamlines)
-
+    print('Uni+SLR time', time()-t0)
+    t0 = time()
     t0 = time()
     if not args.new_labelling:
         indices = np.array(np.nonzero(binary_bundle), dtype=int).T
-        labels, _ = min_dist_to_centroid(indices,
-                                            sft_centroid.streamlines._data,
-                                            args.nb_pts)
-        labels += 1  # 0 means no labels
+        labels = min_dist_to_centroid(indices,
+                                      sft_centroid[0].streamlines._data,
+                                      nb_pts=args.nb_pts)
         labels_map = np.zeros(binary_bundle.shape, dtype=np.int16)
         labels_map[np.where(binary_bundle)] = labels
+        barycenters = compute_euclidean_barycenters(labels_map)
+        nib.save(nib.Nifti1Image(labels_map, sft_list[0].affine),
+                 os.path.join(args.out_dir, 'labels_map_1.nii.gz'))
+        labels = ndi.map_coordinates(labels_map,
+                                     concat_sft.streamlines._data.T-0.5,
+                                     order=0)
+        print('Euclidian time', time()-t0)
+        t0 = time()
     else:
-        svc = SVC(C=1, kernel='rbf')
-        #def transfer_and_diffuse_labels(sft_source, sft_target):
-
-        labels = np.tile(np.arange(0,args.nb_pts)[::-1], len(tmp_sft))
-        labels += 1
+        svc = SVC(C=1, kernel='rbf', cache_size=1000)
+        labels = transfer_and_diffuse_labels(tmp_sft, sft_centroid)
+        print('Diffuse time', time()-t0)
+        t0 = time()
         svc.fit(X=tmp_sft.streamlines._data, y=labels)
+        print('Fit time', time()-t0)
+        t0 = time()
+        # print(exp_labels)
 
-        labels_pred = svc.predict(X=np.array(np.where(binary_bundle)).T)
-        labels_map = np.zeros(binary_bundle.shape, dtype=np.int16)
-        labels_map[np.where(binary_bundle)] = labels_pred
+        exp_labels = svc.predict(X=np.array(np.where(binary_bundle)).T)
+        print('Predict time', time()-t0)
+        t0 = time()
 
-        exp_labels = np.tile(np.arange(0,args.nb_pts)[::-1], len(sft_centroid))
-        exp_labels += 1
-        svc.fit(X=sft_centroid.streamlines._data, y=exp_labels)
-
-        exp_labels_pred = svc.predict(X=np.array(np.where(binary_bundle)).T)
         exp_labels_map = np.zeros(binary_bundle.shape, dtype=np.int16)
-        exp_labels_map[np.where(binary_bundle)] = exp_labels_pred
+        exp_labels_map[np.where(binary_bundle)] = exp_labels
+        barycenters = compute_shell_barycenters(exp_labels_map)
+        exp_labels = svc.predict(X=barycenters)
 
-        mapping = compute_overlap_and_mapping(labels_map, exp_labels_map)
-        print(mapping)
+        labels = ndi.map_coordinates(exp_labels_map,
+                                     concat_sft.streamlines._data.T-0.5,
+                                     order=0)
+        print('Map making time', time()-t0)
+        t0 = time()
 
+    barycenter_sft = StatefulTractogram([barycenters], sft_centroid,
+                                        space=Space.VOX, origin=Origin.TRACKVIS)
 
-    print('a', time()-t0)
+    # It is not allowed that labels jumps labels for consistency
+    # Streamlines should have continous labels
+    final_streamlines = []
+    final_label = []
+    curr_ind = 0
+    for i, streamline in enumerate(concat_sft.streamlines):
+        next_ind = curr_ind + len(streamline)
+        curr_labels = labels[curr_ind:next_ind]
+        curr_ind = next_ind
 
+        # Flip streamlines so the labels increase (facilitate if/else)
+        # Should always be ordered in nextflow pipeline
+        gradient = np.gradient(curr_labels)
+        if len(np.argwhere(gradient < 0)) > len(np.argwhere(gradient > 0)):
+            streamline = streamline[::-1]
+            curr_labels = curr_labels[::-1]
 
+        # # Find jumps, cut them and find the longest
+        gradient = np.ediff1d(curr_labels)
+        max_jump = 2
+        if len(np.argwhere(np.abs(gradient) > max_jump)) > 0:
+            pos_jump = np.where(np.abs(gradient) > max_jump)[0] + 1
+            split_chunk = np.split(curr_labels,
+                                   pos_jump)
+
+            max_len = 0
+            max_pos = 0
+            for j, chunk in enumerate(split_chunk):
+                if len(chunk) > max_len:
+                    max_len = len(chunk)
+                    max_pos = j
+
+            curr_labels = split_chunk[max_pos]
+            gradient_chunk = np.ediff1d(chunk)
+            if len(np.unique(np.sign(gradient_chunk))) > 1:
+                continue
+            streamline = np.split(streamline,
+                                  pos_jump)[max_pos]
+
+        final_streamlines.append(streamline)
+        final_label.append(curr_labels)
+
+    final_streamlines = ArraySequence(final_streamlines)
+    final_labels = ArraySequence(final_label)
+
+    indices = np.array(np.nonzero(binary_bundle), dtype=int).T
+    labels = min_dist_to_centroid(indices,
+                                  final_streamlines._data,
+                                  pre_computed_labels=final_labels._data)
+    labels_map = np.zeros(binary_bundle.shape, dtype=np.int16)
+    labels_map[np.where(binary_bundle)] = labels
+    print('Clean Up time', time()-t0)
+    t0 = time()
+    dists = np.ones(binary_bundle.shape, dtype=float) * -1
+
+    save_tractogram(barycenter_sft, os.path.join(args.out_dir,
+                                                    'barycenters.trk'))
+                                                    
+    import dijkstra3d
+    barycenter_bin = compute_tract_counts_map(barycenter_sft.streamlines,
+                                                  barycenter_sft.dimensions)
+    barycenter_bin[barycenter_bin > 0] = 1
+    # for label in range(1, args.nb_pts+1):
+    #     indices = np.array(np.nonzero(labels_map == label), dtype=int).T
+    #     field = np.ones(labels_map.shape, dtype=float)
+    #     for ind in indices:
+    #         ind = tuple(ind)
+    #         
+    #         path = dijkstra3d.dijkstra(field, barycenter, ind, compass=True)
+    #         dists[ind] = len(path)-1
+    for label in range(1, args.nb_pts+1):
+        mask = np.zeros(labels_map.shape)
+        mask[labels_map == label] = 1
+        barycenter_bin_intersect = barycenter_bin * mask
+        barycenter_intersect_coords = np.array(np.nonzero(barycenter_bin_intersect),
+                                               dtype=int).T
+        bundle_disjoint, num_labels = ndi.label(mask)
+        iterations = 0
+        
+        while num_labels > 1:
+            mask = ndi.binary_dilation(mask)
+            bundle_disjoint, num_labels = ndi.label(mask)
+            iterations += 1
+            print('a', label, iterations, num_labels)
+
+        barycenter = tuple(np.round(barycenters[label-1]).astype(int))
+        print(label, labels_map[barycenter], barycenter)
+        curr_dists = dijkstra3d.distance_field(mask,
+                                               source=barycenter_intersect_coords)
+        dists[labels_map == label] = curr_dists[labels_map == label]
+        print(np.unique(curr_dists, return_counts=True))
+        print()
+
+    print('Dijkstra time', time()-t0)
+    t0 = time()
 
     cmap = get_colormap(args.colormap)
     for i, sft in enumerate(sft_list):
@@ -256,20 +312,17 @@ def main():
         nib.save(nib.Nifti1Image((binary_list[i]*labels_map).astype(np.uint16),
                                  sft_list[0].affine),
                  os.path.join(sub_out_dir, 'labels_map.nii.gz'))
-        # nib.save(nib.Nifti1Image(binary_list[i]*distance_map,
-        #                          sft_list[0].affine),
-        #          os.path.join(sub_out_dir, 'distance_map.nii.gz'))
         nib.save(nib.Nifti1Image(binary_list[i]*corr_map,
                                  sft_list[0].affine),
                  os.path.join(sub_out_dir, 'correlation_map.nii.gz'))
+        nib.save(nib.Nifti1Image(binary_list[i]*dists,
+                                 sft_list[0].affine),
+                 os.path.join(sub_out_dir, 'distance_map.nii.gz'))
 
         if len(sft):
             tmp_labels = ndi.map_coordinates(labels_map,
                                              sft.streamlines._data.T-0.5,
                                              order=0)
-            # tmp_dists = ndi.map_coordinates(distance_map,
-            #                                 sft.streamlines._data.T-0.5,
-            #                                 order=0)
             tmp_corr = ndi.map_coordinates(corr_map,
                                            sft.streamlines._data.T-0.5,
                                            order=0)
@@ -283,17 +336,13 @@ def main():
         save_tractogram(new_sft,
                         os.path.join(sub_out_dir, 'labels.trk'))
 
-        # if len(sft):
-        #     new_sft.data_per_point['color']._data = cmap(
-        #         tmp_dists / np.max(tmp_dists))[:, 0:3] * 255
-        # save_tractogram(new_sft,
-        #                 os.path.join(sub_out_dir, 'distance.trk'))
-
-        if len(sft):
+        if len(sft) and len(args.in_bundles) > 1:
             new_sft.data_per_point['color']._data = cmap(tmp_corr)[
                 :, 0:3] * 255
         save_tractogram(new_sft,
                         os.path.join(sub_out_dir, 'correlation.trk'))
+    print('Finish time', time()-t0)
+    t0 = time()
 
 
 if __name__ == '__main__':
