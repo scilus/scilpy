000495c61457dc1b7111e7897ee6382c12a9fa8c
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-11-02 14:04:06 -0400

    Improve style in scil_extract_dwi_shell

--- scripts/scil_extract_dwi_shell.py
+++ scripts/scil_extract_dwi_shell.py
@@ -25,6 +25,7 @@ import nibabel as nib
 import numpy as np
 
 from scilpy.dwi.utils import extract_dwi_shell
+from scilpy.io.gradient_table import save_gradient_sampling_fsl
 from scilpy.io.utils import (add_overwrite_arg, add_verbose_arg,
                              assert_inputs_exist, assert_outputs_exist)
 
@@ -36,40 +37,29 @@ def _build_arg_parser():
 
     p.add_argument('in_dwi',
                    help='The DW image file to split.')
-
     p.add_argument('in_bval',
                    help='The b-values file in FSL format (.bval).')
-
     p.add_argument('in_bvec',
                    help='The b-vectors file in FSL format (.bvec).')
-
-    p.add_argument('in_bvals_to_extract', nargs='+',
-                   type=int,
+    p.add_argument('in_bvals_to_extract', nargs='+', type=int,
                    help='The list of b-values to extract. For example 0 2000.')
-
     p.add_argument('out_dwi',
                    help='The name of the output DWI file.')
-
     p.add_argument('out_bval',
                    help='The name of the output b-value file (.bval).')
-
     p.add_argument('out_bvec',
                    help='The name of the output b-vector file (.bvec).')
 
     p.add_argument('--out_indices',
-                   help='Optional filename for valid indices in\
-                         input dwi volume')
-
-    p.add_argument('--block-size', '-s',
-                   metavar='INT', type=int,
-                   help='Loads the data using this block size. '
-                        'Useful\nwhen the data is too large to be '
-                        'loaded in memory.')
-
+                   help='Optional filename for valid indices in input dwi '
+                        'volume')
+    p.add_argument('--block-size', '-s', metavar='INT', type=int,
+                   help='Loads the data using this block size. Useful\n'
+                        'when the data is too large to be loaded in memory.')
     p.add_argument('--tolerance', '-t',
                    metavar='INT', type=int, default=20,
-                   help='The tolerated gap between the b-values to '
-                        'extract\nand the actual b-values.')
+                   help='The tolerated gap between the b-values to  extract\n'
+                        'and the actual b-values. [%(default)s]')
 
     add_verbose_arg(p)
     add_overwrite_arg(p)
@@ -91,19 +81,17 @@ def main():
     bvals, bvecs = read_bvals_bvecs(args.in_bval, args.in_bvec)
 
     # Find the volume indices that correspond to the shells to extract.
-    tol = args.tolerance
-
     img = nib.load(args.in_dwi)
 
-    outputs = extract_dwi_shell(img, bvals, bvecs, args.in_bvals_to_extract,
-                                tol, args.block_size)
-
-    indices, shell_data, new_bvals, new_bvecs = outputs
+    indices, shell_data, new_bvals, new_bvecs = extract_dwi_shell(
+        img, bvals, bvecs, args.in_bvals_to_extract,
+        args.tolerance, args.block_size)
 
     logging.info("Selected indices: {}".format(indices))
 
-    np.savetxt(args.out_bval, new_bvals, '%d')
-    np.savetxt(args.out_bvec, new_bvecs.T, '%0.15f')
+    shell_idx = np.ones(len(bvecs))  # Only one shell here!
+    save_gradient_sampling_fsl(bvecs, shell_idx, bvals,
+                               args.out_bval, args.out_bvec)
     nib.save(nib.Nifti1Image(shell_data, img.affine, header=img.header),
              args.out_dwi)
 
