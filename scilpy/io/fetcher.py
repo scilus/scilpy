99868575c5c2d36ad9f432c53b139d2c307b1cc1
frheault <francois.m.rheault@usherbrooke.ca>
2023-11-02 14:30:24 -0400

    Move back fetcher

--- scilpy/io/fetcher.py
+++ scilpy/io/fetcher.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import logging
 import hashlib
 import os
 import pathlib
@@ -7,6 +8,7 @@ import requests
 import zipfile
 
 GOOGLE_URL = "https://drive.google.com/uc?"
+
 
 def download_file_from_google_drive(id, destination):
     """
@@ -171,3 +173,22 @@ def fetch_data(files_dict, keys=None):
         else:
             # toDo. Verify that data on disk is the right one.
             logging.warning("Not fetching data; already on disk.")
+
+
+def get_synb0_template_path():
+    """
+    Return MNI 2.5mm template in scilpy repository
+
+    Returns
+    -------
+    path: str
+        Template path
+    """
+    # Get the valid LUT choices.
+    import scilpy  # ToDo. Is this the only way?
+    module_path = inspect.getfile(scilpy)
+    module_path = os.path.dirname(os.path.dirname(module_path))
+
+    path = os.path.join(module_path, 'data/',
+                        'mni_icbm152_t1_tal_nlin_asym_09c_masked_2_5.nii.gz')
+    return path
