328263585d8ec7b6feba39af4e26e7e4e81e6a01
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-11-02 12:42:45 -0400

    Empty main for scil_apply_bias_field

--- scripts/scil_apply_bias_field_on_dwi.py
+++ scripts/scil_apply_bias_field_on_dwi.py
@@ -4,7 +4,7 @@
 """
 Apply bias field correction to DWI. This script doesn't compute the bias
 field itself. It ONLY applies an existing bias field. Use the ANTs
-N4BiasFieldCorrection executable to compute the bias field
+N4BiasFieldCorrection executable to compute the bias field.
 """
 
 import argparse
@@ -16,7 +16,7 @@ from scilpy.io.image import get_data_as_mask
 from scilpy.io.utils import (add_overwrite_arg,
                              assert_inputs_exist,
                              assert_outputs_exist)
-from scilpy.dwi.utils import rescale_dwi
+from scilpy.dwi.utils import apply_bias_field
 
 
 def _build_arg_parser():
@@ -31,7 +31,10 @@ def _build_arg_parser():
                    help='Corrected DWI Nifti image.')
     p.add_argument('--mask',
                    help='Apply bias field correction only in the region '
-                        'defined by the mask.')
+                        'defined by the mask.\n'
+                        'If this is not given, the bias field is still only '
+                        'applied only in non-background data \n(i.e. where '
+                        'the dwi is not 0).')
     add_overwrite_arg(p)
     return p
 
@@ -55,16 +58,9 @@ def main():
     else:
         nz_mask_data = np.nonzero(np.average(dwi_data, axis=-1))
 
-    nuc_dwi_data = np.divide(
-        dwi_data[nz_mask_data],
-        bias_field_data[nz_mask_data].reshape((len(nz_mask_data[0]), 1)))
+    dwi_data = apply_bias_field(dwi_data, bias_field_data, nz_mask_data)
 
-    rescaled_nuc_data = rescale_dwi(dwi_data[nz_mask_data],
-                                    nuc_dwi_data)
-
-    dwi_data[nz_mask_data] = rescaled_nuc_data
-    nib.save(nib.Nifti1Image(dwi_data, dwi_img.affine,
-                             dwi_img.header),
+    nib.save(nib.Nifti1Image(dwi_data, dwi_img.affine, dwi_img.header),
              args.out_name)
 
 
