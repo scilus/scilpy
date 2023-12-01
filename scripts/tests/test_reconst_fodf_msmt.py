b22c1d9650fc33cada8b11ba9b543c063646876c
Philippe Karan <philippe.karan@usherbrooke.ca>
2023-11-30 21:17:39 -0500

    Renaming scil_compute_msmt_fodf.py to scil_reconst_fodf_msmt.py.

--- scripts/tests/test_reconst_fodf_msmt.py
+++ scripts/tests/test_reconst_fodf_msmt.py
@@ -11,7 +11,7 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_compute_msmt_fodf.py', '--help')
+    ret = script_runner.run('scil_reconst_fodf_msmt.py', '--help')
     assert ret.success
 
 
@@ -25,7 +25,7 @@ def test_execution_processing(script_runner):
     in_csf_frf = os.path.join(get_home(), 'commit_amico', 'csf_frf.txt')
     mask = os.path.join(get_home(), 'commit_amico', 'mask.nii.gz')
 
-    ret = script_runner.run('scil_compute_msmt_fodf.py', in_dwi, in_bval,
+    ret = script_runner.run('scil_reconst_fodf_msmt.py', in_dwi, in_bval,
                             in_bvec, in_wm_frf, in_gm_frf, in_csf_frf,
                             '--mask', mask,
                             '--wm_out_fODF', 'wm_fodf.nii.gz',
