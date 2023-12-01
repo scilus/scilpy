63a063ee448ce56305f752f18702d3120e6a3706
Philippe Karan <philippe.karan@usherbrooke.ca>
2023-11-30 21:22:43 -0500

    Renaming scil_compute_msmt_frf.py to scil_reconst_frf_msmt.py.

--- scripts/tests/test_reconst_frf_msmt.py
+++ scripts/tests/test_reconst_frf_msmt.py
@@ -11,7 +11,7 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_compute_msmt_frf.py', '--help')
+    ret = script_runner.run('scil_reconst_frf_msmt.py', '--help')
     assert ret.success
 
 
@@ -25,13 +25,13 @@ def test_roi_radii_shape_parameter(script_runner):
                            'dwi.bvec')
     mask = os.path.join(get_home(), 'commit_amico',
                            'mask.nii.gz')
-    ret = script_runner.run('scil_compute_msmt_frf.py', in_dwi,
+    ret = script_runner.run('scil_reconst_frf_msmt.py', in_dwi,
                             in_bval, in_bvec, 'wm_frf.txt', 'gm_frf.txt',
                             'csf_frf.txt', '--mask', mask, '--roi_center',
                             '15', '15', '15', '-f')
     assert ret.success
 
-    ret = script_runner.run('scil_compute_msmt_frf.py', in_dwi,
+    ret = script_runner.run('scil_reconst_frf_msmt.py', in_dwi,
                             in_bval, in_bvec, 'wm_frf.txt', 'gm_frf.txt',
                             'csf_frf.txt', '--mask', mask, '--roi_center',
                             '15', '-f')
@@ -48,19 +48,19 @@ def test_roi_radii_shape_parameter(script_runner):
                            'dwi.bvec')
     mask = os.path.join(get_home(), 'commit_amico',
                            'mask.nii.gz')
-    ret = script_runner.run('scil_compute_msmt_frf.py', in_dwi,
+    ret = script_runner.run('scil_reconst_frf_msmt.py', in_dwi,
                             in_bval, in_bvec, 'wm_frf.txt', 'gm_frf.txt',
                             'csf_frf.txt', '--mask', mask, '--roi_radii',
                             '37', '-f')
     assert ret.success
 
-    ret = script_runner.run('scil_compute_msmt_frf.py', in_dwi,
+    ret = script_runner.run('scil_reconst_frf_msmt.py', in_dwi,
                             in_bval, in_bvec, 'wm_frf.txt', 'gm_frf.txt',
                             'csf_frf.txt', '--mask', mask, '--roi_radii',
                             '37', '37', '37', '-f')
     assert ret.success
 
-    ret = script_runner.run('scil_compute_msmt_frf.py', in_dwi,
+    ret = script_runner.run('scil_reconst_frf_msmt.py', in_dwi,
                             in_bval, in_bvec, 'wm_frf.txt', 'gm_frf.txt',
                             'csf_frf.txt', '--mask', mask, '--roi_radii',
                             '37', '37', '37', '37', '37', '-f')
@@ -77,7 +77,7 @@ def test_execution_processing(script_runner):
                            'dwi.bvec')
     mask = os.path.join(get_home(), 'commit_amico',
                            'mask.nii.gz')
-    ret = script_runner.run('scil_compute_msmt_frf.py', in_dwi,
+    ret = script_runner.run('scil_reconst_frf_msmt.py', in_dwi,
                             in_bval, in_bvec, 'wm_frf.txt', 'gm_frf.txt',
                             'csf_frf.txt', '--mask', mask, '--min_nvox', '20',
                             '-f')
