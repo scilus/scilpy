514048182729ac6459c70a07493982554e9813c8
Philippe Karan <philippe.karan@usherbrooke.ca>
2023-12-01 07:33:00 -0500

    Renaming scil_extract_b0.py to scil_dwi_extract_b0.py.

--- scripts/tests/test_dwi_extract_b0.py
+++ scripts/tests/test_dwi_extract_b0.py
@@ -12,7 +12,7 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_extract_b0.py', '--help')
+    ret = script_runner.run('scil_dwi_extract_b0.py', '--help')
     assert ret.success
 
 
@@ -24,6 +24,6 @@ def test_execution_processing(script_runner):
                            'dwi.bval')
     in_bvec = os.path.join(get_home(), 'processing',
                            'dwi.bvec')
-    ret = script_runner.run('scil_extract_b0.py', in_dwi, in_bval, in_bvec,
+    ret = script_runner.run('scil_dwi_extract_b0.py', in_dwi, in_bval, in_bvec,
                             'b0_mean.nii.gz', '--mean', '--b0', '20')
     assert ret.success
