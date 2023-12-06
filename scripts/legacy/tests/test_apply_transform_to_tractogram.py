e7d69423acb6a2582523b3a53dcb16264a7a95b4
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-12-06 10:36:10 -0500

    Remove unused imports

--- scripts/legacy/tests/test_apply_transform_to_tractogram.py
+++ scripts/legacy/tests/test_apply_transform_to_tractogram.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import tempfile
-
-from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
-
 
 def test_help_option(script_runner):
     ret = script_runner.run('scil_apply_transform_to_tractogram.py', '--help')
--- scripts/legacy/tests/test_evaluate_connectivity_graph_measures.py
+++ scripts/legacy/tests/test_evaluate_connectivity_graph_measures.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import tempfile
-
-from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
-
 
 def test_help_option(script_runner):
     ret = script_runner.run('scil_evaluate_connectivity_graph_measures.py', '--help')
