28b94b0c45a074c9c907473a3a7e50d42aa3ac3d
Gabriel Girard <girard.gabriel@gmail.com>
2023-12-07 15:39:37 -0500

    TST - removed individual tests

--- scripts/legacy/tests/test_evaluate_connectivity_pairwise_agreement_measures.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-def test_help_option(script_runner):
-    ret = script_runner.run(
-        'scil_evaluate_connectivity_pairwise_agreement_measures.py', '--help')
-    assert ret.success
