11d7043ee91367cafc38488f10ba8409cd9635fa
Gabriel Girard <girard.gabriel@gmail.com>
2023-12-07 09:24:33 -0500

    RF - renamed scil_compute_qbx.py

--- scripts/tests/test_tractogram_qbx.py
+++ scripts/tests/test_tractogram_qbx.py
@@ -13,7 +13,7 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_compute_qbx.py', '--help')
+    ret = script_runner.run('scil_tractogram_qbx.py', '--help')
     assert ret.success
 
 
@@ -21,6 +21,6 @@ def test_execution_filtering(script_runner):
     os.chdir(os.path.expanduser(tmp_dir.name))
     in_bundle = os.path.join(get_home(), 'filtering',
                              'bundle_all_1mm.trk')
-    ret = script_runner.run('scil_compute_qbx.py', in_bundle, '12',
+    ret = script_runner.run('scil_tractogram_qbx.py', in_bundle, '12',
                             'clusters/', '--out_centroids', 'centroids.trk')
     assert ret.success
