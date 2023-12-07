3de41a558356a75f92f7bbf532d66b39e9b086a8
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-12-07 16:29:22 -0500

    Rename scil_bundle_print_.... Delete scil_compute_bundle_volume

--- scripts/tests/test_bundle_print_shape_measures.py
+++ scripts/tests/test_bundle_print_shape_measures.py
@@ -13,20 +13,17 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_evaluate_bundles_individual_measures.py',
-                            '--help')
+    ret = script_runner.run('scil_bundle_print_shape_measures.py', '--help')
     assert ret.success
 
 
 def test_execution_bundles(script_runner):
     os.chdir(os.path.expanduser(tmp_dir.name))
-    in_1 = os.path.join(get_home(), 'bundles',
-                        'bundle_0_reco.tck')
+    in_1 = os.path.join(get_home(), 'bundles', 'bundle_0_reco.tck')
     in_2 = os.path.join(get_home(), 'bundles', 'voting_results',
                         'bundle_0.trk')
-    in_ref = os.path.join(get_home(), 'bundles',
-                          'bundle_all_1mm.nii.gz')
-    ret = script_runner.run('scil_evaluate_bundles_individual_measures.py',
-                            in_1, in_2, 'AF_L_measures.json',
+    in_ref = os.path.join(get_home(), 'bundles', 'bundle_all_1mm.nii.gz')
+    ret = script_runner.run('scil_bundle_print_shape_measures.py',
+                            in_1, in_2, '--out_json', 'AF_L_measures.json',
                             '--reference', in_ref, '--processes', '1')
     assert ret.success
