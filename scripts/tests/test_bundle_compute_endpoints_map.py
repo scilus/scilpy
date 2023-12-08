b3b1c144a0814ff57bd6da76ebe7be26c9c431d2
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-12-08 11:22:59 -0500

    Rename compute_centroid, compute_endpoints_map

--- scripts/tests/test_bundle_compute_endpoints_map.py
+++ scripts/tests/test_bundle_compute_endpoints_map.py
@@ -13,14 +13,14 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_compute_centroid.py', '--help')
+    ret = script_runner.run('scil_bundle_compute_endpoints_map.py', '--help')
     assert ret.success
 
 
 def test_execution_tractometry(script_runner):
     os.chdir(os.path.expanduser(tmp_dir.name))
-    in_bundle = os.path.join(get_home(), 'tractometry',
-                             'IFGWM_uni.trk')
-    ret = script_runner.run('scil_compute_centroid.py',
-                            in_bundle, 'IFGWM_uni_c.trk')
+    in_bundle = os.path.join(get_home(), 'tractometry', 'IFGWM_uni.trk')
+    ret = script_runner.run('scil_bundle_compute_endpoints_map.py', in_bundle,
+                            'head.nii.gz', 'tail.nii.gz', '--binary')
+
     assert ret.success
