5080ab28103ebe946d4d0e11c0018b14ad761f53
frheault <francois.m.rheault@usherbrooke.ca>
2024-01-15 09:25:05 -0500

    Add back fetcher

--- scripts/scil_volume_b0_synthesis.py
+++ scripts/scil_volume_b0_synthesis.py
@@ -39,7 +39,7 @@ from scilpy.io.utils import (add_overwrite_arg,
                              add_verbose_arg,
                              assert_inputs_exist,
                              assert_outputs_exist)
-from scilpy.utils.image import register_image
+from scilpy.image.volume_operations import register_image
 
 
 def _build_arg_parser():
