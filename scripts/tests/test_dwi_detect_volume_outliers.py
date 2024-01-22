fbf1da1261d24d03340934ea6181a9e10a4f9056
arnaudbore <arnaud.bore@gmail.com>
2024-01-22 14:33:12 -0500

    fix test

--- scripts/tests/test_dwi_detect_volume_outliers.py
+++ scripts/tests/test_dwi_detect_volume_outliers.py
@@ -12,11 +12,11 @@ tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help_option(script_runner):
-    ret = script_runner.run('scil_dwi_extract_shell.py', '--help')
+    ret = script_runner.run('scil_dwi_detect_volume_outliers.py', '--help')
     assert ret.success
 
 
-def test_execution_processing_1000(script_runner):
+def test_execution(script_runner):
     os.chdir(os.path.expanduser(tmp_dir.name))
     in_dwi = os.path.join(get_home(), 'processing',
                           'dwi_crop.nii.gz')
@@ -24,39 +24,6 @@ def test_execution_processing_1000(script_runner):
                            'dwi.bval')
     in_bvec = os.path.join(get_home(), 'processing',
                            'dwi.bvec')
-    ret = script_runner.run('scil_dwi_extract_shell.py', in_dwi,
-                            in_bval, in_bvec, '0', '1000',
-                            'dwi_crop_1000.nii.gz', '1000.bval', '1000.bvec',
-                            '-t', '30')
-    assert ret.success
-
-
-def test_execution_out_indices(script_runner):
-    os.chdir(os.path.expanduser(tmp_dir.name))
-    in_dwi = os.path.join(get_home(), 'processing',
-                          'dwi_crop.nii.gz')
-    in_bval = os.path.join(get_home(), 'processing',
-                           'dwi.bval')
-    in_bvec = os.path.join(get_home(), 'processing',
-                           'dwi.bvec')
-    ret = script_runner.run('scil_dwi_extract_shell.py', in_dwi,
-                            in_bval, in_bvec, '0', '1000',
-                            'dwi_crop_1000__1.nii.gz', '1000__1.bval',
-                            '1000__1.bvec', '-t', '30', '--out_indices',
-                            'out_indices.txt')
-    assert ret.success
-
-
-def test_execution_processing_3000(script_runner):
-    os.chdir(os.path.expanduser(tmp_dir.name))
-    in_dwi = os.path.join(get_home(), 'processing',
-                          'dwi_crop.nii.gz')
-    in_bval = os.path.join(get_home(), 'processing',
-                           'dwi.bval')
-    in_bvec = os.path.join(get_home(), 'processing',
-                           'dwi.bvec')
-    ret = script_runner.run('scil_dwi_extract_shell.py', in_dwi,
-                            in_bval, in_bvec, '0', '3000',
-                            'dwi_crop_3000.nii.gz', '3000.bval', '3000.bvec',
-                            '-t', '30')
+    ret = script_runner.run('scil_dwi_detect_volume_outliers.py', in_dwi,
+                            in_bval, in_bvec)
     assert ret.success
