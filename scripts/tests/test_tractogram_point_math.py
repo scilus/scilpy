212975bb36d2b02e85d08e6d41a77d1bb3db19c8
grahamlittlephd <grahamlittlephd@gmail.com>
2024-01-25 15:25:00 -0500

    added test for point math correlation

--- scripts/tests/test_tractogram_point_math.py
+++ scripts/tests/test_tractogram_point_math.py
@@ -9,6 +9,7 @@ from scilpy.io.fetcher import get_testing_files_dict, fetch_data, get_home
 
 # If they already exist, this only takes 5 seconds (check md5sum)
 fetch_data(get_testing_files_dict(), keys=['tractometry.zip'])
+fetch_data(get_testing_files_dict(), keys=['tracking.zip'])
 tmp_dir = tempfile.TemporaryDirectory()
 
 
@@ -37,5 +38,29 @@ def test_execution_tractogram_point_math_mean_3D_defaults(script_runner):
                             't1_mean_on_streamlines.trk',
                             '--in_dpp_name', 't1',
                             '--out_name', 't1_mean')
+
+    assert ret.success
+
+
+def test_execution_tractogram_point_math_mean_4D_correlation(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_bundle = os.path.join(get_home(), 'tracking',
+                             'local_split_0.trk')
+
+    in_fodf = os.path.join(get_home(), 'tracking',
+                          'fodf.nii.gz')
+    fodf_on_bundle = 'fodf_on_streamlines.trk'
+
+    script_runner.run('scil_tractogram_project_map_to_streamlines.py',
+                      in_bundle, fodf_on_bundle,
+                      '--in_metric', in_fodf, in_fodf,
+                      '--out_dpp_name', 'fodf', 'fodf2')
+
+    ret = script_runner.run('scil_tractogram_point_math.py',
+                            'correlation',
+                            fodf_on_bundle,
+                            'fodf_correlation_on_streamlines.trk',
+                            '--in_dpp_name', 'fodf',
+                            '--out_name', 'fodf_correlation')
 
     assert ret.success
