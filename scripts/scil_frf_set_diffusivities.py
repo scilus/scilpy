2e801b7045795f24a7c4936fa7e712522da4f398
karp2601 <karp2601@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2024-02-13 09:57:45 -0500

    Adding msmt support

--- scripts/scil_frf_set_diffusivities.py
+++ scripts/scil_frf_set_diffusivities.py
@@ -6,7 +6,8 @@ Replace the fiber response function in the FRF file.
 Use this script when you want to use a fixed response function
 and keep the mean b0.
 
-The FRF file is obtained from scil_frf_ssst.py
+The FRF file is obtained from scil_frf_ssst.py or scil_frf_msmt.py in the case
+of multi-shell data.
 
 Formerly: scil_set_response_function.py
 """
@@ -30,7 +31,9 @@ def _build_arg_parser():
     p.add_argument('new_frf', metavar='tuple',
                    help='Replace the response function with\n'
                         'this fiber response function x 10**-4 (e.g. '
-                        '15,4,4).')
+                        '15,4,4). \nIf multi-shell, write the first shell'
+                        ', then the second shell, \nand the third, etc '
+                        '(e.g. 15,4,4,13,5,5,12,5,5).')
     p.add_argument('output_frf_file', metavar='output',
                    help='Path of the new FRF file.')
     p.add_argument('--no_factor', action='store_true',
@@ -51,13 +54,24 @@ def main():
     assert_inputs_exist(parser, args.frf_file)
     assert_outputs_exist(parser, args, args.output_frf_file)
 
-    frf_file = np.array(np.loadtxt(args.frf_file))
+    frf_file = np.array(np.loadtxt(args.frf_file)).T
     new_frf = np.array(literal_eval(args.new_frf), dtype=np.float64)
     if not args.no_factor:
         new_frf *= 10 ** -4
     b0_mean = frf_file[3]
 
-    response = np.concatenate([new_frf, [b0_mean]])
+    if new_frf.shape[0] % 3 != 0:
+        raise ValueError('Inputed new frf is not valid. There should be '
+                         'three values per shell, and thus the total number '
+                         'of values should be a multiple of three.')
+
+    nb_shells = int(new_frf.shape[0] / 3)
+    new_frf = new_frf.reshape((nb_shells, 3))
+
+    response = np.empty((nb_shells, 4))
+    response[:, 0:3] = new_frf
+    response[:, 3] = b0_mean
+
     np.savetxt(args.output_frf_file, response)
 
 
