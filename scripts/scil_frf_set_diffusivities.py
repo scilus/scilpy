dedb657c5474c6fd4eca27734a12dec0dcf835e0
karp2601 <karp2601@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2024-02-14 11:49:19 -0500

    Fixing pep8

--- scripts/scil_frf_set_diffusivities.py
+++ scripts/scil_frf_set_diffusivities.py
@@ -40,10 +40,10 @@ def _build_arg_parser():
                    help='If supplied, the fiber response function is\n'
                         'evaluated without the x 10**-4 factor. [%(default)s].'
                    )
-    
+
     add_verbose_arg(p)
     add_overwrite_arg(p)
-    
+
     return p
 
 
