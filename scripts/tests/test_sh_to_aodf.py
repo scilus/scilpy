6c831174ccf216d0ec8c828c05a6f5a86d6f3478
CHrlS98 <charles.poirier@usherbrooke.ca>
2024-02-28 17:03:25 -0500

    Fixed tests (waiting on updated test data)

--- scripts/tests/test_sh_to_aodf.py
+++ scripts/tests/test_sh_to_aodf.py
@@ -16,119 +16,78 @@ data_path = os.path.join(get_home(), 'fodf_filtering')
 tmp_dir = tempfile.TemporaryDirectory()
 
 
-@pytest.fixture
-def mock_filtering(mocker, out_fodf):
-    def _mock(*args, **kwargs):
-        img = nib.load(out_fodf)
-        return img.get_fdata().astype(np.float32)
-
-    script = 'scil_sh_to_aodf'
-    filtering_fn = "angle_aware_bilateral_filtering"
-    return mocker.patch("scripts.{}.{}".format(script, filtering_fn),
-                        side_effect=_mock, create=True)
-
-
 def test_help_option(script_runner):
     ret = script_runner.run('scil_sh_to_aodf.py', '--help')
     assert ret.success
 
 
-@pytest.mark.parametrize("in_fodf,out_fodf",
-    [[os.path.join(data_path, 'fodf_descoteaux07_sub.nii.gz'),
-      os.path.join(data_path, 'fodf_descoteaux07_sub_full.nii.gz')]])
-def test_asym_basis_output(script_runner, mock_filtering, in_fodf, out_fodf):
-    os.chdir(os.path.expanduser(tmp_dir.name))
-
-    ret = script_runner.run('scil_sh_to_aodf.py',
-                            in_fodf, 'out_fodf1.nii.gz',
-                            '--sphere', 'repulsion100',
-                            '--sigma_angular', '1.0',
-                            '--sigma_spatial', '1.0',
-                            '--sigma_range', '1.0',
-                            '--sh_basis', 'descoteaux07', '-f',
-                            print_result=True, shell=True)
-
-    assert ret.success
-    mock_filtering.assert_called_once()
-
-    ret_fodf = nib.load("out_fodf1.nii.gz")
-    test_fodf = nib.load(out_fodf)
-    assert np.allclose(ret_fodf.get_fdata(), test_fodf.get_fdata())
-
-
-@pytest.mark.parametrize("in_fodf,out_fodf,sym_fodf",
+@pytest.mark.parametrize("in_fodf,expected_fodf",
     [[os.path.join(data_path, "fodf_descoteaux07_sub.nii.gz"),
-      os.path.join(data_path, "fodf_descoteaux07_sub_full.nii.gz"),
-      os.path.join(data_path, "fodf_descoteaux07_sub_sym.nii.gz")]])
-def test_sym_basis_output(
-    script_runner, mock_filtering, in_fodf, out_fodf, sym_fodf):
-    os.chdir(os.path.expanduser(tmp_dir.name))
-
-    ret = script_runner.run('scil_sh_to_aodf.py',
-                            in_fodf,
-                            'out_fodf2.nii.gz',
-                            '--out_sym', 'out_sym.nii.gz',
-                            '--sphere', 'repulsion100',
-                            '--sigma_angular', '1.0',
-                            '--sigma_spatial', '1.0',
-                            '--sigma_range', '1.0',
-                            '--sh_basis', 'descoteaux07', '-f',
-                            print_result=True, shell=True)
-
-    assert ret.success
-    mock_filtering.assert_called_once()
-
-    ret_sym_fodf = nib.load("out_sym.nii.gz")
-    test_sym_fodf = nib.load(sym_fodf)
-    assert np.allclose(ret_sym_fodf.get_fdata(), test_sym_fodf.get_fdata())
-
-
-@pytest.mark.parametrize("in_fodf,out_fodf",
-    [[os.path.join(data_path, "fodf_descoteaux07_sub_full.nii.gz"),
-      os.path.join(data_path, "fodf_descoteaux07_sub_twice.nii.gz")]])
-def test_asym_input(script_runner, mock_filtering, in_fodf, out_fodf):
-    os.chdir(os.path.expanduser(tmp_dir.name))
-
-    ret = script_runner.run('scil_sh_to_aodf.py',
-                            in_fodf,
-                            'out_fodf3.nii.gz',
-                            '--sphere', 'repulsion100',
-                            '--sigma_angular', '1.0',
-                            '--sigma_spatial', '1.0',
-                            '--sigma_range', '1.0',
-                            '--sh_basis', 'descoteaux07', '-f',
-                            print_result=True, shell=True)
-
-    assert ret.success
-    mock_filtering.assert_called_once()
-    
-    ret_fodf = nib.load("out_fodf3.nii.gz")
-    test_fodf = nib.load(out_fodf)
-    assert np.allclose(ret_fodf.get_fdata(), test_fodf.get_fdata())
-
-
-@pytest.mark.parametrize("in_fodf,out_fodf",
-    [[os.path.join(data_path, 'fodf_descoteaux07_sub.nii.gz'),
-      os.path.join(data_path, 'fodf_descoteaux07_sub_full.nii.gz')]])
-def test_cosine_method(script_runner, mock_filtering, in_fodf, out_fodf):
+      os.path.join(data_path,
+                   "fodf_descoteaux07_sub_unified_asym.nii.gz")]])
+def test_asym_basis_output(script_runner, in_fodf, expected_fodf):
     os.chdir(os.path.expanduser(tmp_dir.name))
 
     ret = script_runner.run('scil_sh_to_aodf.py',
                             in_fodf, 'out_fodf1.nii.gz',
                             '--sphere', 'repulsion100',
-                            '--method', 'cosine',
-                            '--sh_basis', 'descoteaux07',
-                            '-f',
+                            '--sigma_align', '0.8',
+                            '--sigma_spatial', '1.0',
+                            '--sigma_range', '0.2',
+                            '--sigma_angle', '0.06',
+                            '--device', 'cpu',
+                            '--sh_basis', 'descoteaux07', '-f',
                             print_result=True, shell=True)
 
     assert ret.success
 
-    # method cosine is fast and not mocked
-    mock_filtering.assert_not_called()
+    ret_fodf = nib.load("out_fodf1.nii.gz")
+    test_fodf = nib.load(expected_fodf)
+    assert np.allclose(ret_fodf.get_fdata(), test_fodf.get_fdata())
+
+
+@pytest.mark.parametrize("in_fodf,expected_fodf",
+    [[os.path.join(data_path,
+                   "fodf_descoteaux07_sub_unified_asym.nii.gz"),
+      os.path.join(data_path,
+                   "fodf_descoteaux07_sub_unified_asym_twice.nii.gz")]])
+def test_asym_input(script_runner, in_fodf, expected_fodf):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+
+    ret = script_runner.run('scil_sh_to_aodf.py',
+                            in_fodf, 'out_fodf1.nii.gz',
+                            '--sphere', 'repulsion100',
+                            '--sigma_align', '0.8',
+                            '--sigma_spatial', '1.0',
+                            '--sigma_range', '0.2',
+                            '--sigma_angle', '0.06',
+                            '--device', 'cpu',
+                            '--sh_basis', 'descoteaux07', '-f',
+                            print_result=True, shell=True)
+
+    assert ret.success
+
+    ret_fodf = nib.load("out_fodf1.nii.gz")
+    test_fodf = nib.load(expected_fodf)
+    assert np.allclose(ret_fodf.get_fdata(), test_fodf.get_fdata())
+
+
+@pytest.mark.parametrize("in_fodf,out_fodf",
+    [[os.path.join(data_path, 'fodf_descoteaux07_sub.nii.gz'),
+      os.path.join(data_path, 'fodf_descoteaux07_sub_cosine_asym.nii.gz')]])
+def test_cosine_method(script_runner, in_fodf, out_fodf):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+
+    ret = script_runner.run('scil_sh_to_aodf.py',
+                            in_fodf, 'out_fodf1.nii.gz',
+                            '--sphere', 'repulsion100',
+                            '--method', 'cosine', '-f',
+                            '--sh_basis', 'descoteaux07',
+                            print_result=True, shell=True)
+
+    assert ret.success
 
     ret_fodf = nib.load("out_fodf1.nii.gz")
     test_fodf = nib.load(out_fodf)
 
-    # We expect the output to be different from the
-    # one obtained with angle-aware bilateral filtering
-    assert not np.allclose(ret_fodf.get_fdata(), test_fodf.get_fdata())
+    assert np.allclose(ret_fodf.get_fdata(), test_fodf.get_fdata())
