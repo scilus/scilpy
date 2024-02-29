82c344c8f0a8822117152a1fbc3046f447828626
karp2601 <karp2601@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2024-02-29 13:03:07 -0500

    Fixing afd bug

--- scilpy/tractanalysis/afd_along_streamlines.py
+++ scilpy/tractanalysis/afd_along_streamlines.py
@@ -85,7 +85,7 @@ def afd_and_rd_sums_along_streamlines(sft, fodf, fodf_basis,
     order = find_order_from_nb_coeff(fodf_data)
     sphere = get_sphere('repulsion724')
     b_matrix, _ = sh_to_sf_matrix(sphere, order, fodf_basis, legacy=is_legacy)
-    n = sph_harm_ind_list(fodf_basis)
+    _, n = sph_harm_ind_list(order)
     legendre0_at_n = lpn(order, 0)[0][n]
     sphere_norm = np.linalg.norm(sphere.vertices)
 
