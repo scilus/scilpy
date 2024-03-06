9f34e4de357e91ad0e4b0e7c8251d8e4614c1e8c
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-03-06 10:30:13 -0500

    Switch scil_data for neurogister. Minor renaming for lisibility

--- scilpy/io/dvc.py
+++ scilpy/io/dvc.py
@@ -8,7 +8,7 @@ from dvc import api, config
 
 from scilpy import SCILPY_HOME, SCILPY_ROOT
 
-DVC_REPOSITORY = "https://github.com/AlexVCaron/scil_data.git"
+DVC_REPOSITORY = "https://github.com/scilus/neurogister.git"
 DEFAULT_CACHE_CONFIG = {
     "type": "symlink",
     "shared": "group",
@@ -38,7 +38,7 @@ def pull_test_case_package(package_name):
         The path to the pulled package.
 
     .. _Scilpy Tests Vendor:
-        https://github.com/AlexVCaron/scilus/tree/main/store/scilpy_tests
+        https://github.com/scilus/neurogister/tree/main/store/scilpy_tests/meta.yml # noqa
     """
     with open(f"{SCILPY_ROOT}/.dvc/test_descriptors.yml", 'r') as f:
         test_descriptors = yaml.safe_load(f)
@@ -48,15 +48,15 @@ def pull_test_case_package(package_name):
 
         pull_package_from_dvc_repository(
             "scilpy_tests", f"{package_name}", f"{SCILPY_HOME}/test_data",
-            test_descriptors[package_name]["revision"])
+            git_revision=test_descriptors[package_name]["revision"])
 
         return f"{SCILPY_HOME}/test_data/{package_name}"
 
 
-def pull_package_from_dvc_repository(vendor, package_name, output_dir,
-                                     revision="main",
-                                     remote_url=DVC_REPOSITORY,
-                                     remote_name="scil-data",
+def pull_package_from_dvc_repository(vendor, package_name, local_destination,
+                                     git_remote_url=DVC_REPOSITORY,
+                                     git_revision="main",
+                                     dvc_remote_name="scil-data",
                                      dvc_config_root=f"{SCILPY_ROOT}/.dvc"):
     """
     Pull a package from a correctly configured DVC remote repository. Packages
@@ -69,13 +69,13 @@ def pull_package_from_dvc_repository(vendor, package_name, output_dir,
         Name of the vendor to pull the package from.
     package_name : str
         Name of the package to pull from the vendor.
-    output_dir : str
-        Path to the directory where the package will be pulled.
-    revision : str
-        Git revision of the DVC artifact to pull.
-    remote_url : str
-        URL of the DVC git repository.
-    remote_name : str
+    local_destination : str
+        Destination where to pull the package.
+    git_remote_url : str
+        URL of the Git repository containing DVC artifacts.
+    git_revision : str
+        Git revision of the DVC artifacts repository.
+    dvc_remote_name : str
         Name of the DVC remote to use.
     dvc_config_root : str
         Location of the DVC configuration files.
@@ -86,32 +86,33 @@ def pull_package_from_dvc_repository(vendor, package_name, output_dir,
         The path to the pulled package.
 
     .. _SCIL Data Store:
-        https://github.com/scilus/scil_data/tree/main/store
+        https://github.com/scilus/neurogister/tree/main/store
     """
     return pull_from_dvc_repository(f"store/{vendor}/{package_name}",
-                                    f"{output_dir}/",
-                                    revision, remote_url, remote_name,
-                                    dvc_config_root)
+                                    f"{local_destination}/",
+                                    git_remote_url, git_revision,
+                                    dvc_remote_name, dvc_config_root)
 
 
-def pull_from_dvc_repository(package_name, output_dir, revision="main",
-                             remote_url=DVC_REPOSITORY,
-                             remote_name="scil-data",
+def pull_from_dvc_repository(remote_location, local_destination,
+                             git_remote_url=DVC_REPOSITORY,
+                             git_revision="main",
+                             dvc_remote_name="scil-data",
                              dvc_config_root=f"{SCILPY_ROOT}/.dvc"):
     """
     Pull data from a DVC remote repository to the specified location.
 
     Parameters
     ----------
-    package_name : str
-        Name of the package to pull from the DVC repository.
-    output_dir : str
-        Path to the directory where the package will be pulled.
-    revision : str
-        Git revision of the DVC artifact to pull.
-    remote_url : str
-        URL of the DVC git repository.
-    remote_name : str
+    remote_location : str
+        Location of the data to pull in the repository.
+    local_destination : str
+        Destination where to pull the data.
+    git_remote_url : str
+        URL of the Git repository containing DVC artifacts.
+    git_revision : str
+        Git revision of the DVC artifacts repository.
+    dvc_remote_name : str
         Name of the DVC remote to use.
     dvc_config_root : str
         Location of the DVC configuration files.
@@ -121,11 +122,13 @@ def pull_from_dvc_repository(package_name, output_dir, revision="main",
     str
         The path to the pulled package.
     """
-    conf = get_default_config()
-    conf.merge(config.Config(dvc_config_root))
+    repository_config = get_default_config()
+    repository_config.merge(config.Config(dvc_config_root))
+    remote_config = repository_config["remote"][dvc_remote_name]
 
-    registry = api.DVCFileSystem(remote_url, revision,
-                                 config=conf, remote_name=remote_name,
-                                 remote_config=conf["remote"][remote_name])
+    registry = api.DVCFileSystem(git_remote_url, git_revision,
+                                 config=repository_config,
+                                 remote_name=dvc_remote_name,
+                                 remote_config=remote_config)
 
-    registry.get(package_name, output_dir, True, cache=True)
+    registry.get(remote_location, local_destination, True, cache=True)
