c54be779f2862e23c0d17cd7d7480c4c1f45f740
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-03-06 14:31:01 -0500

    NODDI_priors: clarifying

--- scripts/scil_NODDI_priors.py
+++ scripts/scil_NODDI_priors.py
@@ -20,20 +20,18 @@ from scilpy.io.utils import (assert_inputs_exist,
                              add_overwrite_arg,
                              add_verbose_arg)
 
-
 EPILOG = """
 Reference:
     [1] Zhang H, Schneider T, Wheeler-Kingshott CA, Alexander DC.
-        NODDI: practical in vivo neurite orientation dispersion
-        and density imaging of the human brain.
-        NeuroImage. 2012 Jul 16;61:1000-16.
+        NODDI: practical in vivo neurite orientation dispersion and density 
+        imaging of the human brain. NeuroImage. 2012 Jul 16;61:1000-16.
 """
 
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, epilog=EPILOG,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+        formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument('in_FA',
                    help='Path to the FA volume.')
     p.add_argument('in_AD',
@@ -45,28 +43,29 @@ def _build_arg_parser():
 
     g1 = p.add_argument_group('Metrics options')
     g1.add_argument(
-        '--fa_min', type=float, default='0.7',
+        '--fa_single_fiber', type=float, default=0.7,
         help='Minimal threshold of FA (voxels above that threshold are '
-             'considered in the single fiber mask). [%(default)s]')
+             'considered in \nthe single fiber mask). [%(default)s]')
     g1.add_argument(
-        '--fa_max', type=float, default='0.1',
+        '--fa_ventricles', type=float, default=0.1,
         help='Maximal threshold of FA (voxels under that threshold are '
-             'considered in the ventricles). [%(default)s]')
+             'considered in \nthe ventricles). [%(default)s]')
     g1.add_argument(
-        '--md_min', dest='md_min',  type=float, default='0.003',
+        '--md_ventricles', type=float, default=0.003,
         help='Minimal threshold of MD in mm2/s (voxels above that threshold '
-             'are considered for in the ventricles). [%(default)s]')
+             'are considered \nfor in the ventricles). [%(default)s]')
 
     g2 = p.add_argument_group('Regions options')
     g2.add_argument(
         '--roi_radius', type=int, default=20,
         help='Radius of the region used to estimate the priors. The roi will '
-             'be a cube spanning from ROI_CENTER in each direction. '
+             'be a cube spanning \nfrom ROI_CENTER in each direction. '
              '[%(default)s]')
     g2.add_argument(
-        '--roi_center', metavar='tuple(3)', nargs="+", type=int,
+        '--roi_center', metavar='pos', nargs=3, type=int,
         help='Center of the roi of size roi_radius used to estimate the '
-             'priors. [center of the 3D volume]')
+             'priors; a 3-value coordinate. \nIf not set, uses the center of '
+             'the 3D volume.')
 
     g3 = p.add_argument_group('Outputs')
     g3.add_argument('--out_txt_1fiber_para', metavar='FILE',
@@ -99,6 +98,7 @@ def main():
     args = parser.parse_args()
     logging.getLogger().setLevel(logging.getLevelName(args.verbose))
 
+    # Verifications
     assert_inputs_exist(parser, [args.in_AD, args.in_FA, args.in_MD,
                                  args.in_RD])
     assert_outputs_exist(parser, args, [],
@@ -110,6 +110,7 @@ def main():
 
     assert_same_resolution([args.in_AD, args.in_FA, args.in_MD, args.in_RD])
 
+    # Loading
     fa_img = nib.load(args.in_FA)
     fa_data = fa_img.get_fdata(dtype=np.float32)
     affine = fa_img.affine
@@ -118,10 +119,7 @@ def main():
     ad_data = nib.load(args.in_AD).get_fdata(dtype=np.float32)
     rd_data = nib.load(args.in_RD).get_fdata(dtype=np.float32)
 
-    mask_cc = np.zeros(fa_data.shape, dtype=np.uint8)
-    mask_vent = np.zeros(fa_data.shape, dtype=np.uint8)
-
-    # center
+    # Finding ROI center
     if args.roi_center is None:
         ci, cj, ck = np.array(fa_data.shape[:3]) // 2
     else:
@@ -132,76 +130,74 @@ def main():
         else:
             ci, cj, ck = args.roi_center
 
+    # Get values in the ROI
     w = args.roi_radius
-    fa_shape = fa_data.shape
-    roi_ad = ad_data[max(int(ci - w), 0): min(int(ci + w), fa_shape[0]),
-                     max(int(cj - w), 0): min(int(cj + w), fa_shape[1]),
-                     max(int(ck - w), 0): min(int(ck + w), fa_shape[2])]
-    roi_rd = rd_data[max(int(ci - w), 0): min(int(ci + w), fa_shape[0]),
-                     max(int(cj - w), 0): min(int(cj + w), fa_shape[1]),
-                     max(int(ck - w), 0): min(int(ck + w), fa_shape[2])]
-    roi_md = md_data[max(int(ci - w), 0): min(int(ci + w), fa_shape[0]),
-                     max(int(cj - w), 0): min(int(cj + w), fa_shape[1]),
-                     max(int(ck - w), 0): min(int(ck + w), fa_shape[2])]
-    roi_fa = fa_data[max(int(ci - w), 0): min(int(ci + w), fa_shape[0]),
-                     max(int(cj - w), 0): min(int(cj + w), fa_shape[1]),
-                     max(int(ck - w), 0): min(int(ck + w), fa_shape[2])]
+    roi_posx = slice(max(int(ci - w), 0), min(int(ci + w), fa_data.shape[0]))
+    roi_posy = slice(max(int(cj - w), 0), min(int(cj + w), fa_data.shape[1]))
+    roi_posz = slice(max(int(ck - w), 0), min(int(ck + w), fa_data.shape[2]))
+    roi_ad = ad_data[roi_posx, roi_posy, roi_posz]
+    roi_rd = rd_data[roi_posx, roi_posy, roi_posz]
+    roi_md = md_data[roi_posx, roi_posy, roi_posz]
+    roi_fa = fa_data[roi_posx, roi_posy, roi_posz]
 
-    logging.info('fa_min, fa_max, md_min: {}, {}, {}'.format(
-        args.fa_min, args.fa_max, args.md_min))
+    # Get information in single fiber voxels
+    # Taking voxels with FA < 0.95 just to avoid using weird broken voxels.
+    indices = np.where((roi_fa > args.fa_single_fiber) & (roi_fa < 0.95))
+    nb_voxels = roi_fa[indices].shape[0]
+    logging.info('Number of voxels found in single fiber area (FA in range '
+                 '{}-{}]: {}'.format(args.fa_single_fiber, 0.95, nb_voxels))
+    single_fiber_ad_mean = np.mean(roi_ad[indices])
+    single_fiber_ad_std = np.std(roi_ad[indices])
+    single_fiber_rd_mean = np.mean(roi_rd[indices])
+    single_fiber_rd_std = np.std(roi_rd[indices])
 
-    indices = np.where((roi_fa > args.fa_min) & (roi_fa < 0.95))
-    N = roi_ad[indices].shape[0]
-
-    logging.info('Number of voxels found in single fiber area: {}'.format(N))
-
-    cc_avg_para = np.mean(roi_ad[indices])
-    cc_std_para = np.std(roi_ad[indices])
-
-    cc_avg_perp = np.mean(roi_rd[indices])
-    cc_std_perp = np.std(roi_rd[indices])
-
+    # Create mask of single fiber in ROI
     indices[0][:] += ci - w
     indices[1][:] += cj - w
     indices[2][:] += ck - w
-    mask_cc[indices] = 1
+    mask_single_fiber = np.zeros(fa_data.shape, dtype=np.uint8)
+    mask_single_fiber[indices] = 1
 
-    indices = np.where((roi_md > args.md_min) & (roi_fa < args.fa_max))
-    N = roi_md[indices].shape[0]
-
-    logging.info('Number of voxels found in ventricles: {}'.format(N))
+    # Get information in ventricles
+    indices = np.where((roi_md > args.md_ventricles) &
+                       (roi_fa < args.fa_ventricles))
+    nb_voxels = roi_md[indices].shape[0]
+    logging.info('Number of voxels found in ventricles (FA < {} and MD > {}): '
+                 '{}'.format(args.fa_ventricles, args.md_ventricles, nb_voxels))
 
     vent_avg = np.mean(roi_md[indices])
     vent_std = np.std(roi_md[indices])
 
+    # Create mask of ventricle in ROI
     indices[0][:] += ci - w
     indices[1][:] += cj - w
     indices[2][:] += ck - w
+    mask_vent = np.zeros(fa_data.shape, dtype=np.uint8)
     mask_vent[indices] = 1
 
+    # Saving
     if args.out_mask_1fiber:
-        nib.save(nib.Nifti1Image(mask_cc, affine), args.out_mask_1fiber)
+        nib.save(nib.Nifti1Image(mask_single_fiber, affine),
+                 args.out_mask_1fiber)
 
     if args.out_mask_ventricles:
         nib.save(nib.Nifti1Image(mask_vent, affine), args.out_mask_ventricles)
 
     if args.out_txt_1fiber_para:
-        np.savetxt(args.out_txt_1fiber_para, [cc_avg_para], fmt='%f')
+        np.savetxt(args.out_txt_1fiber_para, [single_fiber_ad_mean], fmt='%f')
 
     if args.out_txt_1fiber_perp:
-        np.savetxt(args.out_txt_1fiber_perp, [cc_avg_perp], fmt='%f')
+        np.savetxt(args.out_txt_1fiber_perp, [single_fiber_rd_mean], fmt='%f')
 
     if args.out_txt_ventricles:
         np.savetxt(args.out_txt_ventricles, [vent_avg], fmt='%f')
 
-    logging.info("Average AD in single fiber areas: {} +- {}".format(
-                                                                cc_avg_para,
-                                                                cc_std_para))
-    logging.info("Average RD in single fiber areas: {} +- {}".format(
-                                                                cc_avg_perp,
-                                                                cc_std_perp))
-    logging.info("Average MD in ventricles: {} +- {}".format(vent_avg,
-                                                             vent_std))
+    logging.info("Average AD in single fiber areas: {} +- {}"
+                 .format(single_fiber_ad_mean, single_fiber_ad_std))
+    logging.info("Average RD in single fiber areas: {} +- {}"
+                 .format(single_fiber_rd_mean, single_fiber_rd_std))
+    logging.info("Average MD in ventricles: {} +- {}"
+                 .format(vent_avg, vent_std))
 
 
 if __name__ == "__main__":
