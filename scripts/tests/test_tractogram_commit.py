d8023df6b936660ab88bc075e868ecbe19f81af6
arnaudbore <arnaud.bore@gmail.com>
2024-03-06 19:34:03 +0000

    add temporary tests to show update is working

--- scripts/tests/test_tractogram_commit.py
+++ scripts/tests/test_tractogram_commit.py
@@ -40,3 +40,25 @@ def test_execution_commit_amico(script_runner):
                             '--iso_diff', '1.7E-3', '3.0E-3',
                             '--processes', '1')
     assert ret.success
+
+def test_execution_commit_amico_wo_mask(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_tracking = os.path.join(get_home(), 'commit_amico',
+                               'tracking.trk')
+    in_dwi = os.path.join(get_home(), 'commit_amico',
+                          'dwi.nii.gz')
+    in_bval = os.path.join(get_home(), 'commit_amico',
+                           'dwi.bval')
+    in_bvec = os.path.join(get_home(), 'commit_amico',
+                           'dwi.bvec')
+    in_peaks = os.path.join(get_home(), 'commit_amico',
+                            'peaks.nii.gz')
+    ret = script_runner.run('scil_tractogram_commit.py', in_tracking, in_dwi,
+                            in_bval, in_bvec, 'results_bzs/',
+                            '--b_thr', '30', '--nbr_dir', '500',
+                            '--nbr_iter', '500', '--in_peaks', in_peaks,
+                            '--para_diff', '1.7E-3',
+                            '--perp_diff', '1.19E-3', '0.85E-3', '0.51E-3', '0.17E-3',
+                            '--iso_diff', '1.7E-3', '3.0E-3',
+                            '--processes', '1')
+    assert ret.success
