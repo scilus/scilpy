9d4263352709ada545724ed7125685218e16ae09
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-03-14 14:39:19 -0400

    volume_crop: add more coverage to test

--- scripts/tests/test_volume_crop.py
+++ scripts/tests/test_volume_crop.py
@@ -20,5 +20,11 @@ def test_help_option(script_runner):
 def test_execution_processing(script_runner, monkeypatch):
     monkeypatch.chdir(os.path.expanduser(tmp_dir.name))
     in_dwi = os.path.join(SCILPY_HOME, 'processing', 'dwi.nii.gz')
-    ret = script_runner.run('scil_volume_crop.py', in_dwi, 'dwi_crop.nii.gz')
+    ret = script_runner.run('scil_volume_crop.py', in_dwi, 'dwi_crop.nii.gz',
+                            '--output_bbox', 'bbox.pickle')
+    assert ret.success
+
+    # Then try to load back the same box
+    ret = script_runner.run('scil_crop_volume.py', in_dwi, 'dwi_crop.nii.gz',
+                            '--input_bbox', 'bbox.pickle')
     assert ret.success
