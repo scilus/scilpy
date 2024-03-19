cd13d2d13955f898f837557ecc8ebf2b6cab21e4
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-03-19 14:29:48 -0400

    tractogram_apply_transform: just clarifying help

--- scripts/tests/test_tractogram_apply_transform.py
+++ scripts/tests/test_tractogram_apply_transform.py
@@ -11,22 +11,19 @@ from scilpy.io.fetcher import fetch_data, get_testing_files_dict
 fetch_data(get_testing_files_dict(), keys=['bst.zip'])
 tmp_dir = tempfile.TemporaryDirectory()
 
+in_model = os.path.join(SCILPY_HOME, 'bst', 'template', 'rpt_m.trk')
+in_fa = os.path.join(SCILPY_HOME, 'bst', 'fa.nii.gz')
+in_aff = os.path.join(SCILPY_HOME, 'bst', 'output0GenericAffine.mat')
+in_warp = os.path.join(SCILPY_HOME, 'bst', 'output1InverseWarp.nii.gz')
+
 
 def test_help_option(script_runner):
     ret = script_runner.run('scil_tractogram_apply_transform.py', '--help')
     assert ret.success
 
 
-def test_execution_bst(script_runner):
+def test_execution_inverse(script_runner):
     os.chdir(os.path.expanduser(tmp_dir.name))
-    in_model = os.path.join(SCILPY_HOME, 'bst', 'template',
-                            'rpt_m.trk')
-    in_fa = os.path.join(SCILPY_HOME, 'bst',
-                         'fa.nii.gz')
-    in_aff = os.path.join(SCILPY_HOME, 'bst',
-                          'output0GenericAffine.mat')
-    in_warp = os.path.join(SCILPY_HOME, 'bst',
-                           'output1InverseWarp.nii.gz')
     ret = script_runner.run('scil_tractogram_apply_transform.py',
                             in_model, in_fa, in_aff, 'rpt_m_warp.trk',
                             '--inverse', '--in_deformation', in_warp,
