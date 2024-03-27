2593d478539ed881c36d2dd966081e74bbe33d51
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-03-27 15:20:52 -0400

    extract u-shape: ok!

--- scripts/scil_tractogram_extract_ushape.py
+++ scripts/scil_tractogram_extract_ushape.py
@@ -27,7 +27,7 @@ from scilpy.io.utils import (add_json_args,
                              add_reference_arg,
                              assert_inputs_exist,
                              assert_outputs_exist,
-                             check_tracts_same_format)
+                             check_tracts_same_format, ranged_type)
 from scilpy.tractanalysis.bundle_operations import detect_ushape
 
 
@@ -38,9 +38,9 @@ def _build_arg_parser():
                    help='Tractogram input file name.')
     p.add_argument('out_tractogram',
                    help='Output tractogram file name.')
-    p.add_argument('--minU', default=0.5, type=float,
+    p.add_argument('--minU', default=0.5, type=ranged_type(float, -1.0, 1.0),
                    help='Min ufactor value. [%(default)s]')
-    p.add_argument('--maxU', default=1.0, type=float,
+    p.add_argument('--maxU', default=1.0, type=ranged_type(float, -1.0, 1.0),
                    help='Max ufactor value. [%(default)s]')
 
     p.add_argument('--remaining_tractogram', metavar='filename',
@@ -70,43 +70,41 @@ def main():
     check_tracts_same_format(parser, [args.in_tractogram, args.out_tractogram,
                                       args.remaining_tractogram])
 
-    if not (-1 <= args.minU <= 1 and -1 <= args.maxU <= 1):
-        parser.error('Min-Max ufactor "{},{}" '.format(args.minU, args.maxU) +
-                     'must be between -1 and 1.')
-
+    # Loading
     sft = load_tractogram_with_reference(parser, args, args.in_tractogram)
 
-    ids_c = detect_ushape(sft, args.minU, args.maxU)
-    ids_l = np.setdiff1d(np.arange(len(sft.streamlines)), ids_c)
-
-    if len(ids_c) == 0:
-        if args.no_empty:
-            logging.info("The file {} won't be written "
-                         "(0 streamline).".format(args.out_tractogram))
-            return
-
-        logging.info('The file {} contains 0 streamline.'.format(
-            args.out_tractogram))
-
-    save_tractogram(sft[ids_c], args.out_tractogram)
+    # Processing
+    ids_ushaped = detect_ushape(sft, args.minU, args.maxU)
+    ids_others = np.setdiff1d(np.arange(len(sft.streamlines)), ids_ushaped)
 
     if args.display_counts:
         sc_bf = len(sft.streamlines)
-        sc_af = len(ids_c)
+        sc_af = len(ids_ushaped)
         print(json.dumps({'streamline_count_before_filtering': int(sc_bf),
                          'streamline_count_after_filtering': int(sc_af)},
                          indent=args.indent))
 
+    # Saving
+    if len(ids_ushaped) == 0:
+        if args.no_empty:
+            logging.warning("The file {} won't be written (0 streamline)."
+                            .format(args.out_tractogram))
+        else:
+            logging.info('The file {} contains 0 streamline.'
+                         .format(args.out_tractogram))
+    if not (len(ids_ushaped) == 0 and args.no_empty):
+        save_tractogram(sft[ids_ushaped], args.out_tractogram)
+
     if args.remaining_tractogram:
-        if len(ids_l) == 0:
+        if len(ids_others) == 0:
             if args.no_empty:
                 logging.info("The file {} won't be written (0 streamline"
                              ").".format(args.remaining_tractogram))
                 return
-
-            logging.warning('No remaining streamlines.')
-
-        save_tractogram(sft[ids_l], args.remaining_tractogram)
+            else:
+                logging.warning('No remaining streamlines. Saving an empty '
+                                'tractogram')
+        save_tractogram(sft[ids_others], args.remaining_tractogram)
 
 
 if __name__ == "__main__":
