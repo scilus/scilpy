fa0cf9565bb015abd0824c28c5adf627fe9ff4a6
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-05-17 14:00:06 -0400

    recobundles: fix verbose option

--- scripts/scil_tractogram_segment_with_recobundles.py
+++ scripts/scil_tractogram_segment_with_recobundles.py
@@ -12,11 +12,10 @@ Hints:
 - The model needs to be cleaned and lightweight.
 - The transform should come from ANTs: (using the --inverse flag)
   >>> AntsRegistrationSyNQuick.sh -d 3 -m MODEL_REF -f SUBJ_REF
-  If you are unsure about the transformation 'direction' use the verbose
-option (-v) and try with and without the --inverse flag. If you are not using
-the right transformation 'direction' a warning will popup. If there is no
-warning in both case, it means the transformation is very close to identity and
-both 'directions' will work.
+  If you are unsure about the transformation 'direction', try with and without
+  the --inverse flag. If you are not using the right transformation 'direction'
+  a warning will pop up. If there is no warning in both cases, it means the
+  transformation is very close to identity and both 'directions' will work.
 
 Formerly: scil_recognize_single_bundles.py
 """
@@ -38,7 +37,7 @@ from scilpy.io.utils import (add_overwrite_arg,
                              add_verbose_arg,
                              assert_inputs_exist,
                              assert_outputs_exist,
-                             load_matrix_in_any_format)
+                             load_matrix_in_any_format, ranged_type)
 from scilpy.utils.spatial import compute_distance_barycenters
 
 EPILOG = """
@@ -63,18 +62,22 @@ def _build_arg_parser():
     p.add_argument('out_tractogram',
                    help='Output tractogram filename.')
 
-    p.add_argument('--tractogram_clustering_thr', type=float, default=8,
+    g = p.add_argument_group("Recobundles options")
+    g.add_argument('--tractogram_clustering_thr',
+                   type=ranged_type(float, 0, None),
                    help='Clustering threshold used for the whole brain '
-                        '[%(default)smm].')
-    p.add_argument('--model_clustering_thr', type=float, default=4,
+                        'Default: 8.')
+    g.add_argument('--model_clustering_thr',
+                   type=ranged_type(float, 0, None), default=4,
                    help='Clustering threshold used for the model '
                         '[%(default)smm].')
-    p.add_argument('--pruning_thr', type=float, default=6,
+    g.add_argument('--pruning_thr',
+                   type=ranged_type(float, 0, None), default=6,
                    help='MDF threshold used for final streamlines selection '
                         '[%(default)smm].')
-
-    p.add_argument('--slr_threads', type=int, default=1,
+    g.add_argument('--slr_threads', type=int, default=1,
                    help='Number of threads for SLR [%(default)s].')
+
     p.add_argument('--seed', type=int, default=None,
                    help='Random number generator seed [%(default)s].')
     p.add_argument('--inverse', action='store_true',
@@ -101,54 +104,70 @@ def main():
     args = parser.parse_args()
     logging.getLogger().setLevel(logging.getLevelName(args.verbose))
 
+    # Verifications
     assert_inputs_exist(parser, [args.in_tractogram, args.in_transfo],
-                        args.reference)
-    assert_outputs_exist(parser, args, args.out_tractogram)
+                        [args.reference, args.in_pickle])
+    assert_outputs_exist(parser, args, args.out_tractogram,
+                         args.out_pickle)
 
-    wb_file = load_tractogram_with_reference(parser, args, args.in_tractogram)
-    wb_streamlines = wb_file.streamlines
-    model_file = load_tractogram_with_reference(parser, args, args.in_model)
+    if args.tractogram_clustering_thr and args.in_pickle:
+        parser.error("Option --tractogram_clustering_thr should not be "
+                     "used with --in_pickle.")
+    else:
+        # Setting default value. (Will be ignored in args.in_pickle)
+        args.tractogram_clustering_thr = 8.0
 
+    # Loading
+    wb_sft = load_tractogram_with_reference(parser, args, args.in_tractogram)
+    model_sft = load_tractogram_with_reference(parser, args, args.in_model)
     transfo = load_matrix_in_any_format(args.in_transfo)
-    if args.inverse:
-        transfo = np.linalg.inv(load_matrix_in_any_format(args.in_transfo))
 
-    before, after = compute_distance_barycenters(wb_file, model_file, transfo)
+    # Processing
+    if args.inverse:
+        transfo = np.linalg.inv(transfo)
+
+    # 1) Register the streamlines to the model space (apply transform)
+    before, after = compute_distance_barycenters(wb_sft, model_sft, transfo)
     if after > before:
         logging.warning('The distance between volumes barycenter should be '
                         'lower after registration. Maybe try using/removing '
                         '--inverse.')
         logging.info('Distance before: {}, Distance after: {}'.format(
             np.round(before, 3), np.round(after, 3)))
-    model_streamlines = transform_streamlines(model_file.streamlines, transfo)
+    model_streamlines = transform_streamlines(model_sft.streamlines, transfo)
 
+    # 2) Prepare Recobundles object
     rng = np.random.RandomState(args.seed)
+    verbose = args.verbose in ['DEBUG', 'INFO']
     if args.in_pickle:
         with open(args.in_pickle, 'rb') as infile:
             cluster_map = pickle.load(infile)
-        reco_obj = RecoBundles(wb_streamlines,
+        reco_obj = RecoBundles(wb_sft.streamlines,
                                cluster_map=cluster_map,
                                rng=rng, less_than=1,
-                               verbose=args.verbose)
+                               verbose=verbose)
     else:
-        reco_obj = RecoBundles(wb_streamlines,
+        reco_obj = RecoBundles(wb_sft.streamlines,
                                clust_thr=args.tractogram_clustering_thr,
                                rng=rng, greater_than=1,
-                               verbose=args.verbose)
+                               verbose=verbose)
 
     if args.out_pickle:
+        logging.info("Saving out_pickle")
         with open(args.out_pickle, 'wb') as outfile:
             pickle.dump(reco_obj.cluster_map, outfile)
+
+    # 3) Run recobundle.
     _, indices = reco_obj.recognize(ArraySequence(model_streamlines),
                                     args.model_clustering_thr,
                                     pruning_thr=args.pruning_thr,
                                     num_threads=args.slr_threads)
-    new_streamlines = wb_streamlines[indices]
-    new_data_per_streamlines = wb_file.data_per_streamline[indices]
-    new_data_per_points = wb_file.data_per_point[indices]
 
+    # Save results
+    bundle_sft = wb_sft[indices]
+    save_tractogram(sft, filename, no_empty)
     if not args.no_empty or new_streamlines:
-        sft = StatefulTractogram(new_streamlines, wb_file.space_attributes,
+        sft = StatefulTractogram(new_streamlines, wb_sft.space_attributes,
                                  Space.RASMM,
                                  data_per_streamline=new_data_per_streamlines,
                                  data_per_point=new_data_per_points)
