f67cf16890a0391706e478500ccf83e5d512109c
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-06-18 17:11:47 -0400

    autopep8 correction

--- scilpy/segment/tests/test_tractogram_from_roi.py
+++ scilpy/segment/tests/test_tractogram_from_roi.py
@@ -6,7 +6,8 @@ import numpy as np
 from numpy.testing import (assert_array_equal,
                            assert_equal)
 
-from scilpy.segment.tractogram_from_roi import _extract_vb_one_bundle, _extract_ib_one_bundle
+from scilpy.segment.tractogram_from_roi import (_extract_vb_one_bundle,
+                                                _extract_ib_one_bundle)
 from dipy.io.stateful_tractogram import Space, StatefulTractogram
 
 
@@ -21,28 +22,17 @@ def test_extract_vb_one_bundle():
     with tempfile.TemporaryDirectory() as tmp_dir:
         fake_mask1_name = os.path.join(tmp_dir, 'fake_mask1.nii.gz')
         fake_mask2_name = os.path.join(tmp_dir, 'fake_mask2.nii.gz')
-        nib.save(
-            nib.Nifti1Image(
-                np.zeros(
-                    (10,
-                     10,
-                     10)),
-                affine=np.eye(4),
-                dtype=np.int8),
-            fake_mask1_name)
-        nib.save(
-            nib.Nifti1Image(
-                np.zeros(
-                    (10,
-                     10,
-                     10)),
-                affine=np.eye(4),
-                dtype=np.int8),
-            fake_mask2_name)
+        nib.save(nib.Nifti1Image(np.zeros((10, 10, 10)),
+                 affine=np.eye(4), dtype=np.int8), fake_mask1_name)
+        nib.save(nib.Nifti1Image(np.zeros((10, 10, 10)),
+                 affine=np.eye(4), dtype=np.int8), fake_mask2_name)
 
-        vs_ids, wpc_ids, bundle_stats = _extract_vb_one_bundle(empty_sft, fake_mask1_name, fake_mask2_name,
-                                                               None, None, None,
-                                                               None, None, None, None)
+        vs_ids, wpc_ids, bundle_stats = \
+            _extract_vb_one_bundle(empty_sft,
+                                   fake_mask1_name,
+                                   fake_mask2_name,
+                                   None, None, None,
+                                   None, None, None, None)
         assert_array_equal(vs_ids, [])
         assert_array_equal(wpc_ids, [])
         assert_equal(bundle_stats["VS"], 0)
@@ -59,24 +49,10 @@ def test_extract_ib_one_bundle():
     with tempfile.TemporaryDirectory() as tmp_dir:
         fake_mask1_name = os.path.join(tmp_dir, 'fake_mask1.nii.gz')
         fake_mask2_name = os.path.join(tmp_dir, 'fake_mask2.nii.gz')
-        nib.save(
-            nib.Nifti1Image(
-                np.zeros(
-                    (10,
-                     10,
-                     10)),
-                affine=np.eye(4),
-                dtype=np.int8),
-            fake_mask1_name)
-        nib.save(
-            nib.Nifti1Image(
-                np.zeros(
-                    (10,
-                     10,
-                     10)),
-                affine=np.eye(4),
-                dtype=np.int8),
-            fake_mask2_name)
+        nib.save(nib.Nifti1Image(np.zeros((10, 10, 10)),
+                 affine=np.eye(4), dtype=np.int8), fake_mask1_name)
+        nib.save(nib.Nifti1Image(np.zeros((10, 10, 10)),
+                 affine=np.eye(4), dtype=np.int8), fake_mask2_name)
 
         fc_sft, fc_ids = _extract_ib_one_bundle(
             empty_sft, fake_mask1_name, fake_mask2_name, None)
