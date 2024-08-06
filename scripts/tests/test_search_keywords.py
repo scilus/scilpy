62ac325e75538ecad52f395f53ac1663bd3ee562
jinan02 <boufaris.jinane@gmail.com>
2024-08-05 20:22:18 -0400

    add a test for the search_category and no_synonyms args

--- scripts/tests/test_search_keywords.py
+++ scripts/tests/test_search_keywords.py
@@ -6,17 +6,15 @@ def test_help_option(script_runner):
     ret = script_runner.run('scil_search_keywords.py', '--help')
     assert ret.success
 
+def test_search_category(script_runner):
+    ret = script_runner.run('scil_search_keywords.py', '--search_category', 'sh')
+    assert 'Available objects:' in ret.stdout
 
-def test_no_verbose(script_runner):
-    ret = script_runner.run('scil_search_keywords.py', 'mti')
+def test_no_synonyms(script_runner):
+    ret = script_runner.run('scil_search_keywords.py', 'sh', '--no_synonyms')
     assert ret.success
 
-
-def test_verbose_option(script_runner):
-    ret = script_runner.run('scil_search_keywords.py', 'mti', '-v')
-    assert ret.success
-
-
-def test_not_find(script_runner):
+def test_not_found(script_runner):
     ret = script_runner.run('scil_search_keywords.py', 'toto')
     assert ret.success
+    assert 'No results found!' in ret.stdout or 'No results found!' in ret.stderr
