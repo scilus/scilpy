dd0799e70f0d980db491d0fcb5d5cb6af4335d72
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-09-05 12:43:39 -0400

    Remove 16x4 redondant imports

--- scilpy/tractograms/tests/test_streamline_operations.py
+++ scilpy/tractograms/tests/test_streamline_operations.py
@@ -25,39 +25,14 @@ from scilpy.tractograms.tractogram_operations import concatenate_sft
 fetch_data(get_testing_files_dict(), keys=['tractograms.zip'])
 tmp_dir = tempfile.TemporaryDirectory()
 
-
-def _setup_files():
-    """ Load streamlines and masks relevant to the tests here.
-    """
-
-    os.chdir(os.path.expanduser(tmp_dir.name))
-    in_long_sft = os.path.join(SCILPY_HOME, 'tractograms',
-                               'streamline_operations',
-                               'bundle_4.tck')
-    in_mid_sft = os.path.join(SCILPY_HOME, 'tractograms',
-                              'streamline_operations',
-                              'bundle_4_cut_endpoints.tck')
-    in_short_sft = os.path.join(SCILPY_HOME, 'tractograms',
-                                'streamline_operations',
-                                'bundle_4_cut_center.tck')
-    in_ref = os.path.join(SCILPY_HOME, 'tractograms',
-                          'streamline_operations',
-                          'bundle_4_wm.nii.gz')
-
-    in_rois = os.path.join(SCILPY_HOME, 'tractograms',
-                           'streamline_operations',
-                           'bundle_4_head_tail_offset.nii.gz')
-
-    # Load sft
-    long_sft = load_tractogram(in_long_sft, in_ref)
-    mid_sft = load_tractogram(in_mid_sft, in_ref)
-    short_sft = load_tractogram(in_short_sft, in_ref)
-
-    sft = concatenate_sft([long_sft, mid_sft, short_sft])
-
-    # Load mask
-    rois = nib.load(in_rois)
-    return sft, rois
+# Streamlines and masks relevant to the tests here.
+test_files_path = os.path.join(SCILPY_HOME, 'tractograms',
+                               'streamline_operations')
+in_long_sft = os.path.join(test_files_path, 'bundle_4.tck')
+in_mid_sft = os.path.join(test_files_path, 'bundle_4_cut_endpoints.tck')
+in_short_sft = os.path.join(test_files_path, 'bundle_4_cut_center.tck')
+in_ref = os.path.join(test_files_path, 'bundle_4_wm.nii.gz')
+in_rois = os.path.join(test_files_path, 'bundle_4_head_tail_offset.nii.gz')
 
 
 def test_get_angles():
@@ -68,7 +43,8 @@ def test_get_angles():
     fake_ninety_degree = np.asarray([[0, 0, 0],
                                      [1, 1, 0],
                                      [0, 2, 0]], dtype=float)
-    sft, _ = _setup_files()
+
+    sft = load_tractogram(in_short_sft, in_ref)
     sft.streamlines = [fake_straight_line, fake_ninety_degree]
 
     angles = get_angles(sft)
@@ -95,12 +71,13 @@ def test_cut_invalid_streamlines():
     pass
 
 
-def test_filter_streamlines_by_length_max_length():
-    """ Test the filter_streamlines_by_length function with a max length.
-    """
+def test_filter_streamlines_by_length():
+    long_sft = load_tractogram(in_long_sft, in_ref)
+    mid_sft = load_tractogram(in_mid_sft, in_ref)
+    short_sft = load_tractogram(in_short_sft, in_ref)
+    sft = concatenate_sft([long_sft, mid_sft, short_sft])
 
-    sft, _ = _setup_files()
-
+    # === 1. Using max length ===
     min_length = 0.
     max_length = 100
     # Filter streamlines by length and get the lengths
@@ -113,13 +90,7 @@ def test_filter_streamlines_by_length_max_length():
 
     assert np.all(lengths <= max_length)
 
-
-def test_filter_streamlines_by_length_min_length():
-    """ Test the filter_streamlines_by_length function with a min length.
-    """
-
-    sft, _ = _setup_files()
-
+    # === 2. Using min length ===
     min_length = 100
     max_length = np.inf
 
@@ -133,14 +104,7 @@ def test_filter_streamlines_by_length_min_length():
     # Test that streamlines shorter than 100 were removed.
     assert np.all(lengths >= min_length)
 
-
-def test_filter_streamlines_by_length_min_and_max_length():
-    """ Test the filter_streamlines_by_length function with a min
-    and max length.
-    """
-
-    sft, _ = _setup_files()
-
+    # === 3. Using both min and max length ===
     min_length = 100
     max_length = 120
 
@@ -155,21 +119,18 @@ def test_filter_streamlines_by_length_min_and_max_length():
     assert np.all(lengths >= min_length) and np.all(lengths <= max_length)
 
 
-def test_filter_streamlines_by_total_length_per_dim_x():
-    """ Test the filter_streamlines_by_total_length_per_dim function.
-    This function is quite awkward to test without reimplementing
-    the logic, but luckily we have data going purely left-right.
-
-    This test also tests the return of rejected streamlines.
-    """
-
-    # Streamlines are going purely left-right, so the
-    # x dimension should have the longest span.
-    sft, _ = _setup_files()
+def test_filter_streamlines_by_total_length_per_dim():
+    long_sft = load_tractogram(in_long_sft, in_ref)
+    mid_sft = load_tractogram(in_mid_sft, in_ref)
+    short_sft = load_tractogram(in_short_sft, in_ref)
+    sft = concatenate_sft([long_sft, mid_sft, short_sft])
 
     min_length = 115
     max_length = 125
 
+    # === 1. Test x dimension ===
+    # Test sft has streamlines that are going purely left-right, so the x
+    # dimension should have the longest span.
     constraint = [min_length, max_length]
     inf_constraint = [-np.inf, np.inf]
 
@@ -177,7 +138,7 @@ def test_filter_streamlines_by_total_length_per_dim_x():
     # No rejected streamlines should be returned
     filtered_sft, ids, rejected = filter_streamlines_by_total_length_per_dim(
         sft, constraint, inf_constraint, inf_constraint,
-        True, False)
+        use_abs=True, save_rejected=False)
     lengths = length(filtered_sft.streamlines)
 
     # Test that streamlines were removed and that the test is not bogus.
@@ -187,26 +148,11 @@ def test_filter_streamlines_by_total_length_per_dim_x():
     # No rejected streamlines should have been returned
     assert rejected is None
 
-
-def test_filter_streamlines_by_total_length_per_dim_y():
-    """ Test the filter_streamlines_by_total_length_per_dim function.
-    This function is quite awkward to test without reimplementing
-    the logic. We rotate the streamlines to be purely up-down.
-
-    This test also tests the return of rejected streamlines. The rejected
-    streamlines should have "invalid" lengths.
-    """
-
-    # Streamlines are going purely left-right, so the
-    # streamlines have to be rotated to be purely up-down.
-    sft, _ = _setup_files()
+    # === 2. Testing y dimension ===
 
     # Rotate streamlines by swapping x and y for all streamlines
     swapped_streamlines_y = [s[:, [1, 0, 2]] for s in sft.streamlines]
     sft_y = sft.from_sft(swapped_streamlines_y, sft)
-
-    min_length = 115
-    max_length = 125
 
     constraint = [min_length, max_length]
     inf_constraint = [-np.inf, np.inf]
@@ -224,17 +170,7 @@ def test_filter_streamlines_by_total_length_per_dim_y():
     assert np.all(np.logical_or(min_length > rejected_lengths,
                                 rejected_lengths > max_length))
 
-
-def test_filter_streamlines_by_total_length_per_dim_z():
-    """ Test the filter_streamlines_by_total_length_per_dim function.
-    This function is quite awkward to test without reimplementing
-    the logic.
-    """
-
-    # Streamlines are going purely left-right, so the
-    # streamlines have to be rotated to be purely front-back.
-    sft, _ = _setup_files()
-
+    # === 3. Testing z dimension ===
     # Rotate streamlines by swapping x and z for all streamlines
     swapped_streamlines_y = [s[:, [2, 1, 0]] for s in sft.streamlines]
     sft_y = sft.from_sft(swapped_streamlines_y, sft)
@@ -253,42 +189,38 @@ def test_filter_streamlines_by_total_length_per_dim_z():
 
     # Test that streamlines were removed and that the test is not bogus.
     assert len(filtered_sft) < len(sft)
-
     assert np.all(lengths >= min_length) and np.all(lengths <= max_length)
 
 
-def test_resample_streamlines_num_points_2():
-    """ Test the resample_streamlines_num_points function to 2 points.
-    """
+def test_resample_streamlines_num_points():
+    long_sft = load_tractogram(in_long_sft, in_ref)
+    mid_sft = load_tractogram(in_mid_sft, in_ref)
+    short_sft = load_tractogram(in_short_sft, in_ref)
+    sft = concatenate_sft([long_sft, mid_sft, short_sft])
 
-    sft, _ = _setup_files()
+    # Test 1. To two points
     nb_points = 2
-
     resampled_sft = resample_streamlines_num_points(sft, nb_points)
     lengths = [len(s) == nb_points for s in resampled_sft.streamlines]
-
     assert np.all(lengths)
 
-
-def test_resample_streamlines_num_points_1000():
-    """ Test the resample_streamlines_num_points function to 1000 points.
-    """
-
-    sft, _ = _setup_files()
+    # Test 2. To 1000 points.
     nb_points = 1000
-
     resampled_sft = resample_streamlines_num_points(sft, nb_points)
     lengths = [len(s) == nb_points for s in resampled_sft.streamlines]
 
     assert np.all(lengths)
 
 
-def test_resample_streamlines_step_size_1mm():
+def test_resample_streamlines_step_size():
     """ Test the resample_streamlines_step_size function to 1mm.
     """
+    long_sft = load_tractogram(in_long_sft, in_ref)
+    mid_sft = load_tractogram(in_mid_sft, in_ref)
+    short_sft = load_tractogram(in_short_sft, in_ref)
+    sft = concatenate_sft([long_sft, mid_sft, short_sft])
 
-    sft, _ = _setup_files()
-
+    # Test 1. To 1 mm
     step_size = 1.0
     resampled_sft = resample_streamlines_step_size(sft, step_size)
 
@@ -299,13 +231,7 @@ def test_resample_streamlines_step_size_1mm():
     # Tolerance of 10% of the step size
     assert np.allclose(steps, step_size, atol=0.1), steps
 
-
-def test_resample_streamlines_step_size_01mm():
-    """ Test the resample_streamlines_step_size function to 0.1mm.
-    """
-
-    sft, _ = _setup_files()
-
+    # Test 2. To 0.1 mm
     step_size = 0.1
     resampled_sft = resample_streamlines_step_size(sft, step_size)
 
@@ -323,7 +249,7 @@ def test_smooth_line_gaussian_error():
     value of 0, therefore it should throw and error.
     """
 
-    sft, _ = _setup_files()
+    sft = load_tractogram(in_long_sft, in_ref)
     streamline = sft.streamlines[0]
 
     # Add noise to the streamline
@@ -339,8 +265,7 @@ def test_smooth_line_gaussian():
     streamline and smoothing it. The smoothed streamline should be
     closer to the original streamline than the noisy one.
     """
-
-    sft, _ = _setup_files()
+    sft = load_tractogram(in_long_sft, in_ref)
     streamline = sft.streamlines[0]
 
     rng = np.random.default_rng(1337)
@@ -367,8 +292,7 @@ def test_smooth_line_spline_error():
     streamline and smoothing it. The function does not accept a sigma
     value of 0, therefore it should throw and error.
     """
-
-    sft, _ = _setup_files()
+    sft = load_tractogram(in_long_sft, in_ref)
     streamline = sft.streamlines[0]
 
     # Add noise to the streamline
@@ -384,8 +308,7 @@ def test_smooth_line_spline():
     streamline and smoothing it. The smoothed streamline should be
     closer to the original streamline than the noisy one.
     """
-
-    sft, _ = _setup_files()
+    sft = load_tractogram(in_short_sft, in_ref)
     streamline = sft.streamlines[-1]
 
     rng = np.random.default_rng(1337)
@@ -413,7 +336,7 @@ def test_generate_matched_points():
 
 
 def test_parallel_transport_streamline():
-    sft, _ = _setup_files()
+    sft = load_tractogram(in_long_sft, in_ref)
     streamline = sft.streamlines[0]
 
     rng = np.random.default_rng(3018)
