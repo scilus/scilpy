d5ca0a0ca7196c122fb5266788192a3319b722dc
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-09-17 14:56:27 -0400

    First draft, adding back piesno

--- scilpy/stats/stats.py
+++ scilpy/stats/stats.py
@@ -3,26 +3,54 @@
 from itertools import combinations
 import logging
 
-from dipy.denoise.noise_estimate import estimate_sigma
+from dipy.denoise.noise_estimate import estimate_sigma, piesno
 import numpy as np
 import scipy.stats
 
 
-def get_std(data):
+def estimate_piesno_sigma(data, number_coils=0):
     """
+    Here are Dipy's note on this method:
+     > It is expected that
+     >   1. The data has a noisy, non-masked background and
+     >   2. The data is a repetition of the same measurements along the last
+     >      axis, i.e. dMRI or fMRI data, not structural data like T1/T2."
 
+    Parameters
+    ----------
+    data: np.ndarray
+        The 4D volume.
     """
-    # Using dipy's method
-    # We force to zero as the 3T is either over-smoothed or still noisy, but
-    # we prefer the second option
+    assert len(data.shape) == 4
 
-    # In basic noise estimation, number_coils=0 is enforced!
-    sigma = estimate_sigma(data, N=0)
-    sigma = np.median(sigma)
+    sigma = np.zeros(data.shape[:3], dtype=np.float32)
+    mask_noise = np.zeros(data.shape[:3], dtype=np.int16)
 
+    for idx in range(data.shape[-2]):
+        logging.info('Now processing slice {} / {}}'
+                     .format(idx + 1, data.shape[-2]))
+        sigma[..., idx], mask_noise[..., idx] = \
+            piesno(data[..., idx, :], N=number_coils, return_mask=True)
 
-    # Broadcast the single value to a whole 3D volume for nlmeans
-    return sigma
+    # If the noise mask has few voxels, the detected noise standard
+    # deviation can be very low and maybe something went wrong. We
+    # check here that at least 1% of noisy voxels were found and warn
+    # the user otherwise.
+    frac_noisy_voxels = np.sum(mask_noise) / np.size(mask_noise) * 100
+
+    if frac_noisy_voxels < 1.:
+        logging.warning(
+            'PIESNO was used with N={}, but it found only {:.3f}% of voxels '
+            'as pure noise with a mean standard deviation of {:.5f}. This is '
+            'suspicious, so please check the resulting sigma volume if '
+            'something went wrong. In cases where PIESNO is not working, '
+            'you might want to try basic sigma estimation.'
+            .format(number_coils, frac_noisy_voxels, np.mean(sigma)))
+    else:
+        logging.info('The noise standard deviation from piesno is %s',
+                     np.array_str(sigma[0, 0, :]))
+
+    return sigma, mask_noise
 
 
 def verify_normality(data, alpha=0.05):
