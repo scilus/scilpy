ac3db46dded8f6ae0fee170c9d0adb1d55ef8f95
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-11-07 16:52:20 -0500

    Test with specific setup_requires

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ opts = dict(name=NAME,
             },
             ext_modules=get_extensions(),
             python_requires=PYTHON_VERSION,
-            setup_requires=[],
+            setup_requires=['cython==3.0.*', 'numpy==1.25.*'],
             install_requires=external_dependencies,
             entry_points={
                 'console_scripts': ["{}=scripts.{}:main".format(
