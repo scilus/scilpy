423d4ad5b1ea40e97a59e9c97d4704ce6559c468
karp2601 <karp2601@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2024-11-08 13:16:28 -0500

    WIP adding tests for norm

--- scripts/tests/test_bundle_fixel_analysis.py
+++ scripts/tests/test_bundle_fixel_analysis.py
@@ -43,5 +43,24 @@ def test_all_parameters(script_runner, monkeypatch):
                             '--processes', '1', '-f')
     assert ret.success
 
+
+def test_multiple_norm(script_runner, monkeypatch):
+    monkeypatch.chdir(os.path.expanduser(tmp_dir.name))
+    in_peaks = os.path.join(SCILPY_HOME, 'commit_amico', 'peaks.nii.gz')
+    in_bundle = os.path.join(SCILPY_HOME, 'commit_amico', 'tracking.trk')
+
+    ret = script_runner.run('scil_bundle_fixel_analysis.py', in_peaks,
+                            '--in_bundles', in_bundle,
+                            '--in_bundles_names', 'test',
+                            '--abs_thr', '5',
+                            '--rel_thr', '0.05',
+                            '--norm', 'fixel', 'none', 'voxel',
+                            '--split_bundles', '--split_fixels',
+                            '--single_bundle',
+                            '--processes', '1', '-f')
+    assert ret.success
+    print(tmp_dir.name + "/fixel_density_maps.nii.gz")
+    assert os.path.exists(tmp_dir.name + "/fixel_density_maps.nii.gz")
+
 # We would need a tractogram with data_per_streamline to test the --dps_key
 # option
