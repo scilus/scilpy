8a87d04c12a1225efb5b024292f92e8bda0e34e3
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-11-29 11:59:56 -0500

    Compute_pca: big refactor

--- scripts/scil_connectivity_compute_pca.py
+++ scripts/scil_connectivity_compute_pca.py
@@ -2,30 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Script to compute PCA analysis on diffusion metrics. Output returned is all
-significant principal components (e.g. presenting eigenvalues > 1) in a
-connectivity matrix format. This script can take into account all edges from
-every subject in a population or only non-zero edges across all subjects.
-
-The script can take directly as input a connectoflow output folder. Simply use
-the --input_connectoflow flag. For other type of folder input, the script
-expects a single folder containing all matrices for all subjects.
-Example:
-        [in_folder]
-        |--- sub-01_ad.npy
-        |--- sub-01_md.npy
-        |--- sub-02_ad.npy
-        |--- sub-02_md.npy
-        |--- ...
-
-The plots, tables and principal components matrices will be outputted in the
-designated folder from the <out_folder> argument. If you want to move back your
-principal components matrices in your connectoflow output, you can use a
-similar bash command for all principal components:
-for sub in `cat list_id.txt`;
-do
-    cp out_folder/${sub}_PC1.npy connectoflow_output/$sub/Compute_Connectivity/
-done
+Script to compute PCA analysis on a set of connectivity matrices. The output is
+all significant principal components in a connectivity matrix format.
+This script can take into account all edges from every subject in a population
+or only non-zero edges across all subjects.
 
 Interpretation of resulting principal components can be done by evaluating the
 loadings values for each metrics. A value near 0 means that this metric doesn't
@@ -36,14 +16,40 @@ high loading for afd_fixel and near 0 loading for all other metrics can be
 interpreted as axonal density (see Gagnon et al. 2022 for this specific example
 or ref [3] for an introduction to PCA).
 
+The script can take directly as input a connectoflow output folder. Simply use
+the --input_connectoflow flag. Else, the script expects a single folder
+containing all matrices for all subjects. Those matrices can be obtained, for
+instance, by scil_connectivity_compute_matrices.py.
+Example: Default input
+        [in_folder]
+        |--- sub-01_ad.npy
+        |--- sub-01_md.npy
+        |--- sub-02_ad.npy
+        |--- sub-02_md.npy
+        |--- ...
+Connectoflow input:
+        [in_folder]
+              [subj-01]
+                   [Compute_Connectivity]
+                       |--- ad.npy
+
+The plots, tables and principal components matrices will be saved in the
+designated folder from the <out_folder> argument. If you want to move back your
+principal components matrices in your connectoflow output, you can use a
+similar bash command for all principal components:
+for sub in `cat list_id.txt`;
+do
+    cp out_folder/${sub}_PC1.npy connectoflow_output/$sub/Compute_Connectivity/
+done
+
 EXAMPLE USAGE:
 scil_connectivity_compute_pca.py input_folder/ output_folder/
     --metrics ad fa md rd [...] --list_ids list_ids.txt
 """
 
-# Import required libraries.
 import argparse
 import logging
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -55,7 +61,8 @@ from scilpy.io.utils import (load_matrix_in_any_format,
                              save_matrix_in_any_format,
                              add_verbose_arg,
                              add_overwrite_arg,
-                             assert_output_dirs_exist_and_empty)
+                             assert_output_dirs_exist_and_empty,
+                             assert_inputs_dirs_exist, assert_inputs_exist)
 
 
 EPILOG = """
@@ -91,7 +98,7 @@ def _build_arg_parser():
                         'followed by the .npy extension.')
     p.add_argument('--list_ids', required=True, metavar='FILE',
                    help='Path to a .txt file containing a list of all ids.')
-    p.add_argument('--not_only_common', action='store_true',
+    p.add_argument('--all_edges', action='store_true',
                    help='If true, will include all edges from all subjects '
                         'and not only \ncommon edges (Not recommended)')
     p.add_argument('--input_connectoflow', action='store_true',
@@ -104,41 +111,76 @@ def _build_arg_parser():
     return p
 
 
-def generate_pca_input(dictionary):
-    """
-    Function to create PCA input from matrix.
-    :param dictionary:       Dictionary with metrics as keys containing list of matrices sorted by ids.
-    :return:                 Numpy array.
-    """
-    for metric in dictionary.keys():
-        mat = np.rollaxis(np.array(dictionary[metric]), axis=1, start=3)
-        matrix_shape = mat.shape[1:3]
-        n_group = mat.shape[0]
-        mat = mat.reshape((np.prod(matrix_shape) * n_group, 1))
-        dictionary[metric] = mat
+def plot_eigenvalues(pca, principaldf, nb_metrics, out_file):
+    # Plot the eigenvalues.
+    logging.info('Plotting results...')
+    eigenvalues = pca.explained_variance_
+    pos = list(range(1, nb_metrics + 1))
+    plt.clf()
+    plt.cla()
 
-    return np.hstack([dictionary[i] for i in dictionary.keys()])
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    bar_eig = ax.bar(pos, eigenvalues, align='center',
+                     tick_label=principaldf.columns)
+    ax.set_xlabel('Principal Components', fontsize=10)
+    ax.set_ylabel('Eigenvalues', fontsize=10)
+    ax.set_title('Eigenvalues for each principal components.', fontsize=10)
+    ax.margins(0.1)
+    autolabel(bar_eig, ax)
+    plt.savefig(out_file)
+    return eigenvalues
 
 
-def restore_zero_values(orig, new):
-    """
-    Function to restore 0 values in a numpy array.
-    :param orig:    Original numpy array containing 0 values to restore.
-    :param new:     Array in which 0 values need to be restored.
-    :return:        Numpy array with data from the new array but zeros from the original array.
-    """
-    mask = np.copy(orig)
-    mask[mask != 0] = 1
+def plot_explained_variance(pca, principaldf, nb_metrics, out_file):
+    # Plot the explained variance.
+    explained_var = pca.explained_variance_ratio_
+    pos = list(range(1, nb_metrics + 1))
+    plt.clf()
+    plt.cla()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    bar_var = ax.bar(pos, explained_var, align='center',
+                     tick_label=principaldf.columns)
+    ax.set_xlabel('Principal Components', fontsize=10)
+    ax.set_ylabel('Explained variance', fontsize=10)
+    ax.set_title('Amount of explained variance for all principal components.',
+                 fontsize=10)
+    ax.margins(0.1)
+    autolabel(bar_var, ax)
+    plt.savefig(out_file)
 
-    return np.multiply(new, mask)
+
+def plot_contribution(pca, principaldf, metrics, out_excel, out_image):
+    # Plot the contribution of each measures to principal component.
+    component = pca.components_
+    output_component = pd.DataFrame(component, index=principaldf.columns,
+                                    columns=metrics)
+    output_component.to_excel(out_excel, index=True, header=True)
+    plt.clf()
+    plt.cla()
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Graph of the contribution of each measures to the first '
+                 'and second principal component.', fontsize=10)
+    pos = list(range(1, len(metrics) + 1))
+    bar_pc1 = axs[0].bar(pos, component[0], align='center',
+                         tick_label=metrics)
+    bar_pc2 = axs[1].bar(pos, component[1], align='center',
+                         tick_label=metrics)
+    axs[0].margins(0.2)
+    axs[1].margins(0.2)
+    autolabel(bar_pc1, axs[0])
+    autolabel(bar_pc2, axs[1])
+    for ax in axs.flat:
+        ax.set(xlabel='Diffusion measures', ylabel='Loadings')
+    for ax in axs.flat:
+        ax.label_outer()
+    plt.savefig(out_image)
 
 
 def autolabel(rects, axs):
     """
     Attach a text label above each bar displaying its height (or bar value).
-    :param rects:   Graphical object.
-    :param axs:     Axe number.
-    :return:
     """
     for rect in rects:
         height = rect.get_height()
@@ -150,56 +192,19 @@ def autolabel(rects, axs):
                      '%.3f' % float(height), ha='center', va='bottom')
 
 
-def extracting_common_cnx(dictionary, ind):
-    """
-    Function to create a binary mask representing common connections across the population
-    containing non-zero values.
-    :param dictionary:       Dictionary with metrics as keys containing list of matrices sorted by ids.
-    :param ind:              Indice of the key to use to generate the binary mask from the dictionary.
-    :return:                 Binary mask.
-    """
-    keys = list(dictionary.keys())
-    met = np.copy(dictionary[keys[ind]])
-
-    # Replacing all non-zero values by 1.
-    for i in range(0, len(met)):
-        met[i][met[i] != 0] = 1
-
-    # Adding all matrices together.
-    orig = np.copy(met[0])
-    mask = [orig]
-    for i in range(1, len(met)):
-        orig = np.add(orig, met[i])
-        mask.append(orig)
-
-    # Converting resulting values to 0 and 1.
-    mask_f = mask[(len(met) - 1)]
-    mask_f[mask_f != len(met)] = 0
-    mask_f[mask_f == len(met)] = 1
-
-    return mask_f
-
-
-def apply_binary_mask(dictionary, mask):
-    """
-    Function to apply a binary mask to all matrices contained in a dictionary.
-    :param dictionary:       Dictionary with metrics as keys containing list of matrices sorted by ids.
-    :param mask:             Binary mask.
-    :return:                 Dictionary with the same shape as input.
-    """
-    for a in dictionary.keys():
-        for i in range(0, len(dictionary[a])):
-            dictionary[a][i] = np.multiply(dictionary[a][i], mask)
-
-    return dictionary
-
-
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
     logging.getLogger().setLevel(logging.getLevelName(args.verbose))
 
-    assert_output_dirs_exist_and_empty(parser, args, args.out_folder, create_dir=True)
+    assert_inputs_dirs_exist(parser, args.in_folder)
+    assert_inputs_exist(parser, args.list_ids)
+    assert_output_dirs_exist_and_empty(parser, args, args.out_folder,
+                                       create_dir=True)
+    out_eigenvalues = os.path.join(args.out_folder, 'eigenvalues.pdf')
+    out_variance = os.path.join(args.out_folder, 'explained_variance.pdf')
+    out_excel = os.path.join(args.out_folder, 'loadings.xlsx')
+    out_contributions = os.path.join(args.out_folder, 'contribution.pdf')
 
     with open(args.list_ids) as f:
         subjects = f.read().split()
@@ -207,122 +212,97 @@ def main():
     if args.input_connectoflow:
         # Loading all matrix.
         logging.info('Loading all matrices from a Connectoflow output...')
-        dictionary = {m: [load_matrix_in_any_format(f'{args.in_folder}/{a}/Compute_Connectivity/{m}.npy')
-                          for a in subjects]
-                      for m in args.metrics}
+        files_per_metric = [[os.path.join(
+            args.in_folder, a, 'Compute_Connectivity', '{}.npy'.format(m))
+            for a in subjects]
+            for m in args.metrics]
     else:
         logging.info('Loading all matrices...')
-        dictionary = {m: [load_matrix_in_any_format(f'{args.in_folder}/{a}_{m}.npy') for a in subjects]
-                      for m in args.metrics}
-        # Assert that all metrics have the same number of subjects.
-        nb_sub = [len(dictionary[m]) for m in args.metrics]
-        assert all(x == len(subjects) for x in nb_sub), "Error, the number of matrices for each metric doesn't match" \
-                                                        "the number of subject in the id list." \
-                                                        "Please validate input folder."
+        files_per_metric = [[os.path.join(
+            args.in_folder, '{}_{}.npy'.format(a, m))
+            for a in subjects]
+            for m in args.metrics]
+
+    assert_inputs_exist(parser, sum(files_per_metric, []))
+    matrices_per_metric = [[load_matrix_in_any_format(f) for f in files]
+                           for files in files_per_metric]
 
     # Setting individual matrix shape.
-    mat_shape = dictionary[args.metrics[0]][0].shape
+    mat_shape = matrices_per_metric[0][0].shape
 
-    if args.not_only_common:
+    if args.all_edges:
         # Creating input structure using all edges from all subjects.
         logging.info('Creating PCA input structure with all edges...')
-        df = generate_pca_input(dictionary)
     else:
-        m1 = extracting_common_cnx(dictionary, 0)
-        m2 = extracting_common_cnx(dictionary, 1)
-
-        if m1.shape != mat_shape:
-            parser.error("Extracted binary mask doesn't match the shape of individual input matrix.")
-
-        if np.sum(m1) != np.sum(m2):
-            parser.error("Different binary mask have been generated from 2 different metrics, \n "
-                         "please validate input data.")
-        else:
-            logging.info('Data shows {} common connections across the population.'.format(np.sum(m1)))
-
-        dictionary = apply_binary_mask(dictionary, m1)
-
-        # Creating input structure.
         logging.info('Creating PCA input structure with common edges...')
-        df = generate_pca_input(dictionary)
 
-    # Setting 0 values to nan.
-    df[df == 0] = 'nan'
+        common_edges_mask = np.sum(matrices_per_metric[0]) != 0
 
-    # Standardized the data.
+        # Verifying that other metrics have the same common edges
+        for i, matrices in enumerate(matrices_per_metric[1:]):
+            tmp_mask = np.sum(matrices) != 0
+            if not np.array_equal(common_edges_mask, tmp_mask):
+                parser.error("Different binary masks (common edge) have been "
+                             "generated from metric {} than other metrics. "
+                             "Please validate your input data."
+                             .format(args.metrics[i]))
+
+        logging.info('Data shows {} common connections across the population.'
+                     .format(np.sum(common_edges_mask)))
+
+        # Apply mask
+        matrices_per_metric = [[m * common_edges_mask
+                               for m in metric_matrices]
+                               for metric_matrices in matrices_per_metric]
+
+    # Creating input structure.
+    # For each metric, combining all subjects together, flattening all
+    # connectivity matrices.
+    flat_mats = []
+    for metric_matrices in matrices_per_metric:
+        mat = np.rollaxis(np.array(metric_matrices), axis=1, start=3)
+        mat = mat.reshape((np.prod(mat.shape), 1))
+        flat_mats.append(mat)
+    df = np.hstack(flat_mats)   # Shape: nb_metrics x (flattened data)
+    df[df == 0] = np.nan  # Setting 0 values to nan.
+
+    # Standardizing the data.
     logging.info('Standardizing data...')
     x = StandardScaler().fit_transform(df)
     df_pca = x[~np.isnan(x).any(axis=1)]
     x = np.nan_to_num(x, nan=0.)
 
-    # Perform the PCA.
+    # Performing the PCA.
     logging.info('Performing PCA...')
     pca = PCA(n_components=len(args.metrics))
     principalcomponents = pca.fit_transform(df_pca)
-    principaldf = pd.DataFrame(data=principalcomponents, columns=[f'PC{i}' for i in range(1, len(args.metrics) + 1)])
+    principaldf = pd.DataFrame(
+        data=principalcomponents,
+        columns=[f'PC{i}' for i in range(1, len(args.metrics) + 1)])
 
-    # Plot the eigenvalues.
-    logging.info('Plotting results...')
-    eigenvalues = pca.explained_variance_
-    pos = list(range(1, len(args.metrics)+1))
-    plt.clf()
-    plt.cla()
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    bar_eig = ax.bar(pos, eigenvalues, align='center', tick_label=principaldf.columns)
-    ax.set_xlabel('Principal Components', fontsize=10)
-    ax.set_ylabel('Eigenvalues', fontsize=10)
-    ax.set_title('Eigenvalues for each principal components.', fontsize=10)
-    ax.margins(0.1)
-    autolabel(bar_eig, ax)
-    plt.savefig(f'{args.out_folder}/eigenvalues.pdf')
-
-    # Plot the explained variance.
-    explained_var = pca.explained_variance_ratio_
-    plt.clf()
-    plt.cla()
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    bar_var = ax.bar(pos, explained_var, align='center', tick_label=principaldf.columns)
-    ax.set_xlabel('Principal Components', fontsize=10)
-    ax.set_ylabel('Explained variance', fontsize=10)
-    ax.set_title('Amount of explained variance for all principal components.', fontsize=10)
-    ax.margins(0.1)
-    autolabel(bar_var, ax)
-    plt.savefig(f'{args.out_folder}/explained_variance.pdf')
-
-    # Plot the contribution of each measures to principal component.
-    component = pca.components_
-    output_component = pd.DataFrame(component, index=principaldf.columns, columns=args.metrics)
-    output_component.to_excel(f'{args.out_folder}/loadings.xlsx', index=True, header=True)
-    plt.clf()
-    plt.cla()
-    fig, axs = plt.subplots(2)
-    fig.suptitle('Graph of the contribution of each measures to the first and second principal component.', fontsize=10)
-    bar_pc1 = axs[0].bar(pos, component[0], align='center', tick_label=args.metrics)
-    bar_pc2 = axs[1].bar(pos, component[1], align='center', tick_label=args.metrics)
-    axs[0].margins(0.2)
-    axs[1].margins(0.2)
-    autolabel(bar_pc1, axs[0])
-    autolabel(bar_pc2, axs[1])
-    for ax in axs.flat:
-        ax.set(xlabel='Diffusion measures', ylabel='Loadings')
-    for ax in axs.flat:
-        ax.label_outer()
-    plt.savefig(f'{args.out_folder}/contribution.pdf')
+    # Plotting
+    eigenvalues = plot_eigenvalues(pca, principaldf,
+                                   nb_metrics=len(args.metrics),
+                                   out_file=out_eigenvalues)
+    plot_explained_variance(pca, principaldf, nb_metrics=len(args.metrics),
+                            out_file=out_variance)
+    plot_contribution(pca, principaldf, args.metrics, out_excel,
+                      out_contributions)
 
     # Extract the derived newly computed measures from the PCA analysis.
     logging.info('Saving matrices for PC with eigenvalues > 1...')
     out = pca.transform(x)
-    out = restore_zero_values(x, out)
-    out = out.swapaxes(0, 1).reshape(len(args.metrics), len(subjects), mat_shape[0], mat_shape[1])
+    out = out * (x != 0)
+    out = out.swapaxes(0, 1).reshape(len(args.metrics), len(subjects),
+                                     mat_shape[0], mat_shape[1])
 
     # Save matrix for significant components
     nb_pc = eigenvalues[eigenvalues >= 1]
     for i in range(0, len(nb_pc)):
         for s in range(0, len(subjects)):
-            save_matrix_in_any_format(f'{args.out_folder}/{subjects[s]}_PC{i+1}.npy',
-                                      out[i, s, :, :])
+            filename = os.path.join(args.out_folder,
+                                    f'{subjects[s]}_PC{i+1}.npy')
+            save_matrix_in_any_format(filename, out[i, s, :, :])
 
 
 if __name__ == "__main__":
