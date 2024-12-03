d1c9a7ce14b432933e652ddfb369bd092619bd01
Gabriel Girard <girard.gabriel@gmail.com>
2024-12-03 14:51:45 -0500

    NF - added input options for multiple ROI masks

--- scripts/scil_volume_stats_in_ROI.py
+++ scripts/scil_volume_stats_in_ROI.py
@@ -33,9 +33,9 @@ def _build_arg_parser():
     p = argparse.ArgumentParser(description=__doc__,
                                 formatter_class=argparse.RawTextHelpFormatter)
 
-    p.add_argument('in_mask',
-                   help='Mask volume filename.\nCan be a binary mask or a '
-                        'weighted mask.')
+    p.add_argument('in_masks', nargs='+',
+                   help='Mask volume filenames.\nCan be binary masks or '
+                        'weighted masks.')
 
     g = p.add_argument_group('Metrics input options')
     gg = g.add_mutually_exclusive_group(required=True)
@@ -74,59 +74,60 @@ def main():
         if not os.path.exists(args.metrics_dir):
             parser.error("Metrics directory does not exist: {}"
                          .format(args.metrics_dir))
-        assert_inputs_exist(parser, args.in_mask)
+        assert_inputs_exist(parser, args.in_masks)
 
         tmp_file_list = glob.glob(os.path.join(args.metrics_dir, '*nii.gz'))
         args.metrics_file_list = [os.path.join(args.metrics_dir, f)
                                   for f in tmp_file_list]
     else:
-        assert_inputs_exist(parser, [args.in_mask] + args.metrics_file_list)
-    assert_headers_compatible(parser,
-                              [args.in_mask] + args.metrics_file_list)
+        assert_inputs_exist(parser, args.in_masks + args.metrics_file_list)
+    assert_headers_compatible(parser, args.in_masks + args.metrics_file_list)
 
-    # Loading
-    mask_data = nib.load(args.in_mask).get_fdata(dtype=np.float32)
-    if len(mask_data.shape) > 3:
-        parser.error('Mask should be a 3D image.')
-    if np.min(mask_data) < 0:
-        parser.error('Mask should not contain negative values.')
-    roi_name = split_name_with_nii(os.path.basename(args.in_mask))[0]
+    # Computing stats for all masks and metrics files
+    json_stats = {}
+    for mask_filename in args.in_masks:
+        mask_data = nib.load(mask_filename).get_fdata(dtype=np.float32)
+        if len(mask_data.shape) > 3:
+            parser.error('Mask should be a 3D image.')
+        if np.min(mask_data) < 0:
+            parser.error('Mask should not contain negative values.')
+        roi_name = split_name_with_nii(os.path.basename(mask_filename))[0]
 
-    # Discussion about the way the normalization is done.
-    # https://github.com/scilus/scilpy/pull/202#discussion_r411355609
-    # Summary:
-    # 1) We don't want to normalize with data = (data-min) / (max-min) because
-    # it zeroes out the minimal values of the array. This is not a large error
-    # source, but not preferable.
-    # 2) data = data / max(data) or data = data / sum(data): in practice, when
-    # we use them in numpy using their weights argument, leads to the same
-    # result.
-    if args.normalize_weights:
-        mask_data /= np.max(mask_data)
-    elif args.bin:
-        mask_data[np.where(mask_data > 0.0)] = 1.0
-    elif np.min(mask_data) < 0.0 or np.max(mask_data) > 1.0:
-        parser.error('Mask data should only contain values between 0 and 1. '
-                     'Try --normalize_weights.')
+        # Discussion about the way the normalization is done.
+        # https://github.com/scilus/scilpy/pull/202#discussion_r411355609
+        # Summary:
+        # 1) We don't want to normalize with data = (data-min) / (max-min) because
+        # it zeroes out the minimal values of the array. This is not a large error
+        # source, but not preferable.
+        # 2) data = data / max(data) or data = data / sum(data): in practice, when
+        # we use them in numpy using their weights argument, leads to the same
+        # result.
+        if args.normalize_weights:
+            mask_data /= np.max(mask_data)
+        elif args.bin:
+            mask_data[np.where(mask_data > 0.0)] = 1.0
+        elif np.min(mask_data) < 0.0 or np.max(mask_data) > 1.0:
+            parser.error('Mask data should only contain values between 0 and 1. '
+                        'Try --normalize_weights.')
 
-    # Load and process all metrics files.
-    json_stats = {roi_name: {}}
-    for f in args.metrics_file_list:
-        metric_img = nib.load(f)
-        metric_name = split_name_with_nii(os.path.basename(f))[0]
-        if len(metric_img.shape) == 3:
-            data = metric_img.get_fdata(dtype=np.float64)
-            if np.any(np.isnan(data)):
-                logging.warning("Metric '{}' contains some NaN. Ignoring "
-                                "voxels with NaN."
-                                .format(os.path.basename(f)))
-            mean, std = weighted_mean_std(mask_data, data)
-            json_stats[roi_name][metric_name] = {'mean': mean,
-                                                 'std': std}
-        else:
-            parser.error(
-                'Metric {} is not a 3D image ({}D shape).'
-                .format(f, len(metric_img.shape)))
+        # Load and process all metrics files.
+        json_stats[roi_name] = {}
+        for f in args.metrics_file_list:
+            metric_img = nib.load(f)
+            metric_name = split_name_with_nii(os.path.basename(f))[0]
+            if len(metric_img.shape) == 3:
+                data = metric_img.get_fdata(dtype=np.float64)
+                if np.any(np.isnan(data)):
+                    logging.warning("Metric '{}' contains some NaN. Ignoring "
+                                    "voxels with NaN."
+                                    .format(os.path.basename(f)))
+                mean, std = weighted_mean_std(mask_data, data)
+                json_stats[roi_name][metric_name] = {'mean': mean,
+                                                    'std': std}
+            else:
+                parser.error(
+                    'Metric {} is not a 3D image ({}D shape).'
+                    .format(f, len(metric_img.shape)))
 
     # Print results
     print(json.dumps(json_stats, indent=args.indent, sort_keys=args.sort_keys))
