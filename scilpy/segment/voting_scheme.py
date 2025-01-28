8877c1299fea1ecff6d75d2ff0e17ecbcd24607e
Francois Rheault <francois.m.rheault@usherbrooke.ca>
2025-01-28 15:14:56 -0500

    pep8 and merge

--- scilpy/segment/voting_scheme.py
+++ scilpy/segment/voting_scheme.py
@@ -173,7 +173,7 @@ class VotingScheme(object):
                 raise ValueError('Number of streamlines is not equal to the '
                                  'number of streamlines indices')
             new_sft = StatefulTractogram(streamlines, reference, Space.RASMM)
-            
+
             if len(new_sft):
                 _, indices_to_keep = new_sft.remove_invalid_streamlines()
                 new_sft = new_sft[indices_to_keep]
