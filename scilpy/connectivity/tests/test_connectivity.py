9cb23e8fed68ca307a41f43ae89b8f1cf94e1145
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2025-02-03 11:13:14 -0500

    Fix missing casting from list to ArraySequence
    + Fix mode (how to pad out of range) of map_coordinate

--- scilpy/connectivity/tests/test_connectivity.py
+++ scilpy/connectivity/tests/test_connectivity.py
@@ -7,29 +7,37 @@ from scilpy.connectivity.connectivity import \
 
 def test_compute_triu_connectivity_from_labels():
     labels = np.asarray([[3, 4, 5, 6],
-                         [7, 8, 9, 10]])
+                         [7, 8, 9, 10],
+                         [11, 12, 13, 14]])
+    labels = labels[:, :, None]
+    labels = np.concatenate([labels, labels, labels], axis=-1)
+    # Data shape: (3, 4, 3)
 
-    # streamline 1 starts at label 4, ends at label 6
-    # streamline 2 starts at label 9, ends at label 7
-    # streamline 3 too, but not in the center (vox, corner)
-    tractogram = [np.asarray([[0, 1],
-                              [5, 6],
-                              [0, 3]]),
-                  np.asarray([[1, 2],
-                              [1, 0]]),
-                  np.asarray([[1.1, 2.2],
-                              [1.9, 0.5]])]
+    # vox space, center origin
+    # streamline 1 : Easy (middle voxel).
+    #              starts at voxel (1, 1, 1) = label 8,
+    #              ends at voxel (1.1, 0.8, 1.1) = label 8.
+    # streamline 2: Border management, center of the voxel
+    #              starts at voxel (0, 1, 0) = label 4
+    #              ends at voxel (2, 0, 0) = label 11
+    # streamline 3: Idem, but not in the center  (vox, center)
+    tractogram = [np.asarray([[1.0, 1.0, 1.0],
+                              [5.0, 6.1, 10.1],
+                              [1.1, 0.8, 1.1]]),
+                  np.asarray([[0.0, 1.0, 0.0],
+                              [2.0, 0.0, 0.0]]),
+                  np.asarray([[-0.3, 0.8, -0.2],
+                              [2.1, 0.3, -0.2]])]
     output, _, _, _ = compute_triu_connectivity_from_labels(
         tractogram, labels)
-    assert np.array_equal(output.shape, [8, 8])
-    expected_out = np.zeros((8, 8))
-    expected_out[1, 3] = 1  # This is labels (4, 6)
-    expected_out[4, 6] = 2  # This is labels (7, 9)
+
+    assert np.array_equal(output.shape, [12, 12])  # 12 labels
+    expected_out = np.zeros((12, 12))
+    expected_out[5, 5] = 1  # One streamline with label (8, 8) => [5, 5]
+    expected_out[1, 8] = 2  # Two streamlines with label (4, 11) = [1, 8]
     assert np.array_equal(output, expected_out)
 
 
 def test_compute_connectivity_matrices_from_hdf5():
     # ToDo. We will have to create a test hdf5.
     pass
-
-
--- scilpy/connectivity/tests/test_matrix_tools.py
+++ scilpy/connectivity/tests/test_matrix_tools.py
@@ -16,18 +16,14 @@ def test_apply_reordering():
                               [13, 14, 15, 16]])
     output = apply_reordering(conn_matrix, [[0, 1, 3, 2],
                                             [1, 2, 3, 0]])
-    # First changing rows 2 and 3
-    expected_out = np.asarray([[1, 2, 3, 4],
-                              [5, 6, 7, 8],
-                              [13, 14, 15, 16],
-                              [9, 10, 11, 12]])
-    # Permuting columns
+
+    # Changing rows 2 and 3 + permuting columns
     expected_out = np.asarray([[2, 3, 4, 1],
                               [6, 7, 8, 5],
                               [14, 15, 16, 13],
                               [10, 11, 12, 9]])
     assert np.array_equal(output, expected_out)
-    
+
 
 def test_evaluate_graph_measures():
     pass
