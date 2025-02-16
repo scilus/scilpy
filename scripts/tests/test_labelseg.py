2de5f893c029247fe7da5f33299e09ac169091df
AntoineTheb <antoine.theberge@usherbrooke.ca>
2025-02-16 18:33:26 +0900

    FIX: exclude help test

--- scripts/tests/test_labelseg.py
+++ scripts/tests/test_labelseg.py
@@ -10,10 +10,11 @@ fetch_data(get_testing_files_dict(), keys=['tracking.zip'])
 tmp_dir = tempfile.TemporaryDirectory()
 
 
+@pytest.mark.ml
 def test_help_option(script_runner, monkeypatch):
     ret = script_runner.run('scil_labelseg.py', '--help')
 
-    assert not ret.success
+    assert ret.success
 
 
 @pytest.mark.ml
@@ -21,7 +22,7 @@ def test_execution(script_runner, monkeypatch):
     in_fodf = os.path.join(SCILPY_HOME, 'tracking', 'fodf.nii.gz')
     in_mask = os.path.join(SCILPY_HOME, 'tracking', 'seeding_mask.nii.gz')
 
-    ret = script_runner.run('scil_labelseg.py', in_fodf, in_mask)
+    ret = script_runner.run('scil_labelseg.py', in_fodf, in_mask, '--half')
     assert ret.success
 
 
@@ -31,5 +32,5 @@ def test_execution_100_labels(script_runner, monkeypatch):
     in_mask = os.path.join(SCILPY_HOME, 'tracking', 'seeding_mask.nii.gz')
 
     ret = script_runner.run('scil_labelseg.py', in_fodf, in_mask,
-                            '--nb_pts', 100)
+                            '--nb_pts', '100', '--half')
     assert ret.success
