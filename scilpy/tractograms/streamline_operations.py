2f2edbaea74b526ccd3013fa4775f892a5c23e4d
AntoineTheb <antoine.theberge@usherbrooke.ca>
2025-04-22 16:04:21 -0400

    FIX: proper function pick

--- scilpy/tractograms/streamline_operations.py
+++ scilpy/tractograms/streamline_operations.py
@@ -310,21 +310,27 @@ def cut_invalid_streamlines(sft, epsilon=0.001):
     return new_sft, cutting_counter
 
 
-def remove_single_point_streamlines(sft):
+def filter_streamlines_by_nb_points(sft, min_nb_points=2):
     """
-    Remove single point streamlines from a StatefulTractogram.
+    Remove streamlines from a StatefulTractogram with fewer nb_points.
 
     Parameters
     ----------
     sft: StatefulTractogram
         The sft to remove single point streamlines from.
+    min_nb_points: int
+        Minimum number of point a streamline needs to have to kept.
 
     Returns
     -------
     new_sft : StatefulTractogram
         New object with the single point streamlines removed.
     """
-    indices = [i for i in range(len(sft)) if len(sft.streamlines[i]) > 1]
+    if min_nb_points <= 1:
+        raise ValueError("The value of min_nb_points "
+                         "should be greater than 1!")
+
+    indices = [i for i in range(len(sft)) if len(sft.streamlines[i]) > min_nb_points - 1]
     if len(indices):
         new_sft = sft[indices]
     else:
@@ -980,6 +986,7 @@ def get_streamlines_as_fixed_array(streamlines):
     Parameters
     ----------
     streamlines: list
+        The list of streamlines to convert into a fixed length array
 
     Return
     ------
@@ -989,9 +996,45 @@ def get_streamlines_as_fixed_array(streamlines):
         Single dimensional array of all the streamline lengths.
     """
     lengths = [len(streamline) for streamline in streamlines]
-    streamlines_fixed = np.ndarray((len(streamlines), max(lengths), 3))
+    streamlines_fixed = np.zeros((len(streamlines), max(lengths), 3))
     for i, f in enumerate(streamlines_fixed):
         for j, c in enumerate(streamlines[i]):
             f[j] = c
 
     return streamlines_fixed, np.array(lengths)
+
+
+def find_seed_indexes_on_streamlines(seeds, streamlines, atol=1.e-8):
+    """
+    Given a list of seeds and a corresponding list of streamlines, finds
+    the index of each seed on its respective streamline.
+
+    Parameters
+    ----------
+    seeds: list
+        List of seeds to locate on streamlines
+    streamlines: list
+        List of streamlines produced from seeds
+    atol: float
+        Absolute tolerance of the comparison between a seed and each of the
+        streamline coordinates.
+
+    Return
+    ------
+    seed_indexes: list
+        A list containing the index of each seed on its streamline.
+    """
+    seed_indexes = []
+    for seed, streamline in zip(seeds, streamlines):
+        seed_index = -1
+
+        for i, point in enumerate(streamline):
+            if np.allclose(point, seed, rtol=0, atol=atol):
+                seed_index = i
+                break
+
+        if seed_index == -1:
+            raise ValueError('A seed coordinate was not found on streamline.')
+
+        seed_indexes.append(seed_index)
+    return seed_indexes
