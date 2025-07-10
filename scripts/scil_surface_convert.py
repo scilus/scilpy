9d3246f11d12d6b816e2f06f085887c82dd1cb18
frheault <francois.m.rheault@usherbrooke.ca>
2025-07-10 09:53:35 -0400

    revert convert

--- scripts/scil_surface_convert.py
+++ scripts/scil_surface_convert.py
@@ -11,51 +11,46 @@ Supported formats:
 > scil_surface_convert.py surf.vtk converted_surf.ply
 
 Formerly: scil_convert_surface.py
+-----------------------------------------------------------------
+Reference:
+[1] St-Onge, E., Daducci, A., Girard, G. and Descoteaux, M. 2018.
+    Surface-enhanced tractography (SET). NeuroImage.
+-----------------------------------------------------------------
 """
 import argparse
 import logging
 import os
 
-from dipy.io.surface import load_surface, save_surface
+from trimeshpy.vtk_util import (load_polydata,
+                                save_polydata)
 
-from scilpy.io.utils import (add_vtk_legacy_arg,
-                             add_overwrite_arg,
-                             add_surface_spatial_arg,
+from scilpy.surfaces.utils import (convert_freesurfer_into_polydata,
+                                   flip_surfaces_axes)
+
+from scilpy.io.utils import (add_overwrite_arg,
                              add_verbose_arg,
                              assert_inputs_exist,
-                             assert_outputs_exist,
-                             convert_stateful_str_to_enum)
-
-EPILOG = """
-References:
-[1] St-Onge, E., Daducci, A., Girard, G. and Descoteaux, M. 2018.
-    Surface-enhanced tractography (SET). NeuroImage.
-"""
+                             assert_outputs_exist)
+from scilpy.version import version_string
 
 
 def _build_arg_parser():
-    p = argparse.ArgumentParser(description=__doc__, epilog=EPILOG,
-                                formatter_class=argparse.RawTextHelpFormatter)
+    p = argparse.ArgumentParser(description=__doc__,
+                                formatter_class=argparse.RawTextHelpFormatter,
+                                epilog=version_string)
 
     p.add_argument('in_surface',
                    help='Input a surface (FreeSurfer or supported by VTK).')
     p.add_argument('out_surface',
                    help='Output surface (formats supported by VTK).\n'
                         'Recommended extension: .vtk or .ply')
+
+    p.add_argument('--flip_axes', default=[-1, -1, 1], type=int, nargs=3,
+                   help='Flip axes for RAS or LPS convention. '
+                        'Default is LPS convention (MI-Brain) %(default)s.')
     p.add_argument('--reference',
-                   help='Reference image to extract the transformation matrix\n'
+                   help='Reference image to extract the transformation matrix '
                         'to align the freesurfer surface with the T1.')
-
-    r = p.add_mutually_exclusive_group()
-    r.add_argument('--ref_pial',
-                   help='Reference pial surface to extract the transformation\n'
-                   'matrix to align the freesurfer surface with the T1.')
-    r.add_argument('--ref_gii',
-                   help='Reference gii surface to extract the header '
-                   'information to be valid')
-
-    add_vtk_legacy_arg(p)
-    add_surface_spatial_arg(p)
     add_verbose_arg(p)
     add_overwrite_arg(p)
 
@@ -67,10 +62,8 @@ def main():
     args = parser.parse_args()
     logging.getLogger().setLevel(logging.getLevelName(args.verbose))
 
-    assert_inputs_exist(parser, args.in_surface,
-                        optional=[args.reference, args.ref_pial, args.ref_gii])
+    assert_inputs_exist(parser, args.in_surface, optional=args.reference)
     assert_outputs_exist(parser, args, args.out_surface)
-    convert_stateful_str_to_enum(args)
 
     _, ext = os.path.splitext(args.in_surface)
     # FreeSurfer surfaces have no extension, verify if the input has one of the
@@ -80,26 +73,15 @@ def main():
             parser.error('The reference image is required for FreeSurfer '
                          'surfaces.')
 
-        if args.source_space or args.source_origin:
-            print('The source space and source origin can not be changed for '
-                  'FreeSurfer surfaces. Will be ignored.')
+        polydata = convert_freesurfer_into_polydata(args.in_surface,
+                                                    args.reference)
+    else:
+        polydata = load_polydata(args.in_surface)
 
-    _, ext = os.path.splitext(args.out_surface)
-    if ext not in ['.vtk', '.vtp', '.fib', '.ply', '.stl', '.xml', '.obj']:
-        if args.destination_space or args.destination_origin:
-            parser.error('The destination space and destination origin can '
-                         'not be changed for FreeSurfer surfaces')
+    if args.flip_axes:
+        polydata = flip_surfaces_axes(polydata, args.flip_axes)
 
-    # Dipy takes care of Freesurfer surfaces (ignore space and origin if any)
-    sfs = load_surface(args.in_surface, args.reference,
-                       from_space=args.source_space,
-                       from_origin=args.source_origin)
-
-    # The ref will be either None or a valid path or both None
-    save_surface(sfs, args.out_surface, to_space=args.destination_space,
-                 to_origin=args.destination_origin,
-                 legacy_vtk_format=args.legacy_vtk_format,
-                 ref_pial=args.ref_pial, ref_gii=args.ref_gii)
+    save_polydata(polydata, args.out_surface, legacy_vtk_format=True)
 
 
 if __name__ == "__main__":
