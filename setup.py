4c512f0fdf19541f8871d4c9d239c82c034be56f
arnaudbore <arnaud.bore@gmail.com>
2025-07-23 10:31:35 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-import os
-
-from setuptools import setup, find_packages, Extension
-from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
-from numpy import get_include
+import numpy
+from setuptools import setup, Extension
 
 
 define_macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
@@ -17,16 +14,16 @@ extensions = [
               define_macros=define_macros),
     Extension('scilpy.tractanalysis.streamlines_metrics',
               ['src/scilpy/tractanalysis/streamlines_metrics.pyx'],
-              define_macros=define_macros)
-]
+              define_macros=define_macros)]
 
 # This is the function that is executed
 setup(
-    name='scilpy',  # Required
+    name='scilpy',
 
     # A list of compiler Directives is available at
     # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#compiler-directives
 
     # external to be compiled
-    ext_modules = cythonize(extensions, compiler_directives={"language_level": 3, "profile": False})
+    ext_modules=cythonize(extensions),
+    include_dirs=[numpy.get_include()]
 )
