af49082d56b522fcd20a873887356dd1729070e9
Francois Rheault <francois.m.rheault@usherbrooke.ca>
2025-08-01 09:58:31 -0400

    Update scripts/tests/test_lesions_harmonize_labels.py
    Co-authored-by: Arnaud Bore <arnaud.bore@gmail.com>

--- scripts/tests/test_lesions_harmonize_labels.py
+++ scripts/tests/test_lesions_harmonize_labels.py
@@ -18,7 +18,7 @@ def test_help_option(script_runner):
 
 def test_harmonize_label(script_runner, monkeypatch):
     monkeypatch.chdir(os.path.expanduser(tmp_dir.name))
-    original_data = os.path.join(SCILPY_HOME, 'lesions',
+    t1 = os.path.join(SCILPY_HOME, 'lesions', 'S001_T1_lesions_labels.nii.gz')
                                  '*_lesions_labels.nii.gz')
     ret = script_runner.run(['scil_lesions_harmonize_labels.py',
                              original_data, 'test', '--max_adjacency',
