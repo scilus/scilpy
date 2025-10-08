148c9f84da35a1a1011332505d59ad23f1accc87
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2025-10-08 16:16:05 -0400

    Add tractogram_math and streamlines_math + tck/trk explanation

--- docs/source/_static/bash/tractogram_manipulation/streamlines_math.sh
+++ docs/source/_static/bash/tractogram_manipulation/streamlines_math.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail  # Will fail on error
 
-echo "Not ready!"
+# --------------------
+# Resampling / compressing
+# --------------------
+
+# Choosing the number of points
+scil_tractogram_resample_nb_points $tractogram1 20 resampled_20pts.trk
+
+# Compressing
+scil_tractogram_compress
+
+
+# --------------------
+# Cutting streamlines
+# --------------------
+
+# Outside a mask
+scil_tractogram_cut_streamlines $tractogram1 cut_streamlines.trk \
+    --mask $mask --min_length 20
+
+# Outside labels
+ROI1=3
+ROI2=6
+scil_tractogram_cut_streamlines $tractogram1 cut_streamlines.trk \
+    --labels $my_parcellation --label_ids $ROI1 $ROI2
--- docs/source/_static/bash/tractogram_manipulation/tractogram_math.sh
+++ docs/source/_static/bash/tractogram_manipulation/tractogram_math.sh
@@ -1,4 +1,51 @@
 #!/usr/bin/env bash
 set -euo pipefail  # Will fail on error
 
-echo "Not ready!"
+# --------------------
+# Logical operation
+# --------------------
+
+# Merging two tractograms (using all streamlines from both)
+# If a streamline is exactly the same in both tractograms, it will be kept once.
+scil_tractogram_count_streamlines $tractogram1
+scil_tractogram_count_streamlines $tractogram2
+scil_tractogram_math union $tractogram1 $tractogram2 union.trk
+scil_tractogram_count_streamlines union.trk
+
+# Finding streamlines belonging in two tractograms (intersection)
+scil_tractogram_math intersection $tractogram1 $tractogram2 intersection.trk
+
+# Finding streamlines that are in $tractogram1 but not in $tractogram2
+scil_tractogram_math difference $tractogram1 $tractogram2 difference1-2.trk
+
+
+
+# --------------------
+# Resampling
+# --------------------
+
+# Downsampling: keeping only 20 streamlines
+scil_tractogram_resample $tractogram1 20 tractogram_downsampled.trk
+
+# Downsampling per cluster
+scil_tractogram_resample $tractogram1 20 tractogram_downsampled.trk --downsample_per_cluster
+
+# Upsampling: uses randomly picked streamlines to create new similar ones
+# by moving a little bit each point.
+# We will also give a --tube_radius option to make sure final streamlines
+# do not deviate too much from original trajectory.
+scil_tractogram_resample $tractogram1 1000 --point_wise_std
+
+# Splitting into 3 chunks
+scil_tractogram_split --nb_chunks 3 $tractogram1 out_split_3_part
+
+# Splitting into chunks of 100 streamlines
+scil_tractogram_split --chunk_size 100 $tractogram1 out_split_100_part
+
+# Splitting per cluster
+scil_tractogram_split --nb_chunk 2 --split_per_cluster $tractogram1 out_split_2QB_part
+
+# --------------------
+# Flipping
+# --------------------
+scil_tractogram_flip $tractogram1 flipped_x.trk x
