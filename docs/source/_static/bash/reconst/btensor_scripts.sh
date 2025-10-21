268fb8a8eb5cfbaa1b52e7d1bb97f7002f0126e4
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2025-10-21 13:07:42 -0400

    Btensor works locally

--- docs/source/_static/bash/reconst/btensor_scripts.sh
+++ docs/source/_static/bash/reconst/btensor_scripts.sh
@@ -11,13 +11,12 @@ set -euo pipefail  # Will fail on error
 in_dir=$1
 out_dir=$2
 
+in_dir=$in_dir/btensor
 
-# For now, let's use data in .scilpy
+# For now, the tutorial data only contains the masks.
+# Other necessary data can be obtained with:
 scil_data_download
-cp -r $HOME/.scilpy/btensor_testdata $in_dir/
-in_dir=$in_dir/btensor_testdata/
-
-#       -----------------> I DON'T HAVE MASKS. FAILS.
+cp $HOME/.scilpy/btensor_testdata/* $in_dir/
 
 # ==============
 # Now let's run the tutorial
@@ -30,21 +29,24 @@ scil_frf_memsmt wm_frf.txt gm_frf.txt csf_frf.txt \
     --in_dwis $in_dir/dwi_linear.nii.gz $in_dir/dwi_planar.nii.gz $in_dir/dwi_spherical.nii.gz \
     --in_bvals $in_dir/linear.bvals $in_dir/planar.bvals $in_dir/spherical.bvals \
     --in_bvecs $in_dir/linear.bvecs $in_dir/planar.bvecs $in_dir/spherical.bvecs \
-    --in_bdeltas 1 -0.5 0 $new_opts --mask $in_dir/mask.nii.gz \
+    --in_bdeltas 1 -0.5 0 --min_nvox 1  --mask $in_dir/mask.nii.gz \
     --mask_wm $in_dir/wm_mask.nii.gz --mask_gm $in_dir/gm_mask.nii.gz \
-    --mask_csf $in_dir/csf_mask.nii.gz -v
+    --mask_csf $in_dir/csf_mask.nii.gz
 
 echo "Creating the fODF"
 echo "*****************"
-scil_fodf_memsmt wm_fodf.txt gm_fodf.txt csf_fodf.txt \
+scil_fodf_memsmt wm_frf.txt gm_frf.txt csf_frf.txt \
     --in_dwis $in_dir/dwi_linear.nii.gz $in_dir/dwi_planar.nii.gz $in_dir/dwi_spherical.nii.gz \
     --in_bvals $in_dir/linear.bvals $in_dir/planar.bvals $in_dir/spherical.bvals \
     --in_bvecs $in_dir/linear.bvecs $in_dir/planar.bvecs $in_dir/spherical.bvecs \
-    --in_bdeltas 1 -0.5 0  --processes 8 -v --mask $in_dir/mask.nii.gz
+    --in_bdeltas 1 -0.5 0  --processes 8 --mask $in_dir/mask.nii.gz
 
 echo "Compute metrics"
 echo "*****************"
-scil_btensor_metrics --in_dwis $in_dir/dwi_linear.nii.gz $in_dir/dwi_planar.nii.gz $in_dir/dwi_spherical.nii.gz \
-    --in_bvals $in_dir/linear.bvals $in_dir/planar.bval $in_dir/spherical.bvals \
-    --in_bvecs $in_dir/linear.bvecs $in_dir/planar.bvecs $in_dir/dwi_spherical.bvecs \
-    --in_bdeltas 1 -0.5 0 --fa $in_dir/fa.nii.gz --mask $in_dir/mask.nii.gz --processes 8 -v
+scil_btensor_metrics \
+    --in_dwis $in_dir/dwi_linear.nii.gz $in_dir/dwi_planar.nii.gz $in_dir/dwi_spherical.nii.gz \
+    --in_bvals $in_dir/linear.bvals $in_dir/planar.bvals $in_dir/spherical.bvals \
+    --in_bvecs $in_dir/linear.bvecs $in_dir/planar.bvecs $in_dir/spherical.bvecs \
+    --in_bdeltas 1 -0.5 0 --fa $in_dir/fa.nii.gz --processes 8 --mask $in_dir/mask.nii.gz
+echo "Resulting files: "
+ls ./
