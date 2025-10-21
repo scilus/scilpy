6cdf477326154715194b9e69817a18fe176f1656
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2025-10-21 14:30:51 -0400

    MTI script works (but creates warnings)

--- docs/source/_static/bash/reconst/mti_scripts.sh
+++ docs/source/_static/bash/reconst/mti_scripts.sh
@@ -6,7 +6,7 @@ set -euo pipefail  # Will fail on error
 #    1) Load the input data
 #       https://scilpy.readthedocs.io/en/latest/documentation/getting_started.html
 #    2) Call this script with
-#    --->   bash btensor_scripts.sh  path/to/your/data  path/to/save/outputs
+#    --->   bash btensor_scripts.sh  $in_dir/your/data  $in_dir/save/outputs
 # ==============
 in_dir=$1
 out_folder=$2
@@ -14,7 +14,11 @@ out_folder=$2
 
 # For now, let's use data in .scilpy
 scil_data_download
-?
+in_dir=$in_dir/mti
+mkdir $in_dir
+cp $HOME/.scilpy/ihMT/B1* $in_dir/
+cp $HOME/.scilpy/ihMT/echo-1* $in_dir/
+cp $HOME/.scilpy/ihMT/mask_resample.nii.gz $in_dir/mask.nii.gz
 
 # ==============
 # Now let's run the tutorial
@@ -23,21 +27,25 @@ cd $out_folder
 
 
 # 2. Basic usage
-scil_mti_maps_ihMT path/to/output/directory \
-        --in_altnp path/to/*altnp.nii.gz \
-        --in_altpn path/to/*altpn.nii.gz \
-        --in_negative path/to/*neg.nii.gz \
-        --in_positive path/to/echo*pos.nii.gz \
-        --in_mtoff_pd path/to/echo*mtoff.nii.gz \
-        --in_mtoff_t1 path/to/echo*T1w.nii.gz \
-        --mask path/to/mask_bin.nii.gz \
-        --in_jsons path/to/echo*mtoff.json path/to/echo*T1w.json
+echo "Basic usage: ihmt"
+echo "******************"
+scil_mti_maps_ihMT $out_folder \
+        --in_altnp $in_dir/*altnp*.nii.gz \
+        --in_altpn $in_dir/*altpn*.nii.gz \
+        --in_negative $in_dir/*neg*.nii.gz \
+        --in_positive $in_dir/echo*pos*.nii.gz \
+        --in_mtoff_pd $in_dir/echo*mtoff*.nii.gz \
+        --in_mtoff_t1 $in_dir/echo*T1w*.nii.gz \
+        --mask $in_dir/mask.nii.gz \
+        --in_jsons $in_dir/echo*mtoff*.json $in_dir/echo*T1w*.json
 
 # Or:
-scil_mti_maps_MT path/to/output/directory \
-    --in_positive path/to/echo*pos.nii.gz \
-    --in_negative path/to/echo*neg.nii.gz \
-    --in_mtoff_pd path/to/echo*mtoff.nii.gz \
-    --in_mtoff_t1 path/to/echo*T1w.nii.gz \
-    --mask path/to/mask_bin.nii.gz \
-    --in_jsons path/to/echo*mtoff.json path/to/echo*T1w.json
+echo "Or for MT only"
+echo "**************"
+scil_mti_maps_MT $out_folder \
+    --in_positive $in_dir/echo*pos*.nii.gz \
+    --in_negative $in_dir/echo*neg*.nii.gz \
+    --in_mtoff_pd $in_dir/echo*mtoff*.nii.gz \
+    --in_mtoff_t1 $in_dir/echo*T1w*.nii.gz \
+    --mask $in_dir/mask.nii.gz \
+    --in_jsons $in_dir/echo*mtoff*.json $in_dir/echo*T1w*.json
