4a6902d74e02e8944ffbd31b40ad4eb9224015a2
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2025-10-30 16:13:38 -0400

    Streamlines math works locally

--- docs/source/_static/bash/tractogram_manipulation/streamlines_math.sh
+++ docs/source/_static/bash/tractogram_manipulation/streamlines_math.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail  # Will fail on error
 
-<<<<<<< HEAD
-=======
 # ==============
 # How to run this script
 #    1) Load the input data
 #       https://scilpy.readthedocs.io/en/latest/documentation/getting_started.html
 #    2) Call this script with
-#    --->   bash streamlines_math.sh  path/to/your/data
-#    Note that outputs will be saved in the current directory.
+#    --->   bash streamlines_math.sh  path/to/your/data  path/to/save/outputs
 # ==============
-in_folder=$1
+in_dir=$1
+out_dir=$2
 
 # Let's use a tractogram available in the data:
-tractogram1=$in_folder/sub-01/sub-01__cst_L_part1.trk
-mask=$in_folder/sub-01/sub-01__small_mask_wm.nii.gz
-labels=$in_folder/sub-01/sub-01__wmparc.nii.gz
+# tractogram1=$in_dir/sub-01/sub-01__cst_L_part2.trk not compatible...
+tractogram1=$in_dir/sub-01/sub-01__cst_L.trk
+labels=$in_dir/sub-01/sub-01__wmparc.nii.gz
+mask=$in_dir/sub-01/sub-01__small_mask_wm.nii.gz
 
->>>>>>> b3af6517 (Finishing tutorial for tractogram_math)
+# Current wmparc is in float. Should be in int. Let's convert.
+scil_volume_math convert $in_dir/sub-01/sub-01__wmparc.nii.gz \
+    --data_type int16  -f $in_dir/sub-01/sub-01__wmparc.nii.gz
+
 # --------------------
 # Resampling / compressing
 # --------------------
-cd $out_folder
+cd $out_dir
 
 echo "Choosing the number of points"
 scil_tractogram_resample_nb_points $tractogram1 resampled_20pts.trk \
@@ -40,11 +42,6 @@ echo "Cutting streamlines outside a mask"
 scil_tractogram_cut_streamlines $tractogram1 cut_streamlines.trk \
     --mask $mask --min_length 20
 
-    # ---------------> FAILS WITH HEADERS NOT COMPATIBLE?? TODO
-
-echo "Cutting streamlines Outside labels"
-scil_labels_split_volume_by_ids $labels --out_dir labels/
-ROI1=labels/2024.nii.gz
-ROI2=labels/16.nii.gz
-scil_tractogram_cut_streamlines $tractogram1 cut_streamlines.trk \
-    --labels $labels --label_ids $ROI1 $ROI2
+echo "Cutting streamlines outside labels"
+scil_tractogram_cut_streamlines $tractogram1 cut_streamlines2.trk \
+    --labels $labels --label_ids 16 2024
