e463883e6db105211dd70a5118b56d1b4444623b
frheault <francois.m.rheault@usherbrooke.ca>
2026-04-17 13:46:47 -0400

    Fix import Space

--- src/scilpy/io/utils.py
+++ src/scilpy/io/utils.py
@@ -13,6 +13,7 @@ import xml.etree.ElementTree as ET
 import nibabel as nib
 import numpy as np
 from dipy.data import SPHERE_FILES
+from dipy.io.stateful_tractogram import Origin, Space 
 from dipy.io.utils import is_header_compatible
 from scipy.io import loadmat
 import six
