4a558c9f94e69b7a3398e8a2e88bf13f69998b66
CHrlS98 <charles.poirier@usherbrooke.ca>
2026-05-19 11:11:33 -0400

    [WIP] Tracking from tracers

--- src/scilpy/tracking/tracker.py
+++ src/scilpy/tracking/tracker.py
@@ -750,3 +750,324 @@ class GPUTracker():
                 # output is yielded so that we can use LazyTractogram.
                 # seed and strl with origin center (same as DIPY)
                 yield strl - 0.5, seed - 0.5
+
+
+class TracerTracker():
+    def __init__(self, propagator: AbstractPropagator, wm_mask: DataVolume,
+                 projection_map: DataVolume, seed_generator: SeedGenerator,
+                 nbr_seeds, min_nbr_pts, max_nbr_pts, max_invalid_dirs,
+                 compression_th=0.1, save_seeds=False, rng_seed=1234,
+                 track_forward_only=False, skip=0, verbose=False,
+                 min_iter=100, append_last_point=True):
+        """
+        Tracker for tracer experiments. Similar to Tracker but with additional
+        input projection map and white matter mask. The tracking is constrained
+        by the white matter mask and the projection map is used as a
+        probabilistic map to favor tracking in voxels with higher projection
+        density.
+
+        Parameters
+        ----------
+        propagator : AbstractPropagator
+            Tracking object.
+            This tracker will use space and origin defined in the
+            propagator.
+        wm_mask : DataVolume
+            White matter mask. Tracking stops outside this mask.
+        projection_map : DataVolume
+            Projection density map. Used as a probabilistic map to favor tracking
+            in voxels with higher projection density.
+        seed_generator : SeedGenerator
+            Seeding volume.
+        nbr_seeds: int
+            Number of seeds to create via the seed generator.
+        min_nbr_pts: int
+            Minimum number of points for streamlines.
+        max_nbr_pts: int
+            Maximum number of points for streamlines.
+        max_invalid_dirs: int
+            Number of consecutives invalid directions allowed during tracking.
+        compression_th : float,
+            Maximal distance threshold for compression. If None, no
+            compression is applied.
+        nbr_processes: int
+            Number of sub processes to use.
+        save_seeds: bool
+            Whether to save the seeds associated to their respective
+            streamlines.
+        mmap_mode: str
+            Memory-mapping mode. One of {None, 'r+', 'c'}. This value is passed
+            to np.load() when loading the raw tracking data from a subprocess.
+        rng_seed: int
+            The random "seed" for the random generator.
+        track_forward_only: bool
+            If true, only the forward direction is computed.
+        skip: int
+            Skip the first N seeds created (and thus N rng numbers). Useful if
+            you want to create new streamlines to add to a previously created
+            tractogram with a fixed rng_seed. Ex: If tractogram_1 was created
+            with nbr_seeds=1,000,000, you can create tractogram_2 with
+            skip 1,000,000.
+        verbose: bool
+            Display tracking progression with TQDM progress bar.
+        min_iter: int
+            Minimum number of tracked streamlines required to update the
+            tracking progression bar.
+        append_last_point: bool
+            Whether to add the last point (once out of the tracking mask) to
+            the streamline or not. Note that points obtained after an invalid
+            direction (based on the propagator's definition of invalid; ex
+            when angle is too sharp of sh_threshold not reached) are never
+            added.
+        """
+        self.propagator = propagator
+        self.wm_mask = wm_mask
+        self.projection_map = projection_map
+        self.seed_generator = seed_generator
+        self.nbr_seeds = nbr_seeds
+        self.min_nbr_pts = min_nbr_pts
+        self.max_nbr_pts = max_nbr_pts
+        self.max_invalid_dirs = max_invalid_dirs
+        self.compression_th = compression_th
+        self.save_seeds = save_seeds
+        self.rng_seed = rng_seed
+        self.track_forward_only = track_forward_only
+        self.append_last_point = append_last_point
+        self.skip = skip
+
+        self.origin = self.propagator.origin
+        self.space = self.propagator.space
+        if self.space == Space.RASMM:
+            raise NotImplementedError(
+                "This version of the Tracker is not ready to work in RASMM "
+                "space.")
+        if (seed_generator.origin != propagator.origin or
+                seed_generator.space != propagator.space):
+            raise ValueError("Seed generator and propagator must work with "
+                             "the same space and origin!")
+
+        if self.min_nbr_pts <= 0:
+            logging.warning("Minimum number of points cannot be 0. Changed to "
+                            "1.")
+            self.min_nbr_pts = 1
+
+        self.printing_frequency = 1000
+        self.verbose = verbose
+        self.min_iter = min_iter
+
+    def track(self):
+        """
+        Generate a set of streamline from seed, mask and odf files.
+
+        Return
+        ------
+        streamlines: list of numpy.array
+            List of streamlines, represented as an array of positions.
+        seeds: list of numpy.array
+            List of seeding positions, one 3-dimensional position per
+            streamline.
+        """
+        lines, seeds = self._get_streamlines()
+        return lines, seeds
+
+    def _get_streamlines(self):
+        """
+        Tracks all streamlines. If asked by user, may compress the streamlines
+        and save the seeds.
+
+        Returns
+        -------
+        streamlines: list
+            The successful streamlines.
+        seeds: list
+            The list of seeds for each streamline, if self.save_seeds. Else, an
+            empty list.
+        """
+        streamlines = []
+        seeds = []
+
+        # Initialize the random number generator to cover multiprocessing,
+        # skip, which voxel to seed and the subvoxel random position
+        first_seed_of_chunk = self.skip
+        random_generator, indices = self.seed_generator.init_generator(
+            self.rng_seed, first_seed_of_chunk)
+
+        # Getting streamlines
+        tqdm_text = "#" + "{}".format(0).zfill(3)
+
+        if self.verbose:
+            if lock is None:
+                lock = nullcontext()
+            with lock:
+                p = tqdm(total=self.nbr_seeds, desc=tqdm_text, position=1, leave=False)
+
+        for s in range(self.nbr_seeds):
+            seed = self.seed_generator.get_next_pos(
+                random_generator, indices, first_seed_of_chunk + s)
+
+            # Setting the random value.
+            # Previous usage (and usage in Dipy) is to set the random seed
+            # based on the (real) seed position. However, in the case where we
+            # like to have exactly the same seed more than once, this will lead
+            # to exactly the same line, even in probabilistic tracking.
+            # Changing to seed position + seed number.
+            # Then in the case of multiprocessing, adding also a fraction based
+            # on current process ID.
+            eps = s
+            line_generator = np.random.default_rng(
+                np.abs(hash((seed + (eps, eps, eps), self.rng_seed))))
+
+            # Forward and backward tracking
+            line = self._get_line(seed, line_generator)
+
+            if line is not None:
+                streamline = np.array(line, dtype='float32')
+
+                if self.compression_th is not None:
+                    # Compressing. Threshold is in mm. Verifying space.
+                    if self.space == Space.VOX:
+                        # Equivalent of sft.to_voxmm:
+                        streamline *= self.seed_generator.voxres
+                        compress_streamlines(streamline, self.compression_th)
+                        # Equivalent of sft.to_vox:
+                        streamline /= self.seed_generator.voxres
+                    else:
+                        compress_streamlines(streamline, self.compression_th)
+
+                streamlines.append(streamline)
+
+                if self.save_seeds:
+                    seeds.append(np.asarray(seed, dtype='float32'))
+
+            # Note. Option min_iter does not work with manual pbar update.
+            # Will verify manually, lower.
+            # Fixed choice of value rather than a percentage of the chunk
+            # size because our tracker is quite slow.
+            if self.verbose and (s + 1) % self.min_iter == 0:
+                p.update(self.min_iter)
+
+        if self.verbose:
+            p.close()
+        return streamlines, seeds
+
+    def _get_line(self, seeding_pos, line_generator):
+        """
+        Generate a streamline from an initial position following the tracking
+        parameters.
+
+        Parameters
+        ----------
+        seeding_pos : tuple
+            3D position, the seed position.
+
+        Returns
+        -------
+        line: list of 3D positions
+            The generated streamline for seeding_pos.
+        """
+        # Forward
+        line = [np.asarray(seeding_pos)]
+        tracking_info = self.propagator.prepare_forward(seeding_pos, line_generator)
+        if tracking_info == PropagationStatus.ERROR:
+            # No good tracking direction can be found at seeding position.
+            return None
+
+        # propagate the line in forward direction (tracking_info is the initial direction)
+        line = self._propagate_line(line, tracking_info)
+
+        # Backward
+        if not self.track_forward_only:
+            if len(line) > 1:
+                line.reverse()
+
+            tracking_info = self.propagator.prepare_backward(line, tracking_info)
+            line = self._propagate_line(line, tracking_info)
+
+        # Clean streamline
+        if self.min_nbr_pts <= len(line) <= self.max_nbr_pts:
+            return line
+        return None
+
+    def _propagate_line(self, line, previous_dir):
+        """
+        Generate a streamline in forward or backward direction from an initial
+        position following the tracking parameters.
+
+        Propagation will stop if the current position is out of bounds (mask's
+        bounds and data's bounds should be the same) or if mask's value at
+        current position is 0 (usual use is with a binary mask but this is not
+        mandatory).
+
+        Parameters
+        ----------
+        line: List[np.ndarrays]
+            Beginning of the line to propagate: list of 3D coordinates
+            formatted as arrays.
+        previous_dir: Any
+            Information necessary to know how to propagate. Type: as understood
+            by the propagator. Example, with the typical fODF propagator: the
+            previous direction of the streamline, v_in, used to define a cone
+            theta, of type TrackingDirection.
+
+        Returns
+        -------
+        line: list of 3D positions
+            At minimum, stays as initial line. Or extended with new tracked
+            points.
+        """
+        invalid_direction_count = 0
+        propagation_can_continue = True
+
+        while len(line) < self.max_nbr_pts and propagation_can_continue:
+            new_pos, new_dir, is_direction_valid = \
+                self.propagator.propagate(line, previous_dir)
+
+            # Verifying if direction is valid
+            # If invalid: break. Else, verify tracking mask.
+            if is_direction_valid:
+                invalid_direction_count = 0
+            else:
+                invalid_direction_count += 1
+                if invalid_direction_count > self.max_invalid_dirs:
+                    break
+
+            # Test if the new position is inside WM
+            propagation_can_continue = self._verify_stopping_criteria(new_pos)
+            if propagation_can_continue or self.append_last_point:
+                line.append(new_pos)
+
+            previous_dir = new_dir
+
+        logging.debug(f"TRACKER end of propagation: {len(line)} total points, last pos={np.round(line[-1], 2)}")
+        return line
+
+    def _verify_stopping_criteria(self, last_pos):
+        # Checking if out of bound
+        if not self.projection_map.is_coordinate_in_bound(
+                *last_pos, space=self.space, origin=self.origin):
+            return False
+
+        # Propagation stops if density projection value is below some threshold
+        if self.wm_mask.get_value_at_coordinate(
+                *last_pos, space=self.space, origin=self.origin) <= 0:
+            return False
+
+        return True
+
+    def _verify_inclusion_criteria(self, last_pos):
+        # a streamline is included if it ends in a valid termination region.
+
+        # can't terminate inside WM mask
+        if self.wm_mask.get_value_at_coordinate(
+                *last_pos, space=self.space, origin=self.origin) > 0:
+            return False
+
+        # then the probability of terminating in any GM voxel is proportional to the projection density in that voxel
+        projection_density = self.projection_map.get_value_at_coordinate(
+                *last_pos, space=self.space, origin=self.origin)
+
+        randval = np.random.uniform(0, 1)
+        if randval > projection_density:
+            return False
+
+        return True
