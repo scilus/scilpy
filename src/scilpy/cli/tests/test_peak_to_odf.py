4c4377cc90ea77866486c7719e73c5bb3cfdb973
CHrlS98 <charles.poirier@usherbrooke.ca>
2026-07-02 14:34:17 -0400

    One more test for peak_to_odf.py

--- src/scilpy/cli/tests/test_peak_to_odf.py
+++ src/scilpy/cli/tests/test_peak_to_odf.py
@@ -28,7 +28,31 @@ def test_execution(script_runner, monkeypatch):
     nib.save(single_dir_nib, single_dir_img)
 
     out_sh = 'peaks_to_odf.nii.gz'
-    ret = script_runner.run(['scil_peak_to_odf', single_dir_img, out_sh])
+    ret = script_runner.run(['scil_peak_to_odf', single_dir_img, out_sh,
+                             '--sh_order_max', 6])
+
+    assert ret.success
+
+    in_peaks = nib.load(in_img)
+    out_peaks = nib.load(out_sh)
+
+    assert out_peaks.shape[:3] == in_peaks.shape[:3]
+    assert out_peaks.ndim == 4
+    assert np.allclose(out_peaks.affine, in_peaks.affine)
+    assert np.isfinite(out_peaks.get_fdata()).all()
+
+
+def test_execution_no_apodization(script_runner, monkeypatch):
+    monkeypatch.chdir(os.path.expanduser(tmp_dir.name))
+
+    # generate temp file
+    single_dir_nib = nib.load(in_img).slicer[..., :3]
+    single_dir_img = os.path.join(tmp_dir.name, 'single_dir_peaks.nii.gz')
+    nib.save(single_dir_nib, single_dir_img)
+
+    out_sh = 'peaks_to_odf_no_apod.nii.gz'
+    ret = script_runner.run(['scil_peak_to_odf', single_dir_img, out_sh,
+                             '--disable_apodization', '--sh_order_max', 6])
 
     assert ret.success
 
