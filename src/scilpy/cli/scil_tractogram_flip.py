64437f2df9fc618fcfb0d55f4228c860989423c1
frheault <francois.m.rheault@usherbrooke.ca>
2026-09-11 18:35:20 -0400

    Put back actually useful param

--- src/scilpy/cli/scil_tractogram_flip.py
+++ src/scilpy/cli/scil_tractogram_flip.py
@@ -59,7 +59,7 @@ def main():
     sft.to_corner()
 
     new_sft = flip_sft(sft, args.axes)
-    save_tractogram(new_sft, args.out_tractogram,
+    save_tractogram(new_sft, args.out_tractogram, no_empty=False,
                     bbox_valid_check=args.bbox_check)
 
 
